1ec4e7f4dbbabdbcf77202c3d14a04cff47b9d47
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2021-08-27 01:16:06 -0700

    [CIR] Add -fcir-output for writing clang IR output to a file
    Two modes available:
    - `-fcir-output` creates a new file with suffixed with .cir on top of the source file.
    - `-fcir-output=<file>` uses user provided file path.
    
    Currently only supported when the driver takes a single source input.

--- clang/lib/Frontend/CompilerInvocation.cpp
+++ clang/lib/Frontend/CompilerInvocation.cpp
@@ -3978,6 +3978,9 @@ bool CompilerInvocation::ParseLangArgs(LangOptions &Opts, ArgList &Args,
           << Opts.OMPHostIRFile;
   }
 
+  if (Arg *A = Args.getLastArg(options::OPT_fcir_output_EQ))
+    Opts.CIRFile = A->getValue();
+
   // Set CUDA mode for OpenMP target NVPTX/AMDGCN if specified in options
   Opts.OpenMPCUDAMode = Opts.OpenMPIsDevice && (T.isNVPTX() || T.isAMDGCN()) &&
                         Args.hasArg(options::OPT_fopenmp_cuda_mode);
