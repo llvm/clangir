40be0ab818839e04443311dcc22e38e4f3018ad7
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2021-08-27 01:16:06 -0700

    [CIR] Add -fcir-output for writing clang IR output to a file
    Two modes available:
    - `-fcir-output` creates a new file with suffixed with .cir on top of the source file.
    - `-fcir-output=<file>` uses user provided file path.
    
    Currently only supported when the driver takes a single source input.

--- clang/include/clang/Sema/Sema.h
+++ clang/include/clang/Sema/Sema.h
@@ -10108,7 +10108,7 @@ public:
   threadSafety::BeforeSet *ThreadSafetyDeclCache;
 
   /// Worker object for performing CIR based warnings.
-  sema::CIRBasedWarnings CIRWarnings;
+  std::unique_ptr<sema::CIRBasedWarnings> CIRWarnings;
 
   /// An entity for which implicit template instantiation is required.
   ///
