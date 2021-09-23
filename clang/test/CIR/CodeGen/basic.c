a757280429eed0c5e355de7c5d472441cf26bdd6
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2021-09-22 20:48:04 -0700

    [CIR] Add memref to handle allocas
    We might decide to use something custom later and lower that to
    memref's instead, but for now use this for the convenience of
    mapping lvalues.

--- clang/test/CIR/CodeGen/basic.c
+++ clang/test/CIR/CodeGen/basic.c
@@ -1,12 +1,16 @@
 // RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -fsyntax-only -fcir-warnings %s -fcir-output=%t.cir
 // RUN: FileCheck --input-file=%t.cir %s
+// XFAIL: *
 
 int foo(int i) {
   return i;
 }
 
-//      CHECK: module  {
+// CHECK: module  {
 // CHECK-NEXT:   func @foo(%arg0: i32) -> i32 {
-// CHECK-NEXT:     cir.return %arg0 : i32
+// CHECK-NEXT:     %0 = memref.alloca() : memref<i32>
+// CHECK-NEXT:     memref.store %arg0, %0[] : memref<i32>
+// CHECK-NEXT:     %1 = memref.load %0[] : memref<i32>
+// CHECK-NEXT:     cir.return %1 : i32
 // CHECK-NEXT:   }
 // CHECK-NEXT: }
