fe11fc20a4ca526012737a569d8cd613589a0ca5
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2021-10-05 22:00:58 -0700

    [CIR] Add cir-tool and first cir parse/print test

---
+++ clang/tools/cir-tool/cir-tool.cpp
@@ -0,0 +1,29 @@
+//===- cir-tool.cpp - CIR optimizationa and analysis driver -----*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// Similar to MLIR/LLVM's "opt" tools but also deals with analysis and custom
+// arguments. TODO: this is basically a copy from MlirOptMain.cpp, but capable
+// of module emission as specified by the user.
+//
+//===----------------------------------------------------------------------===//
+
+#include "mlir/Dialect/CIR/IR/CIRDialect.h"
+#include "mlir/Support/MlirOptMain.h"
+
+using namespace mlir;
+
+int main(int argc, char **argv) {
+  // TODO: register needed MLIR passes for CIR?
+  mlir::DialectRegistry registry;
+  // TODO: add memref::MemRefDialect> when we add lowering
+  registry.insert<cir::CIRDialect>();
+
+  return failed(MlirOptMain(argc, argv,
+                            "Clang IR analysis and optimization tool\n",
+                            registry, /*preloadDialectsInContext=*/false));
+}
