687f21b9bd12cb0acd5e132e335fc5474eb292fa
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2021-10-07 17:47:33 -0700

    [CIR] Add first set of CIR operations: local, get_addr, load and store
    These are used for using and tracking lvalues, conversions and local variables.
    
    TODOS from here:
    - Add constrains for the types allowed and verification.
    - Change current codegen to emit CIR instead of memref
    - Add a new pass to lowere these to memrefs.
    - Add docs to load/store

--- clang/test/CIR/CodeGen/basic.c
+++ clang/test/CIR/CodeGen/basic.c
@@ -1,16 +1,14 @@
 // RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -fsyntax-only -fcir-warnings %s -fcir-output=%t.cir
 // RUN: FileCheck --input-file=%t.cir %s
-// XFAIL: *
 
 int foo(int i) {
   return i;
 }
 
 // CHECK: module  {
-// CHECK-NEXT:   func @foo(%arg0: i32) -> i32 {
-// CHECK-NEXT:     %0 = memref.alloca() : memref<i32>
-// CHECK-NEXT:     memref.store %arg0, %0[] : memref<i32>
-// CHECK-NEXT:     %1 = memref.load %0[] : memref<i32>
-// CHECK-NEXT:     cir.return %1 : i32
-// CHECK-NEXT:   }
+// CHECK-NEXT: func @foo(%arg0: i32) -> i32 {
+// CHECK-NEXT:   %0 = cir.alloca i32 = uninitialized, cir.ptr <i32>
+// CHECK-NEXT:   cir.store %arg0, %0 : i32, cir.ptr <i32>
+// CHECK-NEXT:   %1 = cir.load %0 : cir.ptr <i32>, i32
+// CHECK-NEXT:   cir.return %1 : i32
 // CHECK-NEXT: }
