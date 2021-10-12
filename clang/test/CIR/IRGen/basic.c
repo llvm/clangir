4451ae8b8274f5cbfc1b136c5580a6c79dbdcaaf
Nathan Lanza <nathan@lanza.io>
2021-10-12 14:39:24 -0400

    [CIR] Implement simple lowering of CIR to LLVMIR, currently only supporting cir.ret
    This patch adds the simple infrastructure necessary to enable the
    `-emit-llvm` flag such that the following invocation will do what you'd
    expect:
    
    ```
    $ clang -cc1 -fenable-clangir -emit-llvm -o out.ll foo.c
    ```
    
    There is only one operation currently supported, `cir.ret`, and it is
    only supported for the case of returning nothing.

---
+++ clang/test/CIR/IRGen/basic.c
@@ -0,0 +1,8 @@
+// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -fenable-clangir -emit-llvm %s -o %t.ll
+// RUN: FileCheck --input-file=%t.ll %s
+
+void foo() {}
+
+// CHECK: define void @foo()
+// CHECK-NEXT: ret void,
+// CHECK-NEXT: }
