a054d59601511ce00ae2bcb3920b7723391f6cd2
Nathan Lanza <nathan@lanza.io>
2021-10-15 00:11:02 -0400

    [CIR] Implement cc1 support for a CIR pipeline
    This patch implements trivial cc1 support that'll let you pass
    `-fenable-clangir` to run clang with the clangir pipeline inserted. This
    also adds `-emit-cir` which will output a cir file and `-emit-cir-only`
    which will run the pipeline stopping after CIRGen.
    
    $ clang -cc1 test.c -fenable-clangir -emit-cir-only        (no output)
    $ clang -cc1 test.c -fenable-clangir -emit-cir             (outputs test.cir)
    $ clang -cc1 test.c -fenable-clangir -emit-cir -o here.cir (outputs here.cir)
    
    The following invocation:
    
    $ clang -cc1 test.c -fenable-clangir
    
    will crash. But it will eventually work as stanard clang does honoring
    your standard permutations of `-emit-llvm`, `-S`, `-c`,
    `-emit-llvm-only` etc.

--- clang/lib/Frontend/CompilerInvocation.cpp
+++ clang/lib/Frontend/CompilerInvocation.cpp
@@ -2399,6 +2399,8 @@ static const auto &getFrontendActionTable() {
       {frontend::DumpTokens, OPT_dump_tokens},
       {frontend::EmitAssembly, OPT_S},
       {frontend::EmitBC, OPT_emit_llvm_bc},
+      {frontend::EmitCIR, OPT_emit_cir},
+      {frontend::EmitCIROnly, OPT_emit_cir_only},
       {frontend::EmitHTML, OPT_emit_html},
       {frontend::EmitLLVM, OPT_emit_llvm},
       {frontend::EmitLLVMOnly, OPT_emit_llvm_only},
@@ -2731,6 +2733,8 @@ static bool ParseFrontendArgs(FrontendOptions &Opts, ArgList &Args,
   if (Opts.ProgramAction != frontend::GenerateModule && Opts.IsSystemModule)
     Diags.Report(diag::err_drv_argument_only_allowed_with) << "-fsystem-module"
                                                            << "-emit-module";
+  if (Args.hasArg(OPT_fenable_clangir))
+    Opts.UseClangIRPipeline = true;
 
   if (Args.hasArg(OPT_aux_target_cpu))
     Opts.AuxTargetCPU = std::string(Args.getLastArgValue(OPT_aux_target_cpu));
@@ -3978,9 +3982,6 @@ bool CompilerInvocation::ParseLangArgs(LangOptions &Opts, ArgList &Args,
           << Opts.OMPHostIRFile;
   }
 
-  if (Arg *A = Args.getLastArg(options::OPT_fcir_output_EQ))
-    Opts.CIRFile = A->getValue();
-
   // Set CUDA mode for OpenMP target NVPTX/AMDGCN if specified in options
   Opts.OpenMPCUDAMode = Opts.OpenMPIsDevice && (T.isNVPTX() || T.isAMDGCN()) &&
                         Args.hasArg(options::OPT_fopenmp_cuda_mode);
@@ -4142,6 +4143,8 @@ static bool isStrictlyPreprocessorAction(frontend::ActionKind Action) {
   case frontend::ASTView:
   case frontend::EmitAssembly:
   case frontend::EmitBC:
+  case frontend::EmitCIR:
+  case frontend::EmitCIROnly:
   case frontend::EmitHTML:
   case frontend::EmitLLVM:
   case frontend::EmitLLVMOnly:
