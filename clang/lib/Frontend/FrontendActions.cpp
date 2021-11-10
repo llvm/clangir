14163fbc21d8654a68455153c11b0bda0ade511b
Nathan Lanza <nathan@lanza.io>
2021-11-09 23:46:38 -0500

    [CIR] Implementing support for parsing and lowering CIR files
    For the purpose of iterating on lowering individual CIR instructions
    it's much easier to just operate on a sample CIR file with one new
    instruction at a time instead of having to go through CIRGen.

--- clang/lib/Frontend/FrontendActions.cpp
+++ clang/lib/Frontend/FrontendActions.cpp
@@ -1061,6 +1061,7 @@ void PrintPreambleAction::ExecuteAction() {
   case Language::CUDA:
   case Language::HIP:
   case Language::HLSL:
+  case Language::CIR:
     break;
 
   case Language::Unknown:
