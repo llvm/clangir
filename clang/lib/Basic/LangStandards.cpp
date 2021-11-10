08af679b3b769f79267a9253249aa941916e14ab
Nathan Lanza <nathan@lanza.io>
2021-11-09 23:46:38 -0500

    [CIR] Implementing support for parsing and lowering CIR files
    For the purpose of iterating on lowering individual CIR instructions
    it's much easier to just operate on a sample CIR file with one new
    instruction at a time instead of having to go through CIRGen.

--- clang/lib/Basic/LangStandards.cpp
+++ clang/lib/Basic/LangStandards.cpp
@@ -47,6 +47,8 @@ const LangStandard *LangStandard::getLangStandardForName(StringRef Name) {
 LangStandard::Kind clang::getDefaultLanguageStandard(clang::Language Lang,
                                                      const llvm::Triple &T) {
   switch (Lang) {
+  case Language::CIR:
+    llvm_unreachable("NYI");
   case Language::Unknown:
   case Language::LLVM_IR:
     llvm_unreachable("Invalid input kind!");
