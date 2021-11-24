8403d639392cd1f40e980e8db01f76a6ef19363a
Nathan Lanza <nathan@lanza.io>
2021-11-24 02:54:22 -0500

    [CIR] Switch cir.alloca to lower to memref.alloca instead of memref.alloc
    This was a typo, simply fix it here.

--- clang/lib/CIR/LowerToLLVM.cpp
+++ clang/lib/CIR/LowerToLLVM.cpp
@@ -85,8 +85,8 @@ public:
   matchAndRewrite(mlir::cir::AllocaOp op,
                   mlir::PatternRewriter &rewriter) const override {
     auto ty = mlir::MemRefType::get({}, op.type());
-    rewriter.replaceOpWithNewOp<mlir::memref::AllocOp>(op, ty,
-                                                       op.alignmentAttr());
+    rewriter.replaceOpWithNewOp<mlir::memref::AllocaOp>(op, ty,
+                                                        op.alignmentAttr());
     return mlir::LogicalResult::success();
   }
 };
