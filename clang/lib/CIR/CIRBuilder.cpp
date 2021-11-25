918da2e7b23aca339c88acc4a29c1b9300b33c6a
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2021-11-24 18:08:01 -0800

    [CIR] Fix codegen of else stmt in if stmt

--- clang/lib/CIR/CIRBuilder.cpp
+++ clang/lib/CIR/CIRBuilder.cpp
@@ -1586,7 +1586,7 @@ public:
         [&](mlir::OpBuilder &b, mlir::Location loc) { (void)buildStmt(thenS); },
         /*elseBuilder=*/
         [&](mlir::OpBuilder &b, mlir::Location loc) {
-          if (elseS)
+          if (!elseS)
             return;
           (void)buildStmt(elseS);
         });
