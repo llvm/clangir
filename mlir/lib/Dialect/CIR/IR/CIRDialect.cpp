52ec43a27a3b8b8f248b2523b297ce9df0e7931a
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2021-12-02 00:36:40 -0800

    [CIR][Builder] Codegen a cir.scope around cir.if
    - This should limit the lifetime of both if initializers and conditions.
    - Update ifstmt tests to check for a scope.

--- mlir/lib/Dialect/CIR/IR/CIRDialect.cpp
+++ mlir/lib/Dialect/CIR/IR/CIRDialect.cpp
@@ -282,6 +282,7 @@ static ParseResult parseScopeOp(OpAsmParser &parser, OperationState &result) {
 }
 
 static void print(OpAsmPrinter &p, ScopeOp op) {
+  p << ' ';
   p.printRegion(op.scopeRegion(),
                 /*printEntryBlockArgs=*/false,
                 /*printBlockTerminators=*/false);
@@ -328,8 +329,9 @@ static LogicalResult verify(ScopeOp op) { return success(); }
 //===----------------------------------------------------------------------===//
 
 static mlir::LogicalResult verify(YieldOp op) {
-  if (!llvm::isa<IfOp>(op->getParentOp()))
-    return op.emitOpError() << "expects 'if' as the parent operation'";
+  if (!llvm::isa<IfOp, ScopeOp>(op->getParentOp()))
+    return op.emitOpError()
+           << "expects 'cir.if' or 'cir.scope' as the parent operation'";
 
   return mlir::success();
 }
