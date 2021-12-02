5a76f8cd47194070f7aeb5b97c60dc80dd94798d
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2021-12-01 23:06:37 -0800

    [CIR] More improvements on cir.if generation
    - Do proper detection of 'else' when building CIR.
    - Fix getSuccessorRegions to get proper verification.
    - Chained if's now work too.
    - More tests

--- clang/lib/CIR/CIRBuilder.cpp
+++ clang/lib/CIR/CIRBuilder.cpp
@@ -1578,7 +1578,7 @@ public:
     cond = cond->IgnoreParens();
     mlir::Value condV = evaluateExprAsBool(cond);
     builder.create<mlir::cir::IfOp>(
-        loc, condV,
+        loc, condV, elseS,
         /*thenBuilder=*/
         [&](mlir::OpBuilder &b, mlir::Location loc) {
           (void)buildStmt(thenS);
@@ -1586,8 +1586,6 @@ public:
         },
         /*elseBuilder=*/
         [&](mlir::OpBuilder &b, mlir::Location loc) {
-          if (!elseS)
-            return;
           (void)buildStmt(elseS);
           builder.create<YieldOp>(getLoc(elseS->getSourceRange().getEnd()));
         });
