cbdb1a442c601b0564c26a76c8e43431d7a799e5
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2021-12-04 00:15:05 -0800

    [CIR] Remove accidental dump() leftover

--- clang/lib/CIR/CIRBuilder.cpp
+++ clang/lib/CIR/CIRBuilder.cpp
@@ -1946,7 +1946,6 @@ void CIRContext::verifyModule() { builder->verifyModule(); }
 bool CIRContext::EmitFunction(const FunctionDecl *FD) {
   CIRCodeGenFunction CCGF{};
   auto func = builder->buildCIR(&CCGF, FD);
-  func->dump();
   assert(func && "should emit function");
   return true;
 }
