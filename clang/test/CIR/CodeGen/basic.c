38e5bc27ec2d515b5aa5d0a29579e0fa0a3d25f0
Nathan Lanza <nathan@lanza.io>
2022-02-10 23:16:58 -0500

    [CIR] Add a buildGlobal func to CIRGenModule to support function prototypes
    This mostly just wraps buildFunction for now while asserting that we
    weren't trying to compile anything we don't yet support. But we also
    weren't checking for whether or not this was just a declaration prior to
    this and thus would fail on a simple function declaration.

--- clang/test/CIR/CodeGen/basic.c
+++ clang/test/CIR/CodeGen/basic.c
@@ -1,6 +1,8 @@
 // RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -fenable-clangir -emit-cir %s -o %t.cir
 // RUN: FileCheck --input-file=%t.cir %s
 
+int foo(int i);
+
 int foo(int i) {
   return i;
 }
