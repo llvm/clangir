34cda361cacf3fe65ba5ce349309c8a15f9784a4
Nathan Lanza <nathan@lanza.io>
2022-02-17 00:34:12 -0500

    [CIR][NFC] Enable StandardOps dialect
    Just enable the StandardOps dialect so that we can build CallOps. AFAIK
    this is actually being removed upstream so this will be fun to rebase.

--- clang/lib/CIR/CIRGenerator.cpp
+++ clang/lib/CIR/CIRGenerator.cpp
@@ -13,6 +13,7 @@
 #include "CIRGenModule.h"
 
 #include "mlir/Dialect/CIR/IR/CIRDialect.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/Dialect/MemRef/IR/MemRef.h"
 #include "mlir/IR/MLIRContext.h"
 
@@ -33,6 +34,7 @@ void CIRGenerator::Initialize(ASTContext &astCtx) {
 
   mlirCtx = std::make_unique<mlir::MLIRContext>();
   mlirCtx->getOrLoadDialect<mlir::cir::CIRDialect>();
+  mlirCtx->getOrLoadDialect<mlir::func::FuncDialect>();
   mlirCtx->getOrLoadDialect<mlir::memref::MemRefDialect>();
   CGM = std::make_unique<CIRGenModule>(*mlirCtx.get(), astCtx, codeGenOpts);
 }
