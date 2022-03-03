7e27d361f1ff9a74fe10c3a9a4fa9c8e624473ab
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2022-03-02 16:15:37 -0800

    [CIR][LifetimeCheck] Add options to allow remarks to be emitted

--- mlir/lib/Dialect/CIR/Transforms/LifetimeCheck.cpp
+++ mlir/lib/Dialect/CIR/Transforms/LifetimeCheck.cpp
@@ -31,6 +31,23 @@ struct LifetimeCheckPass : public LifetimeCheckBase<LifetimeCheckPass> {
   void checkStore(StoreOp op);
   void checkLoad(LoadOp op);
 
+  struct Options {
+    enum : unsigned { None = 0, RemarkPset = 1, RemarkAll = 1 << 1 };
+    unsigned val = None;
+
+    void parseOptions(LifetimeCheckPass &pass) {
+      for (auto &remark : pass.remarksList) {
+        val |= StringSwitch<unsigned>(remark)
+                   .Case("pset", RemarkPset)
+                   .Case("all", RemarkAll)
+                   .Default(None);
+      }
+    }
+
+    bool emitRemarkAll() { return val & RemarkAll; }
+    bool emitRemarkPset() { return emitRemarkAll() || val & RemarkPset; }
+  } opts;
+
   struct State {
     using DataTy = enum {
       Invalid,
@@ -295,6 +312,7 @@ void LifetimeCheckPass::checkOperation(Operation *op) {
 }
 
 void LifetimeCheckPass::runOnOperation() {
+  opts.parseOptions(*this);
   Operation *op = getOperation();
   checkOperation(op);
 }
