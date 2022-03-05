a556d9d2a507d7e77f3e129a39f4eb644b98ba1e
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2022-03-04 18:40:04 -0800

    [CIR][LifetimeCheck] Add support for null history notes

--- clang/test/CIR/Transforms/lifetime-check.cpp
+++ clang/test/CIR/Transforms/lifetime-check.cpp
@@ -1,5 +1,5 @@
 // RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -fenable-clangir -emit-cir %s -o %t.cir
-// RUN: cir-tool %t.cir -cir-lifetime-check="history=invalid" -verify-diagnostics -o %t-out.cir
+// RUN: cir-tool %t.cir -cir-lifetime-check="history=invalid,null" -verify-diagnostics -o %t-out.cir
 
 int *p0() {
   int *p = nullptr;
@@ -13,12 +13,25 @@ int *p0() {
 }
 
 int *p1(bool b = true) {
-  int *p = nullptr;
+  int *p = nullptr; // expected-note {{invalidated here}}
   if (b) {
     int x = 0;
     p = &x;
     *p = 42;
   }        // expected-note {{pointee 'x' invalidated at end of scope}}
   *p = 42; // expected-warning {{use of invalid pointer 'p'}}
+  return p;
+}
+
+int *p2() {
+  int *p = nullptr; // expected-note {{invalidated here}}
+  *p = 42;          // expected-warning {{use of invalid pointer 'p'}}
+  return p;
+}
+
+int *p3() {
+  int *p;
+  p = nullptr; // expected-note {{invalidated here}}
+  *p = 42;     // expected-warning {{use of invalid pointer 'p'}}
   return p;
 }
