203205004e1bc73ed94c0640570c04aeeeda6230
Nathan Lanza <nathan@lanza.io>
2022-03-11 17:09:30 -0500

    [CIR][NFC] Reorder a function to match clang's ordering

--- clang/lib/CIR/CIRGenModule.cpp
+++ clang/lib/CIR/CIRGenModule.cpp
@@ -1658,10 +1658,6 @@ void CIRGenModule::verifyModule() {
     theModule.emitError("module verification error");
 }
 
-mlir::Value CIRGenModule::GetGlobalValue(const Decl *D) {
-  return symbolTable.lookup(D);
-}
-
 mlir::FuncOp CIRGenModule::GetAddrOfFunction(clang::GlobalDecl GD,
                                              mlir::Type Ty, bool ForVTable,
                                              bool DontDefer,
@@ -1805,3 +1801,6 @@ mlir::FuncOp CIRGenModule::GetOrCreateCIRFunction(
   assert(false && "Incompmlete functions NYI");
 }
 
+mlir::Value CIRGenModule::GetGlobalValue(const Decl *D) {
+  return symbolTable.lookup(D);
+}
