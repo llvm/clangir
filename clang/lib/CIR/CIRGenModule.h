9fbdadda09712d661ef0ea5d0746a98fa845ccbd
Nathan Lanza <nathan@lanza.io>
2022-03-11 16:48:34 -0500

    [CIR] Add a helper for getting mangled names in CIRGenModule

--- clang/lib/CIR/CIRGenModule.h
+++ clang/lib/CIR/CIRGenModule.h
@@ -454,6 +454,8 @@ public:
 
   void verifyModule();
 
+  llvm::StringRef getMangledName(clang::GlobalDecl GD);
+
   mlir::Value GetGlobalValue(const clang::Decl *D);
 
 private:
@@ -464,6 +466,10 @@ private:
                          clang::GlobalDecl D, bool ForVTable,
                          bool DontDefer = false, bool IsThunk = false,
                          ForDefinition_t IsForDefinition = NotForDefinition);
+
+  // An ordered map of canonical GlobalDecls to their mangled names.
+  llvm::MapVector<clang::GlobalDecl, llvm::StringRef> MangledDeclNames;
+  llvm::StringMap<clang::GlobalDecl, llvm::BumpPtrAllocator> Manglings;
 };
 } // namespace cir
 
