f1179e370d46d2b244684d8dc53313b99ae3d3b5
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2022-03-31 14:55:21 -0700

    [CIR] Initial support for switch stmt codegen in clang

---
+++ clang/test/CIR/CodeGen/switch.cpp
@@ -0,0 +1,13 @@
+// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -fenable-clangir -emit-cir %s -o %t.cir
+// RUN: FileCheck --input-file=%t.cir %s
+// XFAIL: *
+
+void sw0(int a) {
+  switch (a) {}
+}
+
+// CHECK: cir.scope {
+// CHECK-NEXT:   %1 = cir.load %0 : cir.ptr <i32>, i32
+// CHECK-NEXT:   cir.switch (%1 : i32) [
+// CHECK-NEXT:   ]
+// CHECK-NEXT: }
