16d06bd3d81f3d847fe6ef0473ffb5f56cb2f0ac
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2022-04-13 15:59:23 -0700

    [CIR] Remove extra yield, this should be added already by terminateBody calls

--- clang/lib/CIR/CIRGenModule.cpp
+++ clang/lib/CIR/CIRGenModule.cpp
@@ -1587,7 +1587,6 @@ mlir::LogicalResult CIRGenModule::buildWhileStmt(const WhileStmt &S) {
         [&](mlir::OpBuilder &b, mlir::Location loc) {
           if (buildStmt(S.getBody(), /*useCurrentScope=*/true).failed())
             forRes = mlir::failure();
-          builder.create<YieldOp>(loc);
         },
         /*stepBuilder=*/
         [&](mlir::OpBuilder &b, mlir::Location loc) {
@@ -1656,7 +1655,6 @@ mlir::LogicalResult CIRGenModule::buildForStmt(const ForStmt &S) {
           // for C++ in case it's a compound statement?
           if (buildStmt(S.getBody(), /*useCurrentScope=*/true).failed())
             forRes = mlir::failure();
-          builder.create<YieldOp>(loc);
         },
         /*stepBuilder=*/
         [&](mlir::OpBuilder &b, mlir::Location loc) {
