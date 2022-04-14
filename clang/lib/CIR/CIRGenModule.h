188738a12633588df9cf132cabb5d5151385989f
Nathan Lanza <nathan@lanza.io>
2022-04-14 19:48:58 -0400

    [CIR] Add a hacky temporary GetGlobalValue that gets Fns from the module
    This is kinda gross, but it'll suffice for now. When generating
    functions later in `buildGlobalFunctionDefinition` we should add the
    functions as they are generated to a mapping from their mangled name to
    their FuncOp.

--- clang/lib/CIR/CIRGenModule.h
+++ clang/lib/CIR/CIRGenModule.h
@@ -189,6 +189,8 @@ public:
 
   mlir::Value GetGlobalValue(const clang::Decl *D);
 
+  mlir::Operation *GetGlobalValue(llvm::StringRef Ref);
+
   // Make sure that this type is translated.
   void UpdateCompletedType(const clang::TagDecl *TD);
 
@@ -203,7 +205,6 @@ public:
 
   // Finalize CIR code generation.
   void Release();
-
 
   void emitError(const llvm::Twine &message) { theModule.emitError(message); }
 
