d9665f150571c2b8ae8469c0b4ac3c5dd74b8c44
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2022-04-21 17:46:00 -0700

    [CIR] Enable a proper cir.loop verifier and add more tests

--- mlir/lib/Dialect/CIR/IR/CIRDialect.cpp
+++ mlir/lib/Dialect/CIR/IR/CIRDialect.cpp
@@ -896,6 +896,31 @@ void LoopOp::getSuccessorRegions(Optional<unsigned> index,
 
 Region &LoopOp::getLoopBody() { return getBody(); }
 
+LogicalResult LoopOp::verify() {
+  // Cond regions should only terminate with plain 'cir.yield' or
+  // 'cir.yield continue'.
+  auto terminateError = [&]() {
+    return emitOpError() << "cond region must be terminated with "
+                               "'cir.yield' or 'cir.yield continue'";
+  };
+
+  auto &blocks = getCond().getBlocks();
+  for (Block &block : blocks) {
+    if (block.empty())
+      continue;
+    auto &op = block.back();
+    if (isa<BrCondOp>(op))
+      continue;
+    if (!isa<YieldOp>(op))
+      terminateError();
+    auto y = cast<YieldOp>(op);
+    if (!(y.isPlain() || y.isContinue()))
+      terminateError();
+  }
+
+  return success();
+}
+
 //===----------------------------------------------------------------------===//
 // CIR defined traits
 //===----------------------------------------------------------------------===//
