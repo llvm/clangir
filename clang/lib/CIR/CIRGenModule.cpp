b9a8b0a0a74c958ee3aa3bd5580a2b5d4eeee4cf
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2022-05-04 22:12:57 -0700

    [CIR][CodeGen] Implement basic const global, only emitted when used

--- clang/lib/CIR/CIRGenModule.cpp
+++ clang/lib/CIR/CIRGenModule.cpp
@@ -298,7 +298,8 @@ void CIRGenModule::buildGlobal(GlobalDecl GD) {
   // the order in which it appeared on the file.
   if (getLangOpts().CPlusPlus && isa<VarDecl>(Global) &&
       cast<VarDecl>(Global)->hasInit()) {
-    llvm_unreachable("NYI");
+    DelayedCXXInitPosition[Global] = CXXGlobalInits.size();
+    CXXGlobalInits.push_back(nullptr);
   }
 
   llvm::StringRef MangledName = getMangledName(GD);
@@ -1307,7 +1308,7 @@ void CIRGenModule::Release() {
   // TODO: applyReplacements();
   // TODO: checkAliases();
   // TODO: buildMultiVersionFunctions();
-  // TODO: buildCXXGlobalInitFunc();
+  buildCXXGlobalInitFunc();
   // TODO: buildCXXGlobalCleanUpFunc();
   // TODO: registerGlobalDtorsWithAtExit();
   // TODO: buildCXXThreadLocalInitFunc();
