b4dbe2a0a8c6df9650fc84b67fb926931bde516c
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2022-05-12 15:31:05 -0700

    [CIR] Change the type order on global constants
    Add one more test while here.

--- mlir/lib/Dialect/CIR/IR/CIRDialect.cpp
+++ mlir/lib/Dialect/CIR/IR/CIRDialect.cpp
@@ -88,6 +88,11 @@ static LogicalResult checkConstantTypes(mlir::Operation *op, mlir::Type opType,
     return success();
   }
 
+  if (mlir::isa<mlir::cir::CstArrayAttr>(attrType)) {
+    // CstArrayAttr is already verified to bing with cir.array type.
+    return success();
+  }
+
   assert(mlir::isa<TypedAttr>(attrType) && "What else could we be looking at here?");
   return op->emitOpError("cannot have value of type ")
          << mlir::cast<TypedAttr>(attrType).getType();
@@ -101,16 +106,9 @@ LogicalResult ConstantOp::verify() {
 }
 
 static ParseResult parseConstantValue(OpAsmParser &parser,
-                                      mlir::Attribute &valueAttr,
-                                      mlir::Type ty = {}) {
-  if (succeeded(parser.parseOptionalKeyword("nullptr"))) {
-    valueAttr = UnitAttr::get(parser.getContext());
-    return success();
-  }
-
+                                      mlir::Attribute &valueAttr) {
   NamedAttrList attr;
-
-  if (parser.parseAttribute(valueAttr, ty, "value", attr).failed()) {
+  if (parser.parseAttribute(valueAttr, "value", attr).failed()) {
     return parser.emitError(parser.getCurrentLocation(),
                             "expected constant attribute to match type");
   }
@@ -120,12 +118,8 @@ static ParseResult parseConstantValue(OpAsmParser &parser,
 
 // FIXME: create a CIRCstAttr and hide this away for both global
 // initialization and cir.cst operation.
-static void printConstant(OpAsmPrinter &p, Attribute value,
-                          bool omitType = false) {
-  if (omitType)
-    p.printAttributeWithoutType(value);
-  else
-    p.printAttribute(value);
+static void printConstant(OpAsmPrinter &p, Attribute value) {
+  p.printAttribute(value);
 }
 
 static void printConstantValue(OpAsmPrinter &p, cir::ConstantOp op,
@@ -893,10 +887,12 @@ LogicalResult LoopOp::verify() {
 static void printGlobalOpTypeAndInitialValue(OpAsmPrinter &p, GlobalOp op,
                                              TypeAttr type,
                                              Attribute initAttr) {
-  p << type;
   if (!op.isDeclaration()) {
-    p << " = ";
-    printConstant(p, initAttr, /*omitType=*/true);
+    p << "= ";
+    // This also prints the type...
+    printConstant(p, initAttr);
+  } else {
+    p << type;
   }
 }
 
@@ -904,15 +900,26 @@ static ParseResult
 parseGlobalOpTypeAndInitialValue(OpAsmParser &parser, TypeAttr &typeAttr,
                                  Attribute &initialValueAttr) {
   Type type;
-  if (parser.parseType(type))
-    return failure();
-  typeAttr = TypeAttr::get(type);
-
-  if (parser.parseOptionalEqual().failed())
+  if (parser.parseOptionalEqual().failed()) {
+    // Absence of equal means a declaration, so we need to parse the type.
+    //  cir.global @a : i32
+    if (parser.parseColonType(type))
+      return failure();
+    typeAttr = TypeAttr::get(type);
     return success();
+  }
 
-  if (parseConstantValue(parser, initialValueAttr, type).failed())
+  // Parse constant with initializer, examples:
+  //  cir.global @y = 3.400000e+00 : f32
+  //  cir.global @rgb  = #cir.cst_array<[...] : !cir.array<i8 x 3>>
+  Attribute attr;
+  if (parseConstantValue(parser, attr).failed())
     return failure();
+
+  assert(mlir::isa<mlir::TypedAttr>(attr) &&
+         "Non-typed attrs shouldn't appear here.");
+  initialValueAttr = mlir::cast<mlir::TypedAttr>(attr);
+  typeAttr = TypeAttr::get(mlir::cast<mlir::TypedAttr>(attr).getType());
 
   return success();
 }
@@ -960,6 +967,33 @@ mlir::OpTrait::impl::verifySameFirstOperandAndResultType(Operation *op) {
            << "requires the same type for first operand and result";
 
   return success();
+}
+
+//===----------------------------------------------------------------------===//
+// CIR attributes
+//===----------------------------------------------------------------------===//
+
+LogicalResult mlir::cir::CstArrayAttr::verify(
+    ::llvm::function_ref<::mlir::InFlightDiagnostic()> emitError,
+    ::mlir::Type type, ArrayAttr value) {
+  // Make sure both number of elements and subelement types match type.
+  mlir::cir::ArrayType at = mlir::cast<mlir::cir::ArrayType>(type);
+  if (at.getSize() != value.size())
+    return emitError() << "cst array size should match type size";
+  LogicalResult eltTypeCheck = success();
+  value.walkImmediateSubElements(
+      [&](Attribute attr) {
+        // Once we find a mismatch, stop there.
+        if (eltTypeCheck.failed())
+          return;
+        auto typedAttr = mlir::dyn_cast<TypedAttr>(attr);
+        if (!typedAttr || typedAttr.getType() != at.getEltType()) {
+          eltTypeCheck = failure();
+          emitError() << "cst array element should match array element type";
+        }
+      },
+      [&](Type type) {});
+  return eltTypeCheck;
 }
 
 //===----------------------------------------------------------------------===//
