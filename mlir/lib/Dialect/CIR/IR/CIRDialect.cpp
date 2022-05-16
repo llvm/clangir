43d32c4e9d5edd8ca707ba50b69a00523cab35ea
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2022-05-16 15:37:19 -0700

    [CIR] Globals: Allow building constant globals and fix type printing for uninitialized

--- mlir/lib/Dialect/CIR/IR/CIRDialect.cpp
+++ mlir/lib/Dialect/CIR/IR/CIRDialect.cpp
@@ -953,7 +953,7 @@ static void printGlobalOpTypeAndInitialValue(OpAsmPrinter &p, GlobalOp op,
     // This also prints the type...
     printConstant(p, initAttr.isa<UnitAttr>(), initAttr);
   } else {
-    p << type;
+    p << ": " << type;
   }
 }
 
@@ -999,11 +999,13 @@ static LogicalResult verify(GlobalOp op) {
 }
 
 void GlobalOp::build(OpBuilder &odsBuilder, OperationState &odsState,
-                     StringRef sym_name, Type sym_type) {
+                     StringRef sym_name, Type sym_type, bool isConstant) {
   odsState.addAttribute(sym_nameAttrName(odsState.name),
                         odsBuilder.getStringAttr(sym_name));
   odsState.addAttribute(sym_typeAttrName(odsState.name),
                         ::mlir::TypeAttr::get(sym_type));
+  if (isConstant)
+    odsState.addAttribute("constant", odsBuilder.getUnitAttr());
 }
 
 //===----------------------------------------------------------------------===//
