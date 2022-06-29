72a8dde10c3c208fc01e941fe2f4e9795bc46743
Jingsong Shang <jshang@fb.com>
2022-06-29 16:27:31 -0700

    [CIR][Clang] Add dummy cir-tidy executable
    Summary: Add an initial source for cir-tidy and an initial test.
    
    Test Plan:
    Test with command 
    ``` ninja check-clang-tools ``` 
    and no error expected.
    
    Reviewers: lanza, ivanmurashko, brunolopes
    
    Reviewed By: brunolopes
    
    Subscribers: lanza, ivanmurashko, brunolopes
    
    Differential Revision: https://phabricator.intern.facebook.com/D37538741
    
    Tasks: T124723259
    
    Tags: oncall, oncallteam-clangir

---
+++ clang-tools-extra/clang-tidy/cir/CIRTidyMain.cpp
@@ -0,0 +1,23 @@
+//===--- tools/extra/clang-tidy/cir/CIRTidyToolMain.cpp - cir tidy tool ---===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+///
+///  \file This file implements a cir-tidy tool.
+///
+///  This tool uses the Clang Tooling infrastructure, see
+///    http://clang.llvm.org/docs/HowToSetupToolingForLLVM.html
+///  for details on setting it up with LLVM source tree.
+///
+//===----------------------------------------------------------------------===//
+
+#include "CIRTidyMain.h"
+
+namespace cir {
+namespace tidy {
+int CIRTidyMain(int argc, const char **argv) { return 0; }
+} // namespace tidy
+} // namespace cir
