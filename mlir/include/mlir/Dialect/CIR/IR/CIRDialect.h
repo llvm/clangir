24d4a46fa3130c179e7feb43cc995a8fd46e9443
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2022-07-20 15:50:43 -0700

    [CIR][CodeGen] Add helpers for gathering linkage information
    While here update previously unimplemented feature guarding call sites
    and add change `getCIRGenToUse` to return the appropriated linkage.

--- mlir/include/mlir/Dialect/CIR/IR/CIRDialect.h
+++ mlir/include/mlir/Dialect/CIR/IR/CIRDialect.h
@@ -31,7 +31,7 @@ using FuncOp = func::FuncOp;
 } // namespace mlir
 
 #include "mlir/Dialect/CIR/IR/CIROpsDialect.h.inc"
-#include "mlir/Dialect/CIR/IR/CIROpsEnums.h.inc"
+#include "mlir/Dialect/CIR/IR/CIROpsEnums.h"
 #include "mlir/Dialect/CIR/IR/CIROpsStructs.h.inc"
 #include "mlir/Dialect/CIR/IR/CIRTypes.h"
 
