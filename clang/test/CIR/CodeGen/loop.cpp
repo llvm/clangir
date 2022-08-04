85f312fe4dd08b258793ff58f9ec0cc38cff978c
Nathan Lanza <nathan@lanza.io>
2022-08-03 18:43:40 -0700

    Revert "[DO NOT LAND] XFAIL all the tests broken by verifiers"
    This reverts commit 8ec8c3e88a6533ea8dc488fa3cade144a5521b37.

--- clang/test/CIR/CodeGen/loop.cpp
+++ clang/test/CIR/CodeGen/loop.cpp
@@ -1,6 +1,5 @@
 // RUN: %clang_cc1 -std=c++17 -triple x86_64-unknown-linux-gnu -fenable-clangir -emit-cir %s -o %t.cir
 // RUN: FileCheck --input-file=%t.cir %s
-// XFAIL: *
 
 void l0() {
   for (;;) {
