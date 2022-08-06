f9a85bc86570ba623dc92f867dceb5e5704e1ecc
Nathan Lanza <nathan@lanza.io>
2022-08-05 20:27:46 -0700

    Revert "[mlir][ods] Remove StructAttr"
    This reverts commit d883a02a7c2bb89000d0685749f062c9206ac40c.

--- mlir/test/lib/Dialect/Test/TestDialect.cpp
+++ mlir/test/lib/Dialect/Test/TestDialect.cpp
@@ -1590,6 +1590,7 @@ void TestReflectBoundsOp::inferResultRanges(
 
 #include "TestOpEnums.cpp.inc"
 #include "TestOpInterfaces.cpp.inc"
+#include "TestOpStructs.cpp.inc"
 #include "TestTypeInterfaces.cpp.inc"
 
 #define GET_OP_CLASSES
