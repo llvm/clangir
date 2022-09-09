c79c51bd6216a0cb16398fdab087053cd953d949
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2022-09-08 19:54:04 -0700

    [CIR][CodeGen] Organize CIR directory
    Before
    
    clang/lib/CIR/<codegen files>
    clang/lib/CIRFrontendAction
    
    After
    
    clang/lib/CIR/CodeGen/<codegen files>
    clang/lib/CIR/FrontendAction

--- clang/lib/CIR/CodeGen/CIRGenerator.cpp
+++ clang/lib/CIR/CodeGen/CIRGenerator.cpp
@@ -20,6 +20,7 @@
 #include "clang/AST/ASTContext.h"
 #include "clang/AST/Decl.h"
 #include "clang/CIR/CIRGenerator.h"
+#include "mlir/Dialect/CIR/IR/CIRDialect.h"
 
 using namespace cir;
 using namespace clang;
