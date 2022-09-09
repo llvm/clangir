4c295d766cf20e11be85a3d2ba6adc717ed48c84
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2022-09-08 20:05:23 -0700

    [CIR] Move CIR dialect from mlir to clang
    This was discussed as part of the RFC and since we are going to
    require some AST presence in CIR soon, implement the dialect move
    sooner than later.

--- clang/lib/CIR/CodeGen/CIRGenerator.cpp
+++ clang/lib/CIR/CodeGen/CIRGenerator.cpp
@@ -12,7 +12,7 @@
 
 #include "CIRGenModule.h"
 
-#include "mlir/Dialect/CIR/IR/CIRDialect.h"
+#include "clang/CIR/Dialect/IR/CIRDialect.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/Dialect/MemRef/IR/MemRef.h"
 #include "mlir/IR/MLIRContext.h"
