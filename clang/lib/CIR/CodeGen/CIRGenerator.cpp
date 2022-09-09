0c816a36349e36c829f98d8736b777c947dac56b
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2022-09-08 19:54:04 -0700

    [CIR][CodeGen] Organize CIR directory
    Before
    
    clang/lib/CIR/<codegen files>
    clang/lib/CIRFrontendAction
    
    After
    
    clang/lib/CIR/CodeGen/<codegen files>
    clang/lib/CIR/FrontendAction

--- clang/lib/CIR/CodeGen/CIRGenerator.cpp
+++ clang/lib/CIR/CodeGen/CIRGenerator.cpp
@@ -12,9 +12,13 @@
 
 #include "CIRGenModule.h"
 
+#include "mlir/Dialect/CIR/IR/CIRDialect.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Dialect/MemRef/IR/MemRef.h"
 #include "mlir/IR/MLIRContext.h"
 
-#include "clang/AST/DeclGroup.h"
+#include "clang/AST/ASTContext.h"
+#include "clang/AST/Decl.h"
 #include "clang/CIR/CIRGenerator.h"
 #include "clang/CIR/Dialect/IR/CIRDialect.h"
 
@@ -24,10 +28,12 @@ using namespace clang;
 void CIRGenerator::anchor() {}
 
 CIRGenerator::CIRGenerator(clang::DiagnosticsEngine &diags,
-                           llvm::IntrusiveRefCntPtr<llvm::vfs::FileSystem> vfs,
-                           const CodeGenOptions &cgo)
-    : diags(diags), fs(std::move(vfs)), codeGenOpts{cgo} {}
-CIRGenerator::~CIRGenerator() = default;
+                           const CodeGenOptions &CGO)
+    : Diags(diags), codeGenOpts{CGO}, HandlingTopLevelDecls(0) {}
+CIRGenerator::~CIRGenerator() {
+  // There should normally not be any leftover inline method definitions.
+  assert(DeferredInlineMemberFuncDefs.empty() || Diags.hasErrorOccurred());
+}
 
 void CIRGenerator::Initialize(ASTContext &astCtx) {
   using namespace llvm;
@@ -35,17 +41,118 @@ void CIRGenerator::Initialize(ASTContext &astCtx) {
   this->astCtx = &astCtx;
 
   mlirCtx = std::make_unique<mlir::MLIRContext>();
-  mlirCtx->loadDialect<cir::CIRDialect>();
-  cgm = std::make_unique<clang::CIRGen::CIRGenModule>(*mlirCtx.get(), astCtx,
-                                                      codeGenOpts, diags);
+  mlirCtx->getOrLoadDialect<mlir::func::FuncDialect>();
+  mlirCtx->getOrLoadDialect<mlir::cir::CIRDialect>();
+  mlirCtx->getOrLoadDialect<mlir::func::FuncDialect>();
+  mlirCtx->getOrLoadDialect<mlir::memref::MemRefDialect>();
+  CGM = std::make_unique<CIRGenModule>(*mlirCtx.get(), astCtx, codeGenOpts,
+                                       Diags);
 }
 
-mlir::ModuleOp CIRGenerator::getModule() const { return cgm->getModule(); }
+bool CIRGenerator::verifyModule() { return CGM->verifyModule(); }
 
-bool CIRGenerator::HandleTopLevelDecl(DeclGroupRef group) {
+bool CIRGenerator::EmitFunction(const FunctionDecl *FD) {
+  llvm_unreachable("NYI");
+}
 
-  for (Decl *decl : group)
-    cgm->emitTopLevelDecl(decl);
+mlir::ModuleOp CIRGenerator::getModule() { return CGM->getModule(); }
+
+bool CIRGenerator::HandleTopLevelDecl(DeclGroupRef D) {
+  if (Diags.hasErrorOccurred())
+    return true;
+
+  HandlingTopLevelDeclRAII HandlingDecl(*this);
+
+  for (DeclGroupRef::iterator I = D.begin(), E = D.end(); I != E; ++I) {
+    CGM->buildTopLevelDecl(*I);
+  }
 
   return true;
+}
+
+void CIRGenerator::HandleTranslationUnit(ASTContext &C) {
+  // Release the Builder when there is no error.
+  if (!Diags.hasErrorOccurred() && CGM)
+    CGM->Release();
+
+  // If there are errors before or when releasing the CGM, reset the module to
+  // stop here before invoking the backend.
+  if (Diags.hasErrorOccurred()) {
+    if (CGM)
+      // TODO: CGM->clear();
+      // TODO: M.reset();
+      return;
+  }
+}
+
+void CIRGenerator::HandleInlineFunctionDefinition(FunctionDecl *D) {
+  if (Diags.hasErrorOccurred())
+    return;
+
+  assert(D->doesThisDeclarationHaveABody());
+
+  // We may want to emit this definition. However, that decision might be
+  // based on computing the linkage, and we have to defer that in case we are
+  // inside of something that will chagne the method's final linkage, e.g.
+  //   typedef struct {
+  //     void bar();
+  //     void foo() { bar(); }
+  //   } A;
+  DeferredInlineMemberFuncDefs.push_back(D);
+
+  // Provide some coverage mapping even for methods that aren't emitted.
+  // Don't do this for templated classes though, as they may not be
+  // instantiable.
+  if (!D->getLexicalDeclContext()->isDependentContext())
+    CGM->AddDeferredUnusedCoverageMapping(D);
+}
+
+void CIRGenerator::buildDeferredDecls() {
+  if (DeferredInlineMemberFuncDefs.empty())
+    return;
+
+  // Emit any deferred inline method definitions. Note that more deferred
+  // methods may be added during this loop, since ASTConsumer callbacks can be
+  // invoked if AST inspection results in declarations being added.
+  HandlingTopLevelDeclRAII HandlingDecls(*this);
+  for (unsigned I = 0; I != DeferredInlineMemberFuncDefs.size(); ++I)
+    CGM->buildTopLevelDecl(DeferredInlineMemberFuncDefs[I]);
+  DeferredInlineMemberFuncDefs.clear();
+}
+
+/// HandleTagDeclDefinition - This callback is invoked each time a TagDecl to
+/// (e.g. struct, union, enum, class) is completed. This allows the client hack
+/// on the type, which can occur at any point in the file (because these can be
+/// defined in declspecs).
+void CIRGenerator::HandleTagDeclDefinition(TagDecl *D) {
+  if (Diags.hasErrorOccurred())
+    return;
+
+  // Don't allow re-entrant calls to CIRGen triggered by PCH deserialization to
+  // emit deferred decls.
+  HandlingTopLevelDeclRAII HandlingDecl(*this, /*EmitDeferred=*/false);
+
+  CGM->UpdateCompletedType(D);
+
+  // For MSVC compatibility, treat declarations of static data members with
+  // inline initializers as definitions.
+  if (astCtx->getTargetInfo().getCXXABI().isMicrosoft()) {
+    llvm_unreachable("NYI");
+  }
+  // For OpenMP emit declare reduction functions, if required.
+  if (astCtx->getLangOpts().OpenMP) {
+    llvm_unreachable("NYI");
+  }
+}
+
+void CIRGenerator::HandleTagDeclRequiredDefinition(const TagDecl *D) {
+  if (Diags.hasErrorOccurred())
+    return;
+
+  // Don't allow re-entrant calls to CIRGen triggered by PCH deserialization to
+  // emit deferred decls.
+  HandlingTopLevelDeclRAII HandlingDecl(*this, /*EmitDeferred=*/false);
+
+  if (CGM->getModuleDebugInfo())
+    llvm_unreachable("NYI");
 }
