e2df56ccc2973ad33d880bceda540c8a340e3178
Nathan Lanza <nathan@lanza.io>
2022-09-22 13:01:13 -0700

    [CIR] Remove CIRBasedWarnigns from Sema
    This is not being used and makes Sema depends on CIR/CodeGen, which
    feels quite odd. When we get real about AnalyzerBasedWarnings we can
    introduce the functionality as additional passes in the pipeline via our
    custom frontend actions (which did not exist when this was first done).

--- clang/include/clang/Sema/Sema.h
+++ clang/include/clang/Sema/Sema.h
@@ -46,7 +46,6 @@
 #include "clang/Basic/TemplateKinds.h"
 #include "clang/Basic/TypeTraits.h"
 #include "clang/Sema/AnalysisBasedWarnings.h"
-#include "clang/Sema/CIRBasedWarnings.h"
 #include "clang/Sema/CleanupInfo.h"
 #include "clang/Sema/DeclSpec.h"
 #include "clang/Sema/ExternalSemaSource.h"
