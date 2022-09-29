5f19dddc786862fe4b77262ef58eb6e906153147
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2022-09-29 10:35:13 -0700

    [CIR][Driver] Fix silly Lang parsing mistake

--- clang/lib/Frontend/CompilerInvocation.cpp
+++ clang/lib/Frontend/CompilerInvocation.cpp
@@ -2690,6 +2690,7 @@ static void GenerateFrontendArgs(const FrontendOptions &Opts,
       break;
     case Language::HLSL:
       Lang = "hlsl";
+      break;
     case Language::CIR:
       Lang = "cir";
       break;
