fa4145acb68fd986468706ae73ab4188910e55fa
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2022-09-29 10:36:05 -0700

    [CIR][Driver] Add -fclangir-lifetime-check options
    - Add cc1 options to the driver
    - Hook it up as part of CIRGenAction
    - Teach runCIRToCIRPasses how to run lifetime pass
    - Sanitize pass options, also handle error handling for malformed ones
    - Add -clangir-verify-diagnostics to test source code when using clang
    - Update lifetime checks to also test clang for diagnostic

--- clang/include/clang/Frontend/FrontendOptions.h
+++ clang/include/clang/Frontend/FrontendOptions.h
@@ -419,6 +419,12 @@ public:
   /// Disable Clang IR (CIR) verifier
   unsigned ClangIRDisableCIRVerifier : 1;
 
+  /// Enable diagnostic verification for CIR
+  unsigned ClangIRVerifyDiags : 1;
+
+  // Enable Clang IR based lifetime check
+  unsigned ClangIRLifetimeCheck : 1;
+
   CodeCompleteOptions CodeCompleteOpts;
 
   /// Specifies the output format of the AST.
@@ -489,6 +495,8 @@ public:
 
   std::string MTMigrateDir;
   std::string ARCMTMigrateReportOut;
+
+  std::string ClangIRLifetimeCheckOpts;
 
   /// The input kind, either specified via -x argument or deduced from the input
   /// file name.
@@ -602,7 +610,8 @@ public:
         EmitSymbolGraphSymbolLabelsForTesting(false),
         EmitPrettySymbolGraphs(false), GenReducedBMI(false),
         UseClangIRPipeline(false), ClangIRDisablePasses(false),
-        ClangIRDisableCIRVerifier(false), TimeTraceGranularity(500) {}
+        ClangIRDisableCIRVerifier(false), ClangIRLifetimeCheck(false),
+        TimeTraceGranularity(500) {}
 
   /// getInputKindForExtension - Return the appropriate input kind for a file
   /// extension. For example, "c" would return Language::C.
