84b4a62d1f73ebf3dca17e688f27c0d57f12aebb
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2022-11-30 14:39:09 -0800

    [CIR][CIRGen][NFC] Add more asserts, initialize some data and add missing API

--- clang/lib/CIR/CodeGen/Address.h
+++ clang/lib/CIR/CodeGen/Address.h
@@ -35,10 +35,12 @@ public:
   Address(mlir::Value pointer, mlir::Type elementType,
           clang::CharUnits alignment)
       : Pointer(pointer), ElementType(elementType), Alignment(alignment) {
+    auto ptrTy = llvm::dyn_cast<mlir::cir::PointerType>(pointer.getType());
+    assert(ptrTy && "Expected cir.ptr type");
+
     assert(pointer != nullptr && "Pointer cannot be null");
     assert(elementType != nullptr && "Pointer cannot be null");
-    assert(llvm::cast<mlir::cir::PointerType>(pointer.getType()).getPointee() ==
-               ElementType &&
+    assert(ptrTy.getPointee() == ElementType &&
            "Incorrect pointer element type");
     assert(!alignment.isZero() && "Alignment cannot be zero");
   }
