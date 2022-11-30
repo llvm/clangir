54a03b73d406a20e42d6e112c0b2b09aeb8f3227
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2022-11-30 15:41:37 -0800

    [CIR][CIRGen][NFC] Augment builder with helper for element bitcast

--- clang/lib/CIR/CodeGen/CIRGenExprScalar.cpp
+++ clang/lib/CIR/CodeGen/CIRGenExprScalar.cpp
@@ -1189,7 +1189,7 @@ mlir::Value ScalarExprEmitter::buildScalarCast(
   auto FloatSrcTy = SrcElementTy.cast<mlir::FloatType>();
   if (FloatDstTy.getWidth() < FloatSrcTy.getWidth())
     llvm_unreachable("truncation NYI");
-  return Builder.CreateFPExt(Src, DstTy);
+  return Builder.createFPExt(Src, DstTy);
 }
 
 LValue
