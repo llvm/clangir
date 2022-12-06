348d6683b06106c56f122184715a83c2651ae3a9
Nathan Lanza <nathanlanza@gmail.com>
2022-12-06 00:10:39 -0500

    [CIR][Lowering] Add a flag to opt into direct lowering
    This diff adds a flag that allows the user to opt into direct lowering
    from CIR to LLVMIR and much of the infrastructure necessary to make that
    possible.

--- clang/lib/CIR/Lowering/DirectToLLVM/LowerToLLVM.cpp
+++ clang/lib/CIR/Lowering/DirectToLLVM/LowerToLLVM.cpp
@@ -537,13 +537,17 @@ void ConvertCIRToLLVMPass::runOnOperation() {
     signalPassFailure();
 }
 
+std::unique_ptr<mlir::Pass> createConvertDirectCIRToLLVMPass() {
+  return std::make_unique<ConvertCIRToLLVMPass>();
+}
+
 std::unique_ptr<llvm::Module>
-lowerFromCIRToMLIRToLLVMIR(mlir::ModuleOp theModule,
-                           std::unique_ptr<mlir::MLIRContext> mlirCtx,
-                           LLVMContext &llvmCtx) {
+lowerDirectlyFromCIRToLLVMIR(mlir::ModuleOp theModule,
+                             std::unique_ptr<mlir::MLIRContext> mlirCtx,
+                             LLVMContext &llvmCtx) {
   mlir::PassManager pm(mlirCtx.get());
 
-  pm.addPass(createConvertCIRToLLVMPass());
+  pm.addPass(createConvertDirectCIRToLLVMPass());
 
   auto result = !mlir::failed(pm.run(theModule));
   if (!result)
@@ -564,27 +568,4 @@ lowerFromCIRToMLIRToLLVMIR(mlir::ModuleOp theModule,
 
   return llvmModule;
 }
-
-std::unique_ptr<mlir::Pass> createConvertCIRToLLVMPass() {
-  return std::make_unique<ConvertCIRToLLVMPass>();
-}
-
-mlir::ModuleOp lowerFromCIRToMLIR(mlir::ModuleOp theModule,
-                                  mlir::MLIRContext *mlirCtx) {
-  mlir::PassManager pm(mlirCtx);
-
-  pm.addPass(createConvertCIRToLLVMPass());
-
-  auto result = !mlir::failed(pm.run(theModule));
-  if (!result)
-    report_fatal_error(
-        "The pass manager failed to lower CIR to LLVMIR dialect!");
-
-  // Now that we ran all the lowering passes, verify the final output.
-  if (theModule.verify().failed())
-    report_fatal_error("Verification of the final LLVMIR dialect failed!");
-
-  return theModule;
-}
-
 } // namespace cir
