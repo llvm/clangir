393464f067a5b0071e69755978f23a25da3b3c66
Nathan Lanza <nathanlanza@gmail.com>
2023-03-07 16:21:16 -0500

    [CIR][NFC] Change cir.cst -> cir.const

--- clang/test/CIR/CodeGen/basic.c
+++ clang/test/CIR/CodeGen/basic.c
@@ -8,7 +8,7 @@ int foo(int i) {
   return i;
 }
 
-//      CHECK: module attributes {cir.sob = #cir.signed_overflow_behavior<undefined>} {
+//      CHECK: module attributes {
 // CHECK-NEXT: cir.func @foo(%arg0: i32 loc({{.*}})) -> i32 {
 // CHECK-NEXT: %0 = cir.alloca i32, cir.ptr <i32>, ["i", init] {alignment = 4 : i64}
 // CHECK-NEXT: %1 = cir.alloca i32, cir.ptr <i32>, ["__retval"] {alignment = 4 : i64}
@@ -23,7 +23,7 @@ int f2() { return 3; }
 
 // CHECK: cir.func @f2() -> i32 {
 // CHECK-NEXT: %0 = cir.alloca i32, cir.ptr <i32>, ["__retval"] {alignment = 4 : i64}
-// CHECK-NEXT: %1 = cir.cst(3 : i32) : i32
+// CHECK-NEXT: %1 = cir.const(3 : i32) : i32
 // CHECK-NEXT: cir.store %1, %0 : i32, cir.ptr <i32>
 // CHECK-NEXT: %2 = cir.load %0 : cir.ptr <i32>, i32
 // CHECK-NEXT: cir.return %2 : i32
@@ -36,7 +36,7 @@ int f3() {
 // CHECK: cir.func @f3() -> i32 {
 // CHECK-NEXT: %0 = cir.alloca i32, cir.ptr <i32>, ["__retval"] {alignment = 4 : i64}
 // CHECK-NEXT: %1 = cir.alloca i32, cir.ptr <i32>, ["i", init] {alignment = 4 : i64}
-// CHECK-NEXT: %2 = cir.cst(3 : i32) : i32
+// CHECK-NEXT: %2 = cir.const(3 : i32) : i32
 // CHECK-NEXT: cir.store %2, %1 : i32, cir.ptr <i32>
 // CHECK-NEXT: %3 = cir.load %1 : cir.ptr <i32>, i32
 // CHECK-NEXT: cir.store %3, %0 : i32, cir.ptr <i32>
