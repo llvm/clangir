c1b68fcae9229c0509aa24b604a5e4dec921a207
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2023-04-19 20:24:08 -0700

    [CIR] Improve parsing and naming for #cir.<Decl>.ast nodes

--- clang/lib/CIR/Dialect/IR/CIRDialect.cpp
+++ clang/lib/CIR/Dialect/IR/CIRDialect.cpp
@@ -1746,7 +1746,7 @@ void SignedOverflowBehaviorAttr::print(::mlir::AsmPrinter &printer) const {
                                              ::mlir::Type type) {
   // We cannot really parse anything AST related at this point
   // since we have no serialization/JSON story.
-  return mlir::Attribute();
+  return ASTFunctionDeclAttr::get(parser.getContext(), nullptr);
 }
 
 void ASTFunctionDeclAttr::print(::mlir::AsmPrinter &printer) const {
@@ -1756,10 +1756,6 @@ void ASTFunctionDeclAttr::print(::mlir::AsmPrinter &printer) const {
 LogicalResult ASTFunctionDeclAttr::verify(
     ::llvm::function_ref<::mlir::InFlightDiagnostic()> emitError,
     const ::clang::FunctionDecl *decl) {
-  if (!decl) {
-    emitError() << "expected non-null AST declaration";
-    return failure();
-  }
   return success();
 }
 
@@ -1767,7 +1763,7 @@ LogicalResult ASTFunctionDeclAttr::verify(
                                         ::mlir::Type type) {
   // We cannot really parse anything AST related at this point
   // since we have no serialization/JSON story.
-  return mlir::Attribute();
+  return ASTVarDeclAttr::get(parser.getContext(), nullptr);
 }
 
 void ASTVarDeclAttr::print(::mlir::AsmPrinter &printer) const {
@@ -1777,10 +1773,6 @@ void ASTVarDeclAttr::print(::mlir::AsmPrinter &printer) const {
 LogicalResult ASTVarDeclAttr::verify(
     ::llvm::function_ref<::mlir::InFlightDiagnostic()> emitError,
     const ::clang::VarDecl *decl) {
-  if (!decl) {
-    emitError() << "expected non-null AST declaration";
-    return failure();
-  }
   return success();
 }
 
@@ -1788,7 +1780,7 @@ LogicalResult ASTVarDeclAttr::verify(
                                            ::mlir::Type type) {
   // We cannot really parse anything AST related at this point
   // since we have no serialization/JSON story.
-  return mlir::Attribute();
+  return ASTRecordDeclAttr::get(parser.getContext(), nullptr);
 }
 
 void ASTRecordDeclAttr::print(::mlir::AsmPrinter &printer) const {
@@ -1798,10 +1790,6 @@ void ASTRecordDeclAttr::print(::mlir::AsmPrinter &printer) const {
 LogicalResult ASTRecordDeclAttr::verify(
     ::llvm::function_ref<::mlir::InFlightDiagnostic()> emitError,
     const ::clang::RecordDecl *decl) {
-  if (!decl) {
-    emitError() << "expected non-null AST declaration";
-    return failure();
-  }
   return success();
 }
 
