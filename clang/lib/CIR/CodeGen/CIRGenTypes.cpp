c94ca305d53205c1ecdd65ad95930b8924146675
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2023-04-27 17:28:14 -0700

    [CIR][CIRGen][NFC] Add a CIRGenTypeCache and use wherever possible

--- clang/lib/CIR/CodeGen/CIRGenTypes.cpp
+++ clang/lib/CIR/CodeGen/CIRGenTypes.cpp
@@ -338,7 +338,7 @@ mlir::Type CIRGenTypes::ConvertType(QualType T) {
       llvm_unreachable("NYI");
     case BuiltinType::Void:
       // TODO(cir): how should we model this?
-      ResultType = ::mlir::IntegerType::get(Builder.getContext(), 8);
+      ResultType = CGM.VoidTy;
       break;
 
     case BuiltinType::ObjCId:
@@ -413,10 +413,10 @@ mlir::Type CIRGenTypes::ConvertType(QualType T) {
       ResultType = Builder.getBF16Type();
       break;
     case BuiltinType::Float:
-      ResultType = Builder.getF32Type();
+      ResultType = CGM.FloatTy;
       break;
     case BuiltinType::Double:
-      ResultType = Builder.getF64Type();
+      ResultType = CGM.DoubleTy;
       break;
     case BuiltinType::LongDouble:
     case BuiltinType::Float128:
