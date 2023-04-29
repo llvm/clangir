d4dfc71d86295cbd882cc029051961b5eb480e55
Nathan Lanza <nathanlanza@gmail.com>
2023-04-29 00:28:50 -0400

    [CIR][Dialect] Add the datalayout interface to cir.ArrayType and PointerType
    Add the interface and the boilerplate methods to propagate the elements. This is
    the same behavior as we see from llvm's datalayout types.

--- clang/lib/CIR/Dialect/IR/CIRTypes.cpp
+++ clang/lib/CIR/Dialect/IR/CIRTypes.cpp
@@ -64,6 +64,18 @@ void PointerType::print(mlir::AsmPrinter &printer) const {
   printer << '>';
 }
 
+llvm::TypeSize
+PointerType::getTypeSizeInBits(const ::mlir::DataLayout &dataLayout,
+                               ::mlir::DataLayoutEntryListRef params) const {
+  llvm_unreachable("NYI");
+}
+
+uint64_t
+PointerType::getABIAlignment(const ::mlir::DataLayout &dataLayout,
+                             ::mlir::DataLayoutEntryListRef params) const {
+  llvm_unreachable("NYI");
+}
+
 Type BoolType::parse(mlir::AsmParser &parser) {
   return get(parser.getContext());
 }
@@ -162,6 +174,18 @@ void ArrayType::print(mlir::AsmPrinter &printer) const {
   printer.printType(getEltType());
   printer << " x " << getSize();
   printer << '>';
+}
+
+llvm::TypeSize
+ArrayType::getTypeSizeInBits(const ::mlir::DataLayout &dataLayout,
+                             ::mlir::DataLayoutEntryListRef params) const {
+  return dataLayout.getTypeSizeInBits(getEltType());
+}
+
+uint64_t
+ArrayType::getABIAlignment(const ::mlir::DataLayout &dataLayout,
+                           ::mlir::DataLayoutEntryListRef params) const {
+  return dataLayout.getTypeABIAlignment(getEltType());
 }
 
 //===----------------------------------------------------------------------===//
