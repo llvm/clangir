2c434d3863212a1dd9ec2d89bf9b9da5c0ab3c87
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2023-05-11 15:38:13 -0700

    [CIR][CIRGen] Dtor/Ctor: implement alias emission

--- clang/lib/CIR/Dialect/IR/CIRDialect.cpp
+++ clang/lib/CIR/Dialect/IR/CIRDialect.cpp
@@ -1293,6 +1293,7 @@ ParseResult cir::FuncOp::parse(OpAsmParser &parser, OperationState &state) {
   auto builtinNameAttr = getBuiltinAttrName(state.name);
   auto coroutineNameAttr = getCoroutineAttrName(state.name);
   auto lambdaNameAttr = getLambdaAttrName(state.name);
+  auto visNameAttr = getSymVisibilityAttrName(state.name);
   if (::mlir::succeeded(parser.parseOptionalKeyword(builtinNameAttr.strref())))
     state.addAttribute(builtinNameAttr, parser.getBuilder().getUnitAttr());
   if (::mlir::succeeded(
@@ -1307,6 +1308,13 @@ ParseResult cir::FuncOp::parse(OpAsmParser &parser, OperationState &state) {
                          parser.getContext(),
                          parseOptionalCIRKeyword<GlobalLinkageKind>(
                              parser, GlobalLinkageKind::ExternalLinkage)));
+
+  ::llvm::StringRef visAttrStr;
+  if (parser.parseOptionalKeyword(&visAttrStr, {"private", "public", "nested"})
+          .succeeded()) {
+    state.addAttribute(visNameAttr,
+                       parser.getBuilder().getStringAttr(visAttrStr));
+  }
 
   StringAttr nameAttr;
   SmallVector<OpAsmParser::Argument, 8> arguments;
@@ -1404,6 +1412,7 @@ bool cir::FuncOp::isDeclaration() {
 
 void cir::FuncOp::print(OpAsmPrinter &p) {
   p << ' ';
+
   if (getBuiltin())
     p << "builtin ";
 
@@ -1416,6 +1425,10 @@ void cir::FuncOp::print(OpAsmPrinter &p) {
   if (getLinkage() != GlobalLinkageKind::ExternalLinkage)
     p << stringifyGlobalLinkageKind(getLinkage()) << ' ';
 
+  auto vis = getVisibility();
+  if (vis != mlir::SymbolTable::Visibility::Public)
+    p << vis << " ";
+
   // Print function name, signature, and control.
   p.printSymbolName(getSymName());
   auto fnType = getFunctionType();
@@ -1423,10 +1436,15 @@ void cir::FuncOp::print(OpAsmPrinter &p) {
                                                   /*isVariadic=*/false,
                                                   fnType.getResults());
   function_interface_impl::printFunctionAttributes(
-      p, *this, {getFunctionTypeAttrName(), getLinkageAttrName()});
+      p, *this,
+      {getSymVisibilityAttrName(), getAliaseeAttrName(),
+       getFunctionTypeAttrName(), getLinkageAttrName(), getBuiltinAttrName()});
 
-  if (auto aliaseeName = getAliasee())
+  if (auto aliaseeName = getAliasee()) {
+    p << " alias(";
     p.printSymbolName(*aliaseeName);
+    p << ")";
+  }
 
   // Print the body if this is not an external function.
   Region &body = getOperation()->getRegion(0);
