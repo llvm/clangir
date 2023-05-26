adee2ac5d9370d6ffc13de68114201914627f3ca
Hongtao Yu <hoy@fb.com>
2023-05-26 11:04:06 -0700

    [CIR] Enable -fclangir-direct-lowering by default
    Summary:
    This change turns on direct lowering from CIR to LLVM IR by default. A missing API call to register the builtin LLVM dialtect is added, otherwise an error `cannot be converted to LLVM IR: missing `LLVMTranslationDialectInterface` registration for dialect for op: builtin.module` is hit during llvm IR conversion.  A test is also tweaked.

--- clang/test/CIR/driver.c
+++ clang/test/CIR/driver.c
@@ -1,7 +1,11 @@
-// RUN: %clang -target x86_64-unknown-linux-gnu -fclangir-enable -S -emit-cir %s -o %t.cir
-// RUN: FileCheck --input-file=%t.cir %s -check-prefix=CIR
-// RUN: %clang -target x86_64-unknown-linux-gnu -fclangir-enable -S -emit-llvm %s -o %t.ll
-// RUN: FileCheck --input-file=%t.ll %s -check-prefix=LLVM
+// RUN: %clang -target x86_64-unknown-linux-gnu -fclangir-enable -fclangir-direct-lowering -S -emit-cir %s -o %t1.cir
+// RUN: FileCheck --input-file=%t1.cir %s -check-prefix=CIR
+// RUN: %clang -target x86_64-unknown-linux-gnu -fclangir-enable -fno-clangir-direct-lowering -S -emit-cir %s -o %t2.cir
+// RUN: FileCheck --input-file=%t2.cir %s -check-prefix=CIR
+// RUN: %clang -target x86_64-unknown-linux-gnu -fclangir-enable -fclangir-direct-lowering -S -emit-llvm %s -o %t1.ll
+// RUN: FileCheck --input-file=%t1.ll %s -check-prefix=LLVM
+// RUN: %clang -target x86_64-unknown-linux-gnu -fclangir-enable -fno-clangir-direct-lowering -S -emit-llvm %s -o %t2.ll
+// RUN: FileCheck --input-file=%t2.ll %s -check-prefix=LLVM
 // RUN: %clang -target x86_64-unknown-linux-gnu -fclangir-enable -c %s -o %t.o
 // RUN: llvm-objdump -d %t.o | FileCheck %s -check-prefix=OBJ
 // RUN: %clang -target x86_64-unknown-linux-gnu -fclangir-enable -clangir-disable-passes -S -emit-cir %s -o %t.cir
