31bf30ec1401fab9641565fc0b72b8383fb1601c
Hongtao Yu <hoy@fb.com>
2023-06-07 00:59:29 -0700

    [CIR] Support MLIR command line arguments.
    Summary:
    
    With this change MLIR command line arguments can be passed in with -mmlir, e.g,
    
    clang -mmlir -mlir-disable-threadingd
    clang -mmlir -debug-only=mlircontext

--- clang/lib/Driver/ToolChains/Clang.cpp
+++ clang/lib/Driver/ToolChains/Clang.cpp
@@ -7687,6 +7687,11 @@ void Clang::ConstructJob(Compilation &C, const JobAction &JA,
     }
   }
 
+  for (const Arg *A : Args.filtered(options::OPT_mmlir)) {
+    A->claim();
+    A->render(Args, CmdArgs);
+  }
+
   // With -save-temps, we want to save the unoptimized bitcode output from the
   // CompileJobAction, use -disable-llvm-passes to get pristine IR generated
   // by the frontend.
