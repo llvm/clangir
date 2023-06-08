78f1a9adc577ae59ca6a75e2250a62f7fac48966
Hongtao Yu <hoy@fb.com>
2023-06-08 09:31:07 -0700

    [CIR] Fix a test failure introduced by PR #103

--- clang/test/CIR/CodeGen/mlirargs.c
+++ clang/test/CIR/CodeGen/mlirargs.c
@@ -4,7 +4,7 @@
 // RUN: %clang -### -mmlir -mlir-disable-threading %s 2>&1 | FileCheck %s --check-prefix=CC1
 
 
-// WRONG: clang (MLIR option parsing): Unknown command line argument '-mlir-disable-threadingd'.  Try: 'clang (MLIR option parsing) --help'
-// WRONG: clang (MLIR option parsing): Did you mean '--mlir-disable-threading'?
+// WRONG: ClangIR (MLIR option parsing): Unknown command line argument '-mlir-disable-threadingd'.  Try: 'ClangIR (MLIR option parsing) --help'
+// WRONG: ClangIR (MLIR option parsing): Did you mean '--mlir-disable-threading'?
 
 // CC1: "-mmlir" "-mlir-disable-threading"
