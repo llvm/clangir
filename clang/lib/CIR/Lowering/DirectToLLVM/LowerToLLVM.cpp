6e8eff169bfc0ca899f4c665167911602ab3008a
Hongtao Yu <hoy@fb.com>
2023-06-14 09:23:23 -0700

    [CIR][Lowering] Add CIRDialectLLVMIRTranslationInterface to handle CIR attributes to LLVM conversion.
    Summary:
    The interface is currently just a placeholder and doesn't do real work. Suppport for specific atttribure is on the way.

--- clang/lib/CIR/Lowering/DirectToLLVM/LowerToLLVM.cpp
+++ clang/lib/CIR/Lowering/DirectToLLVM/LowerToLLVM.cpp
@@ -1216,6 +1216,8 @@ std::unique_ptr<mlir::Pass> createConvertCIRToLLVMPass() {
   return std::make_unique<ConvertCIRToLLVMPass>();
 }
 
+extern void registerCIRDialectTranslation(mlir::MLIRContext &context);
+
 std::unique_ptr<llvm::Module>
 lowerDirectlyFromCIRToLLVMIR(mlir::ModuleOp theModule,
                              std::unique_ptr<mlir::MLIRContext> mlirCtx,
@@ -1240,6 +1242,7 @@ lowerDirectlyFromCIRToLLVMIR(mlir::ModuleOp theModule,
 
   mlir::registerBuiltinDialectTranslation(*mlirCtx);
   mlir::registerLLVMDialectTranslation(*mlirCtx);
+  registerCIRDialectTranslation(*mlirCtx);
 
   auto ModuleName = theModule.getName();
   auto llvmModule = mlir::translateModuleToLLVMIR(
