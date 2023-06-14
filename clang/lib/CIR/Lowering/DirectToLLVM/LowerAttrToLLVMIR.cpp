73e2e95852c5e0d850d37c1242e9123b5bd8f9cd
Hongtao Yu <hoy@fb.com>
2023-06-14 09:23:23 -0700

    [CIR][Lowering] Add CIRDialectLLVMIRTranslationInterface to handle CIR attributes to LLVM conversion.
    Summary:
    The interface is currently just a placeholder and doesn't do real work. Suppport for specific atttribure is on the way.

---
+++ clang/lib/CIR/Lowering/DirectToLLVM/LowerAttrToLLVMIR.cpp
@@ -0,0 +1,54 @@
+//====- LowerAttrToLLVMIR.cpp - Lowering CIR attributes to LLVMIR ---------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file implements lowering of CIR attributes to LLVMIR.
+//
+//===----------------------------------------------------------------------===//
+
+#include "mlir/IR/DialectRegistry.h"
+#include "mlir/Target/LLVMIR/LLVMTranslationInterface.h"
+#include "clang/CIR/Dialect/IR/CIRDialect.h"
+#include "clang/CIR/Passes.h"
+
+using namespace cir;
+using namespace llvm;
+
+namespace cir {
+namespace direct {
+
+/// Implementation of the dialect interface that converts CIR attributes to LLVM
+/// IR metadata.
+class CIRDialectLLVMIRTranslationInterface
+    : public mlir::LLVMTranslationDialectInterface {
+public:
+  using LLVMTranslationDialectInterface::LLVMTranslationDialectInterface;
+
+  /// Any named attribute in the CIR dialect, i.e, with name started with
+  /// "cir.", will be handled here.
+  mlir::LogicalResult
+  amendOperation(mlir::Operation *op, mlir::NamedAttribute attribute,
+                 mlir::LLVM::ModuleTranslation &moduleTranslation) const final {
+    // TODO: Implement this
+    return mlir::success();
+  }
+};
+
+void registerCIRDialectTranslation(mlir::DialectRegistry &registry) {
+  registry.insert<mlir::cir::CIRDialect>();
+  registry.addExtension(+[](mlir::MLIRContext *ctx, mlir::cir::CIRDialect *dialect) {
+    dialect->addInterfaces<CIRDialectLLVMIRTranslationInterface>();
+  });
+}
+
+void registerCIRDialectTranslation(mlir::MLIRContext &context) {
+  mlir::DialectRegistry registry;
+  registerCIRDialectTranslation(registry);
+  context.appendDialectRegistry(registry);
+}
+} // namespace direct
+} // namespace cir
--- clang/lib/CIR/Lowering/DirectToLLVM/LowerToLLVM.cpp
+++ clang/lib/CIR/Lowering/DirectToLLVM/LowerToLLVM.cpp
@@ -1218,6 +1218,8 @@ std::unique_ptr<mlir::Pass> createConvertCIRToLLVMPass() {
   return std::make_unique<ConvertCIRToLLVMPass>();
 }
 
+extern void registerCIRDialectTranslation(mlir::MLIRContext &context);
+
 std::unique_ptr<llvm::Module>
 lowerDirectlyFromCIRToLLVMIR(mlir::ModuleOp theModule,
                              std::unique_ptr<mlir::MLIRContext> mlirCtx,
@@ -1243,6 +1245,7 @@ lowerDirectlyFromCIRToLLVMIR(mlir::ModuleOp theModule,
 
   mlir::registerBuiltinDialectTranslation(*mlirCtx);
   mlir::registerLLVMDialectTranslation(*mlirCtx);
+  registerCIRDialectTranslation(*mlirCtx);
 
   auto ModuleName = theModule.getName();
   auto llvmModule = mlir::translateModuleToLLVMIR(
