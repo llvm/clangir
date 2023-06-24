bd5f9da91798883861cc1655354143129e50a68b
Vinicius Couto Espindola <vini.couto.e@gmail.com>
2023-06-24 19:59:19 -0300

    [CIR][CIRGen][Lowering] Restrict FuncType to exactly one return type
    Updates FuncType table gen definition to restrict the number of return
    types to exactly one.
    
    Patches FuncOp, CallOp, and codegen builders to work with the new
    function type definition.
    
    Empty results in FuncType builders are replaced by void types.
    
    Void function definitions can be parsed in three different ways:
      - No return type specified: the return type is implicitly void
      - Return type specified as void: the return type is explicitly void
      - Return type specified as empty: the return type is implicitly void
    
    Some verification checks are removed since they are now implicitly
    checked.
    
    Added lowering for void types in both direct and indirect lowering.
    
    ghstack-source-id: 5468006bd0e0ed27be9e9f1a32ee6371a9d9c237
    Pull Request resolved: https://github.com/llvm/clangir/pull/118

--- clang/lib/CIR/Lowering/DirectToLLVM/LowerToLLVM.cpp
+++ clang/lib/CIR/Lowering/DirectToLLVM/LowerToLLVM.cpp
@@ -670,12 +670,8 @@ public:
       signatureConversion.addInputs(argType.index(), convertedType);
     }
 
-    mlir::Type resultType;
-    if (fnType.getNumResults() == 1) {
-      resultType = getTypeConverter()->convertType(fnType.getResult(0));
-      if (!resultType)
-        return mlir::failure();
-    }
+    mlir::Type resultType =
+        getTypeConverter()->convertType(fnType.getReturnType());
 
     // Create the LLVM function operation.
     auto llvmFnTy = mlir::LLVM::LLVMFunctionType::get(
@@ -1163,6 +1159,9 @@ mlir::LLVMTypeConverter prepareTypeConverter(mlir::MLIRContext *ctx) {
     if (llvmStruct.setBody(llvmMembers, /*isPacked=*/type.getPacked()).failed())
       llvm_unreachable("Failed to set body of struct");
     return llvmStruct;
+  });
+  converter.addConversion([&](mlir::cir::VoidType type) -> mlir::Type {
+    return mlir::LLVM::LLVMVoidType::get(type.getContext());
   });
 
   return converter;
