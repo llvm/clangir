2bf8e6cf8294c6d18104ebe5cb732412f35cd824
Vinicius Couto Espindola <vini.couto.e@gmail.com>
2023-06-26 20:20:19 -0300

    [CIR][CIRGen] Add !cir.lang module attribute
    The `!cir.lang` module attribute is used to represent the source
    language used to compile the module.
    
    Not all languages are supported yet.
    
    ghstack-source-id: d6cc07dc8fdaeff611a48f0008febe5d815d3b3a
    Pull Request resolved: https://github.com/llvm/clangir/pull/120

--- clang/lib/CIR/Lowering/DirectToLLVM/LowerToLLVM.cpp
+++ clang/lib/CIR/Lowering/DirectToLLVM/LowerToLLVM.cpp
@@ -1232,6 +1232,7 @@ void ConvertCIRToLLVMPass::runOnOperation() {
                            mlir::func::FuncDialect>();
 
   getOperation()->removeAttr("cir.sob");
+  getOperation()->removeAttr("cir.lang");
 
   if (failed(applyPartialConversion(module, target, std::move(patterns))))
     signalPassFailure();
