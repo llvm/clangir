a75fba38166b651179be2adba237cd50c13fd962
Vinicius Couto Espindola <vini.couto.e@gmail.com>
2023-06-26 21:12:55 -0300

    [CIR][CIRGen][Bugfix] Remove Optional wrapper from MLIR Type
    MLIR Types are inherently wrappers. Checking if a value exists is a
    matter of checking if the type is null.
    
    The Optional wrapper was causing a bug in the `CIRGenModule::buildGlobal`
    method. If an empty type (`mlir::Type{}`) was passed as `Ty`, the
    `if (!ty)` clause would evalute to true, because the `!` operator would
    simply validate that there was a `mlir::Type` in the optional wrapper.
    Then the code gen would continue and try to emit an invalid type.
    
    This patch removes the wrapper, which ensures that the `!Ty` clause now
    checks if the given `mlir::Type` is an empty type. In that case, it
    generates a real type from the AST Type:
    
    ```
      if (!Ty)
        Ty = getTypes().convertTypeForMem(ASTTy);
    ```
    
    ghstack-source-id: 345e49cd75ef0c682b4089121bc4f8ce0a29167f
    Pull Request resolved: https://github.com/llvm/clangir/pull/132

--- clang/lib/CIR/CodeGen/CIRGenModule.cpp
+++ clang/lib/CIR/CodeGen/CIRGenModule.cpp
@@ -663,8 +663,7 @@ CIRGenModule::getOrCreateCIRGlobal(StringRef MangledName, mlir::Type Ty,
   return GV;
 }
 
-mlir::cir::GlobalOp CIRGenModule::buildGlobal(const VarDecl *D,
-                                              std::optional<mlir::Type> Ty,
+mlir::cir::GlobalOp CIRGenModule::buildGlobal(const VarDecl *D, mlir::Type Ty,
                                               ForDefinition_t IsForDefinition) {
   assert(D->hasGlobalStorage() && "Not a global variable");
   QualType ASTTy = D->getType();
@@ -672,7 +671,7 @@ mlir::cir::GlobalOp CIRGenModule::buildGlobal(const VarDecl *D,
     Ty = getTypes().convertTypeForMem(ASTTy);
 
   StringRef MangledName = getMangledName(D);
-  return getOrCreateCIRGlobal(MangledName, *Ty, ASTTy.getAddressSpace(), D,
+  return getOrCreateCIRGlobal(MangledName, Ty, ASTTy.getAddressSpace(), D,
                               IsForDefinition);
 }
 
@@ -682,8 +681,7 @@ mlir::cir::GlobalOp CIRGenModule::buildGlobal(const VarDecl *D,
 /// If IsForDefinition is true, it is guaranteed that an actual global with type
 /// Ty will be returned, not conversion of a variable with the same mangled name
 /// but some other type.
-mlir::Value CIRGenModule::getAddrOfGlobalVar(const VarDecl *D,
-                                             std::optional<mlir::Type> Ty,
+mlir::Value CIRGenModule::getAddrOfGlobalVar(const VarDecl *D, mlir::Type Ty,
                                              ForDefinition_t IsForDefinition) {
   assert(D->hasGlobalStorage() && "Not a global variable");
   QualType ASTTy = D->getType();
