2390b9312b06f50c37901011250852fcd595b1be
Vinicius Couto Espindola <vini.couto.e@gmail.com>
2023-07-01 06:40:08 -0300

    [CIR] Yield boolean value in cir.loop condition region
    Before this patch, the loop operation condition block yielded either
    empty or continue. This was replaced by a yield of a boolean value.
    
    This change simplifies both codegen and lowering, while also being
    semantically closer to the C language.
    
    It also refactors loop op codegen tests to validate only the lowering
    related to the cir.loop operation.
    
    Fixes #161

--- clang/lib/CIR/CodeGen/CIRGenStmt.cpp
+++ clang/lib/CIR/CodeGen/CIRGenStmt.cpp
@@ -650,26 +650,6 @@ mlir::LogicalResult CIRGenFunction::buildDefaultStmt(const DefaultStmt &S,
   return res;
 }
 
-static mlir::LogicalResult buildLoopCondYield(mlir::OpBuilder &builder,
-                                              mlir::Location loc,
-                                              mlir::Value cond) {
-  mlir::Block *trueBB = nullptr, *falseBB = nullptr;
-  {
-    mlir::OpBuilder::InsertionGuard guard(builder);
-    trueBB = builder.createBlock(builder.getBlock()->getParent());
-    builder.create<mlir::cir::YieldOp>(loc, YieldOpKind::Continue);
-  }
-  {
-    mlir::OpBuilder::InsertionGuard guard(builder);
-    falseBB = builder.createBlock(builder.getBlock()->getParent());
-    builder.create<mlir::cir::YieldOp>(loc);
-  }
-
-  assert((trueBB && falseBB) && "expected both blocks to exist");
-  builder.create<mlir::cir::BrCondOp>(loc, cond, trueBB, falseBB);
-  return mlir::success();
-}
-
 mlir::LogicalResult
 CIRGenFunction::buildCXXForRangeStmt(const CXXForRangeStmt &S,
                                      ArrayRef<const Attr *> ForAttrs) {
@@ -703,8 +683,7 @@ CIRGenFunction::buildCXXForRangeStmt(const CXXForRangeStmt &S,
           assert(!UnimplementedFeature::createProfileWeightsForLoop());
           assert(!UnimplementedFeature::emitCondLikelihoodViaExpectIntrinsic());
           mlir::Value condVal = evaluateExprAsBool(S.getCond());
-          if (buildLoopCondYield(b, loc, condVal).failed())
-            loopRes = mlir::failure();
+          builder.create<mlir::cir::YieldOp>(loc, condVal);
         },
         /*bodyBuilder=*/
         [&](mlir::OpBuilder &b, mlir::Location loc) {
@@ -791,8 +770,7 @@ mlir::LogicalResult CIRGenFunction::buildForStmt(const ForStmt &S) {
                 loc, boolTy,
                 mlir::cir::BoolAttr::get(b.getContext(), boolTy, true));
           }
-          if (buildLoopCondYield(b, loc, condVal).failed())
-            loopRes = mlir::failure();
+          builder.create<mlir::cir::YieldOp>(loc, condVal);
         },
         /*bodyBuilder=*/
         [&](mlir::OpBuilder &b, mlir::Location loc) {
@@ -860,8 +838,7 @@ mlir::LogicalResult CIRGenFunction::buildDoStmt(const DoStmt &S) {
           // expression compares unequal to 0. The condition must be a
           // scalar type.
           mlir::Value condVal = evaluateExprAsBool(S.getCond());
-          if (buildLoopCondYield(b, loc, condVal).failed())
-            loopRes = mlir::failure();
+          builder.create<mlir::cir::YieldOp>(loc, condVal);
         },
         /*bodyBuilder=*/
         [&](mlir::OpBuilder &b, mlir::Location loc) {
@@ -925,8 +902,7 @@ mlir::LogicalResult CIRGenFunction::buildWhileStmt(const WhileStmt &S) {
           // expression compares unequal to 0. The condition must be a
           // scalar type.
           condVal = evaluateExprAsBool(S.getCond());
-          if (buildLoopCondYield(b, loc, condVal).failed())
-            loopRes = mlir::failure();
+          builder.create<mlir::cir::YieldOp>(loc, condVal);
         },
         /*bodyBuilder=*/
         [&](mlir::OpBuilder &b, mlir::Location loc) {
