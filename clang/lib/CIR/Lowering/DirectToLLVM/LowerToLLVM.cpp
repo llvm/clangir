cc6759393cd73d64c191db2342b9f20372104899
Vinicius Couto Espindola <vini.couto.e@gmail.com>
2023-07-05 18:12:31 -0300

    [CIR][Lowering] Lower structured do-while loops
    Conditionally set the loop entry point depending on whether the loop is
    of the `do-while` kind or not.
    
    ghstack-source-id: 711f4befd051a68a70bd099982b31d7cad3f5a71
    Pull Request resolved: https://github.com/llvm/clangir/pull/146

--- clang/lib/CIR/Lowering/DirectToLLVM/LowerToLLVM.cpp
+++ clang/lib/CIR/Lowering/DirectToLLVM/LowerToLLVM.cpp
@@ -135,7 +135,8 @@ public:
 
   mlir::LogicalResult
   rewriteWhileLoop(mlir::cir::LoopOp loopOp, OpAdaptor adaptor,
-                   mlir::ConversionPatternRewriter &rewriter) const {
+                   mlir::ConversionPatternRewriter &rewriter,
+                   mlir::cir::LoopOpKind kind) const {
     auto *currentBlock = rewriter.getInsertionBlock();
     auto *continueBlock =
         rewriter.splitBlock(currentBlock, rewriter.getInsertionPoint());
@@ -158,9 +159,10 @@ public:
     rewriter.inlineRegionBefore(condRegion, continueBlock);
     rewriter.inlineRegionBefore(bodyRegion, continueBlock);
 
-    // Set loop entry point to condition block.
+    // Set loop entry point to condition or to body in do-while cases.
     rewriter.setInsertionPointToEnd(currentBlock);
-    rewriter.create<mlir::cir::BrOp>(loopOp.getLoc(), &condFrontBlock);
+    auto &entry = (kind != LoopKind::DoWhile ? condFrontBlock : bodyFrontBlock);
+    rewriter.create<mlir::cir::BrOp>(loopOp.getLoc(), &entry);
 
     // Set loop exit point to continue block.
     rewriter.setInsertionPoint(yieldToCont);
@@ -186,9 +188,8 @@ public:
     case LoopKind::For:
       break;
     case LoopKind::While:
-      return rewriteWhileLoop(loopOp, adaptor, rewriter);
     case LoopKind::DoWhile:
-      llvm_unreachable("NYI");
+      return rewriteWhileLoop(loopOp, adaptor, rewriter, loopOp.getKind());
     }
 
     auto loc = loopOp.getLoc();
