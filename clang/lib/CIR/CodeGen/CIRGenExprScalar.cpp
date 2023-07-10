005d727279580e560163cd66a212b48ef4d462ce
Vinicius Couto Espindola <vini.couto.e@gmail.com>
2023-07-10 08:45:22 -0300

    [CIR][CIRGen] Support floating point subtraction
    While the original codegen attempts to generate a fmuladd, this patch
    ignores this optimization as it should be deferred to MLIR.
    
    It also updates FP options builder to be closer to the original codegen
    and improve the tracking of missing features.

--- clang/lib/CIR/CodeGen/CIRGenExprScalar.cpp
+++ clang/lib/CIR/CodeGen/CIRGenExprScalar.cpp
@@ -1067,7 +1067,8 @@ mlir::Value ScalarExprEmitter::buildSub(const BinOpInfo &Ops) {
 
     assert(!UnimplementedFeature::cirVectorType());
     if (Ops.LHS.getType().isa<mlir::FloatType>()) {
-      llvm_unreachable("NYI");
+      CIRGenFunction::CIRGenFPOptionsRAII FPOptsRAII(CGF, Ops.FPFeatures);
+      return Builder.createFSub(Ops.LHS, Ops.RHS);
     }
 
     if (Ops.isFixedPointOp())
