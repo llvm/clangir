1314c05ae9fb59f8563d119a41f9f5ecd2a3b66a
Hongtao Yu <hoy@fb.com>
2023-07-31 15:15:44 -0700

    [CIR] Enable MLIR asm printer options (#195)
    Enabling MLIR asm printing options such as -mlir-print-op-generic

--- clang/test/CIR/mlirargs.c
+++ clang/test/CIR/mlirargs.c
@@ -1,10 +1,12 @@
 // Clang returns 1 when wrong arguments are given.
-// RUN: not %clang_cc1 -mmlir -mlir-disable-threadingd 2>&1 | FileCheck %s --check-prefix=WRONG
+// RUN: not %clang_cc1 -mmlir -mlir-disable-threadingd  -mmlir -mlir-print-op-genericd 2>&1 | FileCheck %s --check-prefix=WRONG
 // Test that the driver can pass mlir args to cc1.
 // RUN: %clang -### -mmlir -mlir-disable-threading %s 2>&1 | FileCheck %s --check-prefix=CC1
 
 
 // WRONG: ClangIR (MLIR option parsing): Unknown command line argument '-mlir-disable-threadingd'.  Try: 'ClangIR (MLIR option parsing) --help'
 // WRONG: ClangIR (MLIR option parsing): Did you mean '--mlir-disable-threading'?
+// WRONG: ClangIR (MLIR option parsing): Unknown command line argument '-mlir-print-op-genericd'.  Try: 'ClangIR (MLIR option parsing) --help'
+// WRONG: ClangIR (MLIR option parsing): Did you mean '--mlir-print-op-generic'?
 
 // CC1: "-mmlir" "-mlir-disable-threading"
