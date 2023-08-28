e7cae099b080a2be70ed469d1226050509513f5f
Henrich Lauko <xlauko@mail.muni.cz>
2023-08-28 14:08:53 +0200

    [CIR] Introduce AST attribute interfaces.

---
+++ clang/include/clang/CIR/Interfaces/ASTAttrInterfaces.h
@@ -0,0 +1,19 @@
+//===- ASTAttrInterfaces.h - CIR AST Interfaces -----------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef MLIR_INTERFACES_CIR_AST_ATR_INTERFACES_H_
+#define MLIR_INTERFACES_CIR_AST_ATR_INTERFACES_H_
+
+#include "mlir/IR/Attributes.h"
+
+#include "clang/AST/Attr.h"
+
+/// Include the generated interface declarations.
+#include "clang/CIR/Interfaces/ASTAttrInterfaces.h.inc"
+
+#endif // MLIR_INTERFACES_CIR_AST_ATR_INTERFACES_H_
---
+++ clang/lib/CIR/Interfaces/ASTAttrInterfaces.cpp
@@ -0,0 +1,11 @@
+//====- ASTAttrInterfaces.cpp - Interface to AST Attributes ---------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+#include "clang/CIR/Interfaces/ASTAttrInterfaces.h"
+
+/// Include the generated type qualifiers interfaces.
+#include "clang/CIR/Interfaces/ASTAttrInterfaces.cpp.inc"
