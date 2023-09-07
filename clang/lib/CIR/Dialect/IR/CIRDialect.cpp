1899f8689a1f53ae8b22fbf660c54018b8ba7a5b
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2023-09-06 17:12:14 -0700

    [CIR][NFC] Add sketch for TryOp

--- clang/lib/CIR/Dialect/IR/CIRDialect.cpp
+++ clang/lib/CIR/Dialect/IR/CIRDialect.cpp
@@ -664,6 +664,23 @@ void ScopeOp::build(OpBuilder &builder, OperationState &result,
 LogicalResult ScopeOp::verify() { return success(); }
 
 //===----------------------------------------------------------------------===//
+// TryOp
+//===----------------------------------------------------------------------===//
+
+void TryOp::getSuccessorRegions(mlir::RegionBranchPoint point,
+                                SmallVectorImpl<RegionSuccessor> &regions) {
+  // The only region always branch back to the parent operation.
+  if (!point.isParent()) {
+    regions.push_back(
+        RegionSuccessor(getOperation(), this->getODSResults(0)));
+    return;
+  }
+
+  // If the condition isn't constant, both regions may be executed.
+  regions.push_back(RegionSuccessor(&getBody()));
+}
+
+//===----------------------------------------------------------------------===//
 // TernaryOp
 //===----------------------------------------------------------------------===//
 
