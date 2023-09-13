124d1b1c16c52e1e51478d3bc994301a7de97b76
Yury Gribov <tetra2005@gmail.com>
2023-09-14 01:51:57 +0300

    [CIR] Implement fabs operation. (#254)
    Following discussion in #237 this adds support for `fabs` builtins which
    are used extensively in llvm-test-suite.

--- clang/lib/CIR/Lowering/DirectToLLVM/LowerToLLVM.cpp
+++ clang/lib/CIR/Lowering/DirectToLLVM/LowerToLLVM.cpp
@@ -1841,6 +1841,19 @@ public:
   }
 };
 
+class CIRFAbsOpLowering : public mlir::OpConversionPattern<mlir::cir::FAbsOp> {
+public:
+  using OpConversionPattern<mlir::cir::FAbsOp>::OpConversionPattern;
+
+  mlir::LogicalResult
+  matchAndRewrite(mlir::cir::FAbsOp op, OpAdaptor adaptor,
+                  mlir::ConversionPatternRewriter &rewriter) const override {
+    rewriter.replaceOpWithNewOp<mlir::LLVM::FAbsOp>(
+        op, adaptor.getOperands().front());
+    return mlir::success();
+  }
+};
+
 void populateCIRToLLVMConversionPatterns(mlir::RewritePatternSet &patterns,
                                          mlir::TypeConverter &converter) {
   patterns.add<CIRReturnLowering>(patterns.getContext());
@@ -1853,7 +1866,8 @@ void populateCIRToLLVMConversionPatterns(mlir::RewritePatternSet &patterns,
                CIRVAStartLowering, CIRVAEndLowering, CIRVACopyLowering,
                CIRVAArgLowering, CIRBrOpLowering, CIRTernaryOpLowering,
                CIRGetMemberOpLowering, CIRSwitchOpLowering,
-               CIRPtrDiffOpLowering, CIRCopyOpLowering, CIRMemCpyOpLowering>(
+               CIRPtrDiffOpLowering, CIRCopyOpLowering, CIRMemCpyOpLowering,
+               CIRFAbsOpLowering>(
       converter, patterns.getContext());
 }
 
