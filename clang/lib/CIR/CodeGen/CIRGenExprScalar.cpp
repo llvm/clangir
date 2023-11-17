9ae0f51a3ef05904a9f572f49093abbd06975657
David Olsen <dolsen@nvidia.com>
2023-11-17 15:17:22 -0800

    [CIR] Fix bool-to-pointer conversions (#319)
    Conversions from an integer to a pointer are implemented in CIR code gen
    as an integral conversion to uintptr_t followed by the
    integral-to-pointer conversion. Conversions from bool to pointer were
    following the same code path. But bool-to-int is a different CastKind
    than int-to-int in CIR, and CIR was failing validation. Fix the integer
    to pointer conversion code to correctly handle a source type of bool.
    
    (A conversion from bool to pointer makes no sense and should never
    happen in the real world. But it is legal due to bool being sort of an
    integral type. So we need to support it.)
    
    Also, in `ScalarExprEmitter::buildScalarConversion` change a couple
    not-yet-implemented messages about pointer types into assertion
    failures. Conversions involving pointer types should never go through
    `ScalarExprEmitter::buildScalarConversion`.

--- clang/lib/CIR/CodeGen/CIRGenExprScalar.cpp
+++ clang/lib/CIR/CodeGen/CIRGenExprScalar.cpp
@@ -898,17 +898,9 @@ public:
       return Src;
     }
 
-    // Handle pointer conversions next: pointers can only be converted to/from
-    // other pointers and integers.
-    if (mlir::isa<::mlir::cir::PointerType>(DstTy)) {
-      llvm_unreachable("not implemented");
-    }
-
-    if (mlir::isa<::mlir::cir::PointerType>(SrcTy)) {
-      // Must be a ptr to int cast.
-      assert(mlir::isa<mlir::IntegerType>(DstTy) && "not ptr->int?");
-      llvm_unreachable("not implemented");
-    }
+    assert(!mlir::isa<::mlir::cir::PointerType>(SrcTy) &&
+           !mlir::isa<::mlir::cir::PointerType>(DstTy) &&
+           "Internal error: pointer conversions are handled elsewhere");
 
     // A scalar can be splatted to an extended vector of the same element type
     if (DstType->isExtVectorType() && !SrcType->isVectorType()) {
@@ -1439,8 +1431,14 @@ mlir::Value ScalarExprEmitter::VisitCastExpr(CastExpr *CE) {
     mlir::Value Src = Visit(const_cast<Expr *>(E));
 
     // Properly resize by casting to an int of the same size as the pointer.
+    // Clang's IntegralToPointer includes 'bool' as the source, but in CIR
+    // 'bool' is not an integral type.  So check the source type to get the
+    // correct CIR conversion.
     auto MiddleTy = CGF.CGM.getDataLayout().getIntPtrType(DestCIRTy);
-    auto MiddleVal = Builder.createIntCast(Src, MiddleTy);
+    auto MiddleVal = Builder.createCast(E->getType()->isBooleanType()
+                                            ? mlir::cir::CastKind::bool_to_int
+                                            : mlir::cir::CastKind::integral,
+                                        Src, MiddleTy);
 
     if (CGF.CGM.getCodeGenOpts().StrictVTablePointers)
       llvm_unreachable("NYI");
