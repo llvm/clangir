6059d915b8f26ae84d56d8758a59378f75a18db7
gitoleg <forown@yandex.ru>
2023-11-22 22:54:17 +0300

    [CIR][IR] Harden get_member verifier (#330)
    I think it's time to claim that CIR supports recursive types (many
    thanks to #303 and to @sitio-couto :) )
    And we can bring back the `get_member` verification back, with no checks
    for incomplete types. What do you think?
    
    And we can close #256 as well

--- clang/lib/CIR/Dialect/IR/CIRDialect.cpp
+++ clang/lib/CIR/Dialect/IR/CIRDialect.cpp
@@ -2443,14 +2443,6 @@ LogicalResult MemCpyOp::verify() {
   return mlir::success();
 }
 
-static bool isIncompleteType(mlir::Type typ) {
-  if (auto ptr = mlir::dyn_cast<PointerType>(typ))
-    return isIncompleteType(ptr.getPointee());
-  else if (auto rec = mlir::dyn_cast<StructType>(typ))
-    return rec.isIncomplete();
-  return false;
-}
-
 //===----------------------------------------------------------------------===//
 // GetMemberOp Definitions
 //===----------------------------------------------------------------------===//
@@ -2461,21 +2453,12 @@ LogicalResult GetMemberOp::verify() {
   if (!recordTy)
     return emitError() << "expected pointer to a record type";
 
-  // FIXME: currently we bypass typechecking of incomplete types due to errors
-  // in the codegen process. This should be removed once the codegen is fixed.
-  if (isIncompleteType(recordTy))
-    return mlir::success();
-
   if (recordTy.getMembers().size() <= getIndex())
     return emitError() << "member index out of bounds";
 
   // FIXME(cir): member type check is disabled for classes as the codegen for
   // these still need to be patched.
-  // Also we bypass the typechecking for the fields of incomplete types.
-  bool shouldSkipMemberTypeMismatch =
-    recordTy.isClass() || isIncompleteType(recordTy.getMembers()[getIndex()]);
-
-  if (!shouldSkipMemberTypeMismatch
+  if (!recordTy.isClass()
       && recordTy.getMembers()[getIndex()] != getResultTy().getPointee())
     return emitError() << "member type mismatch";
 
