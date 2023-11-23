165bec957169fcfb42a2da40a547cb37f9123a9b
gitoleg <forown@yandex.ru>
2023-11-23 17:25:06 +0300

    adds result type

--- clang/lib/CIR/CodeGen/CIRAsm.cpp
+++ clang/lib/CIR/CodeGen/CIRAsm.cpp
@@ -44,7 +44,7 @@ mlir::LogicalResult CIRGenFunction::buildAsmStmt(const AsmStmt &S) {
   AsmDialect AsmDialect = inferDialect(CGM, S);
 
   builder.create<mlir::cir::InlineAsmOp>(
-      getLoc(S.getAsmLoc()), AsmString, AsmDialect);
+      getLoc(S.getAsmLoc()), ResultType, AsmString, AsmDialect);
 
   return mlir::success();
 }
