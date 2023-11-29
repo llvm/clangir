28fcf4f040d71e3307fd734e993dc0d324502b2d
Vinicius Couto Espindola <34522047+sitio-couto@users.noreply.github.com>
2023-11-29 17:05:07 -0300

    [CIR][CIRGen] Fix agg-init2.cpp test (#343)
    This patch fixes the agg-init2.cpp test by doing two things:
    
    - Updating the `VisitCXXConstructExpr` to return a zero attribute
    initializer for trivial zero-initialized objects.
    - Forcing the `buildAutoVarInit` method to use ctor calls on temporary
    object expressions even if the object can be constant-initialized.

--- clang/lib/CIR/CodeGen/CIRGenExprConst.cpp
+++ clang/lib/CIR/CodeGen/CIRGenExprConst.cpp
@@ -912,7 +912,7 @@ public:
       return nullptr;
     }
 
-    llvm_unreachable("NYI");
+    return CGM.getBuilder().getZeroInitAttr(CGM.getCIRType(Ty));
   }
 
   mlir::Attribute VisitStringLiteral(StringLiteral *E, QualType T) {
