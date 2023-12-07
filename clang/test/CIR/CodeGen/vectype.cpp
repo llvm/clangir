8d1069058707f15378a497fa642f1536506e2c4d
David Olsen <dolsen@nvidia.com>
2023-12-07 11:29:20 -0800

    [CIR] Update code gen test vectype.cpp to be more flexible
    The order in which CIR is generated is not always guaranteed, so the
    expected results for code gen tests need to be flexible about the order
    of operations and the names of MLIR values.

--- clang/test/CIR/CodeGen/vectype.cpp
+++ clang/test/CIR/CodeGen/vectype.cpp
@@ -8,29 +8,8 @@ int main() {
   return c[1];
 }
 
-// CHECK:    %0 = cir.alloca !s32i, cir.ptr <!s32i>, ["__retval"] {alignment = 4 : i64}
-// CHECK:    %1 = cir.alloca !cir.vector<!s32i x 4>, cir.ptr <!cir.vector<!s32i x 4>>, ["a", init] {alignment = 16 : i64}
-// CHECK:    %2 = cir.alloca !cir.vector<!s32i x 4>, cir.ptr <!cir.vector<!s32i x 4>>, ["b", init] {alignment = 16 : i64}
-// CHECK:    %3 = cir.alloca !cir.vector<!s32i x 4>, cir.ptr <!cir.vector<!s32i x 4>>, ["c", init] {alignment = 16 : i64}
-// CHECK:    %4 = cir.const(#cir.int<1> : !s32i) : !s32i
-// CHECK:    %5 = cir.const(#cir.int<2> : !s32i) : !s32i
-// CHECK:    %6 = cir.const(#cir.int<3> : !s32i) : !s32i
-// CHECK:    %7 = cir.const(#cir.int<4> : !s32i) : !s32i
-// CHECK:    %8 = cir.vec(%4, %5, %6, %7 : !s32i, !s32i, !s32i, !s32i) : <!s32i x 4>
-// CHECK:    cir.store %8, %1 : !cir.vector<!s32i x 4>, cir.ptr <!cir.vector<!s32i x 4>>
-// CHECK:    %9 = cir.const(#cir.int<5> : !s32i) : !s32i
-// CHECK:    %10 = cir.const(#cir.int<6> : !s32i) : !s32i
-// CHECK:    %11 = cir.const(#cir.int<7> : !s32i) : !s32i
-// CHECK:    %12 = cir.const(#cir.int<8> : !s32i) : !s32i
-// CHECK:    %13 = cir.vec(%9, %10, %11, %12 : !s32i, !s32i, !s32i, !s32i) : <!s32i x 4>
-// CHECK:    cir.store %13, %2 : !cir.vector<!s32i x 4>, cir.ptr <!cir.vector<!s32i x 4>>
-// CHECK:    %14 = cir.load %1 : cir.ptr <!cir.vector<!s32i x 4>>, !cir.vector<!s32i x 4>
-// CHECK:    %15 = cir.load %2 : cir.ptr <!cir.vector<!s32i x 4>>, !cir.vector<!s32i x 4>
-// CHECK:    %16 = cir.binop(add, %14, %15) : !cir.vector<!s32i x 4>
-// CHECK:    cir.store %16, %3 : !cir.vector<!s32i x 4>, cir.ptr <!cir.vector<!s32i x 4>>
-// CHECK:    %17 = cir.const(#cir.int<1> : !s32i) : !s32i
-// CHECK:    %18 = cir.load %3 : cir.ptr <!cir.vector<!s32i x 4>>, !cir.vector<!s32i x 4>
-// CHECK:    %19 = cir.vec_elem %18[%17 : !s32i] <!s32i x 4> -> !s32i
-// CHECK:    cir.store %19, %0 : !s32i, cir.ptr <!s32i>
-// CHECK:    %20 = cir.load %0 : cir.ptr <!s32i>, !s32i
-// CHECK:    cir.return %20 : !s32i
+// CHECK:    %{{[0-9]+}} = cir.vec(%{{[0-9]+}}, %{{[0-9]+}}, %{{[0-9]+}}, %{{[0-9]+}} : !s32i, !s32i, !s32i, !s32i) : <!s32i x 4>
+// CHECK:    cir.store %{{[0-9]+}}, %{{[0-9]+}} : !cir.vector<!s32i x 4>, cir.ptr <!cir.vector<!s32i x 4>>
+// CHECK:    %{{[0-9]+}} = cir.load %{{[0-9]+}} : cir.ptr <!cir.vector<!s32i x 4>>, !cir.vector<!s32i x 4>
+// CHECK:    %{{[0-9]+}} = cir.binop(add, %{{[0-9]+}}, %{{[0-9]+}}) : !cir.vector<!s32i x 4>
+// CHECK:    %{{[0-9]+}} = cir.vec_elem %{{[0-9]+}}[%{{[0-9]+}} : !s32i] <!s32i x 4> -> !s32i
