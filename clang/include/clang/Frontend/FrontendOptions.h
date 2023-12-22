5b1b94b9ebe3d1674822f5fed3fea1e0d8fb5dd3
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2023-12-21 21:03:23 -0300

    [CIR] Cleanup idiom-recognizer and lib-opt options
    This was a bit half backed, give it some love.

--- clang/include/clang/Frontend/FrontendOptions.h
+++ clang/include/clang/Frontend/FrontendOptions.h
@@ -434,6 +434,12 @@ public:
   // Enable Clang IR based lifetime check
   unsigned ClangIRLifetimeCheck : 1;
 
+  // Enable Clang IR idiom recognizer
+  unsigned ClangIRIdiomRecognizer : 1;
+
+  // Enable Clang IR library optimizations
+  unsigned ClangIRLibOpt : 1;
+
   CodeCompleteOptions CodeCompleteOpts;
 
   /// Specifies the output format of the AST.
@@ -627,6 +633,7 @@ public:
         UseClangIRPipeline(false), ClangIRDirectLowering(false),
         ClangIRDisablePasses(false), ClangIRDisableCIRVerifier(false),
         ClangIRDisableEmitCXXDefault(false), ClangIRLifetimeCheck(false),
+        ClangIRIdiomRecognizer(false), ClangIRLibOpt(false),
         TimeTraceGranularity(500) {}
 
   /// getInputKindForExtension - Return the appropriate input kind for a file
