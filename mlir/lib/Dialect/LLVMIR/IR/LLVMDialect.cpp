93f61064777a267e3594cef63c6389d3729e8a22
gitoleg <forown@yandex.ru>
2023-12-26 11:19:37 +0300

    Revert "[mlir][llvm] Fixes CallOp builder for the case of indirect call"
    This reverts commit bbaa147083ac6cf1b406c5f63b199fb7b971d6dc.

--- mlir/lib/Dialect/LLVMIR/IR/LLVMDialect.cpp
+++ mlir/lib/Dialect/LLVMIR/IR/LLVMDialect.cpp
@@ -978,21 +978,12 @@ void CallOp::build(OpBuilder &builder, OperationState &state, TypeRange results,
 
 void CallOp::build(OpBuilder &builder, OperationState &state, TypeRange results,
                    FlatSymbolRefAttr callee, ValueRange args) {
-  auto fargs = callee ? args : args.drop_front();
   build(builder, state, results,
         /*var_callee_type=*/nullptr, callee, args, /*fastmathFlags=*/nullptr,
         /*branch_weights=*/nullptr,
         /*CConv=*/nullptr, /*TailCallKind=*/nullptr,
         /*memory_effects=*/nullptr,
         /*convergent=*/nullptr, /*no_unwind=*/nullptr, /*will_return=*/nullptr,
-        //
-        // TODO(ClangIR): This was a local change that is no longer valid during
-        // rebase
-        // TypeAttr::get(getLLVMFuncType(builder.getContext(), results, fargs)),
-        // callee, args, /*fastmathFlags=*/nullptr, /*branch_weights=*/nullptr,
-        // /*CConv=*/nullptr,
-        //
-        //
         /*access_groups=*/nullptr, /*alias_scopes=*/nullptr,
         /*noalias_scopes=*/nullptr, /*tbaa=*/nullptr);
 }
