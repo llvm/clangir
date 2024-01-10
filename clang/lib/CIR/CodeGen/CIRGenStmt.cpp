a37d71ec87a2cf1c090683620872a3f36e25ab5a
Vinicius Couto Espindola <34522047+sitio-couto@users.noreply.github.com>
2024-01-09 22:42:35 -0300

    [CIR][IR] Implement loop's conditional operation (#391)
    Like SCF's `scf.condition`, the `cir.condition` simplifies codegen of
    loop conditions by removing the need of a contitional branch. It takes a
    single boolean operand which, if true, executes the body region,
    otherwise exits the loop. This also simplifies lowering and the dialect
    it self.
    
    A new constraint is now enforced on `cir.loops`: the condition region
    must terminate with a `cir.condition` operation.
    
    A few tests were removed as they became redundant, and others where
    simplified.
    
    The merge-cleanups pass no longer simplifies compile-time constant
    conditions, as the condition body terminator is no longer allowed to be
    terminated with a `cir.yield`. To circumvent this, a proper folder
    should be implemented to fold constant conditions, but this was left as
    future work.
    
    Co-authored-by: Bruno Cardoso Lopes <bcardosolopes@users.noreply.github.com>

--- clang/lib/CIR/CodeGen/CIRGenStmt.cpp
+++ clang/lib/CIR/CodeGen/CIRGenStmt.cpp
@@ -650,26 +650,6 @@ CIRGenFunction::buildDefaultStmt(const DefaultStmt &S, mlir::Type condType,
   return buildCaseDefaultCascade(&S, condType, caseAttrs, os);
 }
 
-static mlir::LogicalResult buildLoopCondYield(mlir::OpBuilder &builder,
-                                              mlir::Location loc,
-                                              mlir::Value cond) {
-  mlir::Block *trueBB = nullptr, *falseBB = nullptr;
-  {
-    mlir::OpBuilder::InsertionGuard guard(builder);
-    trueBB = builder.createBlock(builder.getBlock()->getParent());
-    builder.create<mlir::cir::YieldOp>(loc, YieldOpKind::Continue);
-  }
-  {
-    mlir::OpBuilder::InsertionGuard guard(builder);
-    falseBB = builder.createBlock(builder.getBlock()->getParent());
-    builder.create<mlir::cir::YieldOp>(loc);
-  }
-
-  assert((trueBB && falseBB) && "expected both blocks to exist");
-  builder.create<mlir::cir::BrCondOp>(loc, cond, trueBB, falseBB);
-  return mlir::success();
-}
-
 mlir::LogicalResult
 CIRGenFunction::buildCXXForRangeStmt(const CXXForRangeStmt &S,
                                      ArrayRef<const Attr *> ForAttrs) {
@@ -703,8 +683,7 @@ CIRGenFunction::buildCXXForRangeStmt(const CXXForRangeStmt &S,
           assert(!UnimplementedFeature::createProfileWeightsForLoop());
           assert(!UnimplementedFeature::emitCondLikelihoodViaExpectIntrinsic());
           mlir::Value condVal = evaluateExprAsBool(S.getCond());
-          if (buildLoopCondYield(b, loc, condVal).failed())
-            loopRes = mlir::failure();
+          builder.createCondition(condVal);
         },
         /*bodyBuilder=*/
         [&](mlir::OpBuilder &b, mlir::Location loc) {
@@ -786,8 +765,7 @@ mlir::LogicalResult CIRGenFunction::buildForStmt(const ForStmt &S) {
                 loc, boolTy,
                 mlir::cir::BoolAttr::get(b.getContext(), boolTy, true));
           }
-          if (buildLoopCondYield(b, loc, condVal).failed())
-            loopRes = mlir::failure();
+          builder.createCondition(condVal);
         },
         /*bodyBuilder=*/
         [&](mlir::OpBuilder &b, mlir::Location loc) {
@@ -850,8 +828,7 @@ mlir::LogicalResult CIRGenFunction::buildDoStmt(const DoStmt &S) {
           // expression compares unequal to 0. The condition must be a
           // scalar type.
           mlir::Value condVal = evaluateExprAsBool(S.getCond());
-          if (buildLoopCondYield(b, loc, condVal).failed())
-            loopRes = mlir::failure();
+          builder.createCondition(condVal);
         },
         /*bodyBuilder=*/
         [&](mlir::OpBuilder &b, mlir::Location loc) {
@@ -910,8 +887,7 @@ mlir::LogicalResult CIRGenFunction::buildWhileStmt(const WhileStmt &S) {
           // expression compares unequal to 0. The condition must be a
           // scalar type.
           condVal = evaluateExprAsBool(S.getCond());
-          if (buildLoopCondYield(b, loc, condVal).failed())
-            loopRes = mlir::failure();
+          builder.createCondition(condVal);
         },
         /*bodyBuilder=*/
         [&](mlir::OpBuilder &b, mlir::Location loc) {
