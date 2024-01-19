ed2d268b7d14362e097023a694e761ab5eadbd18
Fabian Mora <fmora.dev@gmail.com>
2024-01-19 00:54:41 +0000

    fix test

--- clang/test/CIR/CodeGen/openmp.cpp
+++ clang/test/CIR/CodeGen/openmp.cpp
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 -std=c++20 -triple x86_64-unknown-linux-gnu -clangir-disable-emit-cxx-default -fopenmp -fclangir-enable -emit-cir %s -o %t.cir
+// RUN: %clang_cc1 -std=c++20 -triple x86_64-unknown-linux-gnu -fopenmp -fclangir-enable -emit-cir %s -o %t.cir
 // RUN: FileCheck --input-file=%t.cir %s
 
 // CHECK: cir.func
