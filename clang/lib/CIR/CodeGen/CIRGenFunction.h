23d6954d2cc692b821992a4c14277e32de9663bb
eZWALT <waltertheshadow333@gmail.com>
2024-03-14 12:09:33 +0100

    [CIR][OpenMP] implementation of barrier

--- clang/lib/CIR/CodeGen/CIRGenFunction.h
+++ clang/lib/CIR/CodeGen/CIRGenFunction.h
@@ -995,6 +995,7 @@ public:
   mlir::LogicalResult buildOMPParallelDirective(const OMPParallelDirective &S);
   mlir::LogicalResult buildOMPTaskwaitDirective(const OMPTaskwaitDirective &S);
   mlir::LogicalResult buildOMPTaskyieldDirective(const OMPTaskyieldDirective &S);
+  mlir::LogicalResult buildOMPBarrierDirective(const OMPBarrierDirective &S);
   
   LValue buildOpaqueValueLValue(const OpaqueValueExpr *e);
 
