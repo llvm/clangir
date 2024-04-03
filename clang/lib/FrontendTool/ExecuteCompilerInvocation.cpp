1a3f7c08b1b4b294ae5be91599b9bc6118553f8b
gitoleg <forown@yandex.ru>
2024-04-03 22:23:00 +0300

    [CIR] Introduce a flattening pass (#516)
    We start our journey towards `goto` support and this is a first step on
    this way.
    
    There are some discussion in #508 and according to the plan we do the
    following here:
    - a new pass called `cir-flatten-cfg` that is a stub now but later will
    be responsible for the regions inlining. The pass works only if
    `-emit-flat-cir` is passed in cmd line. Thus, the clang behavior is not
    changed here from the user's point of view.
    - The pass will be accomplished with `goto` solver later, so we talk
    about several passes that are mandatory for the lowering into `llvm`
    dialect. There are at least two clients of this pass that will be
    affected: `clang` itself and `cir-opt`, so we need a common point for
    them: and `populateCIRFlatteningPasses` and `populateCIRToLLVMPasses`
    guarantee that `CIR` will be in the correct state for all the clients,
    whatever new passes we will add later.

--- clang/lib/FrontendTool/ExecuteCompilerInvocation.cpp
+++ clang/lib/FrontendTool/ExecuteCompilerInvocation.cpp
@@ -52,7 +52,7 @@ CreateFrontendBaseAction(CompilerInstance &CI) {
 
   auto UseCIR = CI.getFrontendOpts().UseClangIRPipeline;
   auto Act = CI.getFrontendOpts().ProgramAction;
-  auto EmitsCIR = Act == EmitCIR || Act == EmitCIROnly;
+  auto EmitsCIR = Act == EmitCIR || Act == EmitCIRFlat || Act == EmitCIROnly;
 
   if (!UseCIR && EmitsCIR)
     llvm::report_fatal_error(
@@ -80,10 +80,13 @@ CreateFrontendBaseAction(CompilerInstance &CI) {
   case EmitBC:                 return std::make_unique<EmitBCAction>();
 #if CLANG_ENABLE_CIR
   case EmitCIR:                return std::make_unique<::cir::EmitCIRAction>();
+  case EmitCIRFlat:
+    return std::make_unique<::cir::EmitCIRFlatAction>();
   case EmitCIROnly:            return std::make_unique<::cir::EmitCIROnlyAction>();
   case EmitMLIR:               return std::make_unique<::cir::EmitMLIRAction>();
 #else
   case EmitCIR:
+  case EmitCIRFlat:
   case EmitCIROnly:
     llvm_unreachable("CIR suppport not built into clang");
 #endif
