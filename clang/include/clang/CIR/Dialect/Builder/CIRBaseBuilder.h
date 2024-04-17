e43c3875debcc0a1654dc1d2a0fb2a9fdafdfbe2
gitoleg <forown@yandex.ru>
2024-04-17 16:33:45 +0300

    changes after review

--- clang/include/clang/CIR/Dialect/Builder/CIRBaseBuilder.h
+++ clang/include/clang/CIR/Dialect/Builder/CIRBaseBuilder.h
@@ -140,15 +140,19 @@ public:
     return create<mlir::cir::StoreOp>(loc, val, dst, _volatile, order);
   }
 
-  mlir::Value createSub(mlir::Value lhs, mlir::Value rhs) {
-    return createBinop(lhs, mlir::cir::BinOpKind::Sub, rhs);
+  mlir::Value createSub(mlir::Value lhs, mlir::Value rhs,
+                        bool hasNUW = false, bool hasNSW = false) {
+    auto op = create<mlir::cir::BinOp>(lhs.getLoc(), lhs.getType(),
+                                       mlir::cir::BinOpKind::Sub, lhs, rhs);
+    if (hasNUW)
+      op.setNoUnsignedWrap(true);
+    if (hasNSW)
+      op.setNoSignedWrap(true);
+    return op;
   }
 
   mlir::Value createNSWSub(mlir::Value lhs, mlir::Value rhs) {
-    auto op = create<mlir::cir::BinOp>(lhs.getLoc(), lhs.getType(),
-                                       mlir::cir::BinOpKind::Sub, lhs, rhs);
-    op.setNoSignedWrap(true);
-    return op;
+    return createSub(lhs, rhs, false, true);
   }
 
   //===--------------------------------------------------------------------===//
