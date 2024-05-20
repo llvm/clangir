3828adb94d103388d4a91aca65985f29e464888b
seven-mile <i@7li.moe>
2024-05-20 10:07:47 +0800

    [CIR][NFC] Separate test for asm format

--- clang/test/CIR/CodeGen/address-space.c
+++ clang/test/CIR/CodeGen/address-space.c
@@ -3,14 +3,8 @@
 // RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -fclangir -S -emit-llvm %s -o %t.ll
 // RUN: FileCheck --input-file=%t.ll %s -check-prefix=LLVM
 
-// CIR: cir.func {{@.*foo.*}}(%arg0: !cir.ptr<!s32i>
-// LLVM: define void @foo(ptr %0)
-void foo(int __attribute__((address_space(0))) *arg) {
-  return;
-}
-
-// CIR: cir.func {{@.*bar.*}}(%arg0: !cir.ptr<!s32i, addrspace(1)>
-// LLVM: define void @bar(ptr addrspace(1) %0)
-void bar(int __attribute__((address_space(1))) *arg) {
+// CIR: cir.func {{@.*foo.*}}(%arg0: !cir.ptr<!s32i, addrspace(1)>
+// LLVM: define void @foo(ptr addrspace(1) %0)
+void foo(int __attribute__((address_space(1))) *arg) {
   return;
 }
