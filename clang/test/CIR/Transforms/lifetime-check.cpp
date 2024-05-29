8f3df8dce98b971ddfe3fcc0bab270c7c33bfb03
Chuanqi Xu <yedeng.yd@linux.alibaba.com>
2024-05-29 16:39:05 +0800

    [CIR] [Pipeline] Support -fclangir-analysis-only
    Close https://github.com/llvm/clangir/issues/633.
    
    See the issue for details.

--- clang/test/CIR/Transforms/lifetime-check.cpp
+++ clang/test/CIR/Transforms/lifetime-check.cpp
@@ -1,4 +1,5 @@
 // RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -fclangir -fclangir-lifetime-check="history=invalid,null" -clangir-verify-diagnostics -emit-cir %s -o %t.cir
+// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -fclangir-analysis-only -fclangir-lifetime-check="history=invalid,null" -clangir-verify-diagnostics -emit-obj %s -o /dev/null
 
 int *p0() {
   int *p = nullptr;
