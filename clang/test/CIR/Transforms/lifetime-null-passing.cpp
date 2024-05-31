5d4440d3588a4777a317d688aacf83c2e6f20e5a
Chuanqi Xu <yedeng.yd@linux.alibaba.com>
2024-05-31 10:43:21 +0800

    Address comments

--- clang/test/CIR/Transforms/lifetime-null-passing.cpp
+++ clang/test/CIR/Transforms/lifetime-null-passing.cpp
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 -std=c++20 -triple x86_64-unknown-linux-gnu -mconstructor-aliases -fclangir-analysis-only -fclangir-lifetime-check="history=all" -clangir-verify-diagnostics -emit-obj %s -o /dev/null
+// RUN: %clang_cc1 -std=c++20 -triple x86_64-unknown-linux-gnu -mconstructor-aliases -fclangir -clangir-disable-emit-cxx-default -fclangir-lifetime-check="history=all" -clangir-verify-diagnostics -emit-cir %s -o %t.cir
 
 class _j {};
 typedef _j* jobj;
--- clang/test/CIR/Transforms/lifetime-switch.cpp
+++ clang/test/CIR/Transforms/lifetime-switch.cpp
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 -std=c++17 -triple x86_64-unknown-linux-gnu -fclangir-analysis-only -fclangir-lifetime-check="history=invalid,null" -clangir-verify-diagnostics -emit-obj %s -o /dev/null
+// RUN: %clang_cc1 -std=c++17 -triple x86_64-unknown-linux-gnu -fclangir -fclangir-lifetime-check="history=invalid,null" -clangir-verify-diagnostics -emit-cir %s -o %t.cir
 
 void s0(int b) {
   int *p = nullptr;
