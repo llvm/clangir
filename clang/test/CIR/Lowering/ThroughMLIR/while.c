7b17ea5f419bb60c2d012dc14f07e4e0a5ff1d1f
GaoXiangYa <xiang.gao@terapines.com>
2024-06-07 14:38:48 +0800

    use regex to rewrite test file

--- clang/test/CIR/Lowering/ThroughMLIR/while.c
+++ clang/test/CIR/Lowering/ThroughMLIR/while.c
@@ -9,25 +9,25 @@ void foo() {
 }
 
 //CHECK: func.func @foo() {
-//CHECK:   %alloca = memref.alloca() {alignment = 4 : i64} : memref<i32>
-//CHECK:   %c0_i32 = arith.constant 0 : i32 
-//CHECK:   memref.store %c0_i32, %alloca[] : memref<i32> 
+//CHECK:   %[[alloca:.+]] = memref.alloca() {alignment = 4 : i64} : memref<i32>
+//CHECK:   %[[C0_I32:.+]] = arith.constant 0 : i32 
+//CHECK:   memref.store %[[C0_I32]], %[[alloca]][] : memref<i32> 
 //CHECK:   memref.alloca_scope  {
 //CHECK:     scf.while : () -> () {
-//CHECK:       %0 = memref.load %alloca[] : memref<i32> 
-//CHECK:       %c2_i32 = arith.constant 2 : i32 
-//CHECK:       %1 = arith.cmpi ult, %0, %c2_i32 : i32 
-//CHECK:       %2 = arith.extui %1 : i1 to i32 
-//CHECK:       %c0_i32_0 = arith.constant 0 : i32 
-//CHECK:       %3 = arith.cmpi ne, %2, %c0_i32_0 : i32 
-//CHECK:       %4 = arith.extui %3 : i1 to i8 
-//CHECK:       %5 = arith.trunci %4 : i8 to i1 
-//CHECK:       scf.condition(%5) 
+//CHECK:       %[[ZERO:.+]] = memref.load %[[alloca]][] : memref<i32> 
+//CHECK:       %[[C2_I32:.+]] = arith.constant 2 : i32 
+//CHECK:       %[[ONE:.+]] = arith.cmpi ult, %[[ZERO:.+]], %[[C2_I32]] : i32 
+//CHECK:       %[[TWO:.+]] = arith.extui %[[ONE:.+]] : i1 to i32 
+//CHECK:       %[[C0_I32_0:.+]] = arith.constant 0 : i32 
+//CHECK:       %[[THREE:.+]] = arith.cmpi ne, %[[TWO:.+]], %[[C0_I32_0]] : i32 
+//CHECK:       %[[FOUR:.+]] = arith.extui %[[THREE:.+]] : i1 to i8 
+//CHECK:       %[[FIVE:.+]] = arith.trunci %[[FOUR:.+]] : i8 to i1 
+//CHECK:       scf.condition(%[[FIVE]]) 
 //CHECK:     } do {
-//CHECK:       %0 = memref.load %alloca[] : memref<i32> 
-//CHECK:       %c1_i32 = arith.constant 1 : i32 
-//CHECK:       %1 = arith.addi %0, %c1_i32 : i32 
-//CHECK:       memref.store %1, %alloca[] : memref<i32> 
+//CHECK:       %[[ZERO:.+]] = memref.load %[[alloca]][] : memref<i32> 
+//CHECK:       %[[C1_I32:.+]] = arith.constant 1 : i32 
+//CHECK:       %[[ONE:.+]] = arith.addi %0, %[[C1_I32:.+]] : i32 
+//CHECK:       memref.store %[[ONE:.+]], %[[alloca]][] : memref<i32> 
 //CHECK:       scf.yield 
 //CHECK:     } 
 //CHECK:  } 
