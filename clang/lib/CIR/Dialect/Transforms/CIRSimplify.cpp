581d484fd60ced57b0a923e3f8bc536942941613
gitoleg <forown@yandex.ru>
2024-06-10 10:31:23 +0300

    adds unary and casts, removes doWhile

--- clang/lib/CIR/Dialect/Transforms/CIRSimplify.cpp
+++ clang/lib/CIR/Dialect/Transforms/CIRSimplify.cpp
@@ -14,6 +14,13 @@ using namespace mlir::cir;
 
 namespace {
 
+//TODO: there is something bad here 
+static bool has_one_user(mlir::Value val) {
+  unsigned users = 0;      
+  for (auto it = val.user_begin(); it != val.user_end(); ++it)  
+    users++;            
+  return users == 1;  
+}
 
 struct CIRSimplifyPass : public CIRSimplifyBase<CIRSimplifyPass> {
 
@@ -21,77 +28,75 @@ struct CIRSimplifyPass : public CIRSimplifyBase<CIRSimplifyPass> {
   void runOnOperation() override;
 };
 
-class SimplifyDoWhileZero
-    : public mlir::OpRewritePattern<mlir::cir::DoWhileOp> {
+class SimplifyCastOp
+    : public mlir::OpRewritePattern<mlir::cir::CastOp> {
 
-  bool isZero(mlir::Value v) const {
-    if (auto c = dyn_cast<mlir::cir::CastOp>(v.getDefiningOp()))
-      return isZero(c.getSrc());
-
-    auto c = dyn_cast<mlir::cir::ConstantOp>(v.getDefiningOp());
-    return c && c.isIntZero();
-  }
-
-  void replaceWithBranch(mlir::Operation *op, mlir::Block *dest,
-                         mlir::PatternRewriter &rewriter) const {
-    mlir::OpBuilder::InsertionGuard guard(rewriter);    
-    rewriter.setInsertionPoint(op);
-    rewriter.replaceOpWithNewOp<mlir::cir::BrOp>(op, dest);
-  }
-  
-  bool isSingleBlockScope(Operation* op) const {
-    auto scope = dyn_cast<mlir::cir::ScopeOp>(op);
-    return scope && scope.getScopeRegion().hasOneBlock();
+  bool isOpposite(mlir::cir::CastKind kind1, mlir::cir::CastKind kind2) const {
+    return kind1 == mlir::cir::CastKind::bool_to_int && kind2 == mlir::cir::CastKind::int_to_bool;
   }
 
 public:
-  using mlir::OpRewritePattern<
-      mlir::cir::DoWhileOp>::OpRewritePattern;
+  using mlir::OpRewritePattern<mlir::cir::CastOp>::OpRewritePattern;
 
   mlir::LogicalResult
-  matchAndRewrite(mlir::cir::DoWhileOp op,
+  matchAndRewrite(mlir::cir::CastOp op,
                   mlir::PatternRewriter &rewriter) const final {
+      auto val = op.getResult();
+      if (has_one_user(val)) {    
+        auto user = *val.user_begin();
+        if (auto cast = dyn_cast<mlir::cir::CastOp>(user)) {
+          if (isOpposite(op.getKind(), cast.getKind())) {
+            auto def = op.getSrc();
+            rewriter.replaceAllUsesWith(cast, def);
+            rewriter.eraseOp(cast);
+            rewriter.eraseOp(op);
+          }
+        }      
+      }
+      
+      return mlir::success();
+  }
+};
 
-    auto& cond = op.getCond().front();
-    auto conditionOp = cast<mlir::cir::ConditionOp>(cond.getTerminator());
 
-    if (!isZero(conditionOp.getCondition()) 
-        || !isSingleBlockScope(op->getParentOp()))
-      return mlir::failure();
+class SimplifyUnaryNot : public mlir::OpRewritePattern<mlir::cir::UnaryOp> {
 
-    auto scope = dyn_cast<mlir::cir::ScopeOp>(op->getParentOp());      
-    mlir::Block& scopeBody = scope.getScopeRegion().front();   
+  bool is_not(mlir::cir::UnaryOp op) const {
+    return op.getKind() == mlir::cir::UnaryOpKind::Not;
+  } 
 
-    auto *entry = rewriter.getInsertionBlock();
-    auto term = scopeBody.getTerminator();
-    auto* exit = scopeBody.splitBlock(term);
+public:
+  using mlir::OpRewritePattern<mlir::cir::UnaryOp>::OpRewritePattern;
+
+  mlir::LogicalResult
+  matchAndRewrite(mlir::cir::UnaryOp op,
+                  mlir::PatternRewriter &rewriter) const final {
+    if (!is_not(op) || !has_one_user(op.getResult()))
+      return mlir::success();
     
-    rewriter.setInsertionPointToEnd(entry);
-    rewriter.create<mlir::cir::BrOp>(op.getLoc(), &op.getEntry().front());
-
-    for (auto &b : op.getBody().getBlocks()) {
-      auto op = b.getTerminator();
-      if (isa<mlir::cir::BreakOp, 
-              mlir::cir::ContinueOp, 
-              mlir::cir::YieldOp>(op))
-        replaceWithBranch(op, exit, rewriter);
+    auto val = op.getResult();    
+    auto user = *val.user_begin();
+    if (auto unop = dyn_cast<mlir::cir::UnaryOp>(user)) {
+      if (!is_not(unop))
+        return mlir::success();               
+          rewriter.replaceAllUsesWith(unop, op.getInput());
+          rewriter.eraseOp(unop);
+          rewriter.eraseOp(op);
     }
 
-    rewriter.inlineRegionBefore(op.getBody(), exit);
-    rewriter.eraseOp(op);
-   
     return mlir::success();
+
   }
 };
 
 void CIRSimplifyPass::runOnOperation() {
   RewritePatternSet patterns(&getContext());
-  patterns.add<SimplifyDoWhileZero>(patterns.getContext());
+  patterns.add<SimplifyCastOp, SimplifyUnaryNot>(patterns.getContext());
 
   // Collect operations to apply patterns.
   SmallVector<Operation *, 16> ops;
   getOperation()->walk<mlir::WalkOrder::PostOrder>([&](Operation *op) {
-    if (isa<DoWhileOp>(op))
+    if (isa<mlir::cir::CastOp, mlir::cir::UnaryOp>(op))
       ops.push_back(op);
   });
 
