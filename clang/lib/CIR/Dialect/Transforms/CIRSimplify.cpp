0ce79ee328f1de6174069bcbc156587328b8590a
gitoleg <forown@yandex.ru>
2024-06-13 14:30:56 +0300

    tried a dummy fix-point solution

--- clang/lib/CIR/Dialect/Transforms/CIRSimplify.cpp
+++ clang/lib/CIR/Dialect/Transforms/CIRSimplify.cpp
@@ -8,6 +8,8 @@
 #include "clang/CIR/Dialect/Passes.h"
 
 #include <iostream>
+#include <set>
+
 
 using namespace mlir;
 using namespace mlir::cir;
@@ -20,8 +22,11 @@ static bool has_one_user(mlir::Value val) {
   for (auto it = val.user_begin(); it != val.user_end(); ++it)  
     users++;
 
+  if (!val.hasOneUse() && users == 1)
+    std::cout << "ALARM1!!!\n";
+
   if (val.hasOneUse() && users != 1)
-    std::cout << "ALARM!!!\n";
+    std::cout << "ALARM2!!!\n";
 
   return users == 1;  
 }
@@ -57,7 +62,7 @@ class SimplifyBoolCasts
     auto val = op.getResult();     
     if (is_int_or_bool_cast(op)) {
       casts.push_back(op);
-      if (has_one_user(val)) 
+      if (val.hasOneUse()) 
         if (auto next = dyn_cast<mlir::cir::CastOp>(*val.user_begin()))
           collect_casts(casts, next);        
     }
@@ -69,8 +74,8 @@ public:
   mlir::LogicalResult
   matchAndRewrite(mlir::cir::CastOp op,
                   mlir::PatternRewriter &rewriter) const final {
-      std::cout << "SimplifyBoolCasts: we are here\n";
-      return mlir::success();
+      // std::cout << "SimplifyBoolCasts: we are here\n";
+      // return mlir::success();
 
       chain_of_casts casts;
       collect_casts(casts, op);
@@ -123,12 +128,12 @@ public:
   mlir::LogicalResult
   matchAndRewrite(mlir::cir::UnaryOp op,
                   mlir::PatternRewriter &rewriter) const final {
-
-    if (!is_unary_not(op) || !has_one_user(op.getResult()))
-      return mlir::failure();
-    
+    if (!is_unary_not(op) || !op.getResult().hasOneUse())
+      return mlir::success();
+  
     auto val = op.getResult();    
     auto user = *val.user_begin();
+   
     if (auto next = dyn_cast<mlir::cir::UnaryOp>(user)) {
       next->dump();
       if (!is_unary_not(next))
@@ -136,9 +141,6 @@ public:
       rewriter.replaceAllUsesWith(next, op.getInput());
       rewriter.eraseOp(next);
       rewriter.eraseOp(op);
-      std::cout << "erase'\n";
-      op.dump();
-      next.dump();
       return mlir::success();
     }
 
@@ -146,25 +148,169 @@ public:
   }
 };
 
-void CIRSimplifyPass::runOnOperation() {
-  RewritePatternSet patterns(&getContext());
-  patterns.add<SimplifyBoolCasts>(patterns.getContext());
 
+
+  typedef std::vector<mlir::cir::CastOp> chain_of_casts;
+
+  bool is_bool_aimed(mlir::cir::CastOp op)  {
+    return op.getKind() == mlir::cir::CastKind::int_to_bool;
+  }
+
+  bool is_int_or_bool_cast(mlir::cir::CastOp op)  {
+    auto kind = op.getKind();
+    return kind == mlir::cir::CastKind::bool_to_int 
+          || kind == mlir::cir::CastKind::int_to_bool
+          || kind == mlir::cir::CastKind::integral;
+  }
+
+  // makes a chain of casts with the next properties:
+  // - each member is a cast of one of the next kinds: integral, bool_to_int, int_to_bool
+  // - each member except the last one may have only one user - the next member in the chain
+  // - last member may have one or many users
+  void collect_casts(chain_of_casts& casts, 
+                     mlir::cir::CastOp op)  {    
+    auto val = op.getResult();     
+    if (is_int_or_bool_cast(op)) {
+      casts.push_back(op);
+      if (val.hasOneUse()) 
+        if (auto next = dyn_cast<mlir::cir::CastOp>(*val.user_begin()))
+          collect_casts(casts, next);        
+    }
+  }
+
+bool runCastOp(mlir::cir::CastOp op, std::set<mlir::Operation*>& erased)  {
+      
+      chain_of_casts casts;
+      collect_casts(casts, op);
+
+      if (casts.size() < 2 || !is_bool_aimed(casts.back()))
+        return false;
+
+      auto first = casts.front();  
+      auto last = casts.back();
+
+      mlir::Value actual;
+      auto shift = 0;
+
+      // The next two cases are covered:
+      // 1) bool_to_int -> ... -> int_to_bool: remove all the casts, 
+      //    bool value was already obtained before all the conversions
+      // 2) int_to_bool -> ... -> int_to_bool: take the result of the first op, 
+      //    erase all others
+      if (first.getKind() == mlir::cir::CastKind::bool_to_int) { 
+        actual = first.getSrc();
+      } else if (first.getKind() == mlir::cir::CastKind::int_to_bool) {
+        actual = first.getResult();
+        shift++;
+      } else { // unexpected case
+        return false;  
+      }
+      std::cout << "replace\n";
+      last.dump();
+      actual.dump();
+      last.replaceAllUsesWith(actual);
+      auto it = casts.begin();
+      for (auto rit = casts.end() - 1; rit >= it + shift; --rit) {
+        std::cout << "erase1\n";
+        erased.emplace(*rit);
+       // (*rit)->erase();
+      }
+
+      return true;
+}
+
+// void CIRSimplifyPass::runOnOperation() {
+//   RewritePatternSet patterns(&getContext());
+//   patterns.add<SimplifyUnaryNot>(patterns.getContext());
+
+//   // Collect operations to apply patterns.
+//   SmallVector<Operation *, 16> ops;
+//   getOperation()->walk([&](Operation *op) {
+//     if (isa<mlir::cir::UnaryOp>(op))
+//       ops.push_back(op);
+//   });
+
+//   GreedyRewriteConfig config;
+//   config.enableRegionSimplification = false;
+
+//   // Apply patterns.
+//   if (applyOpPatternsAndFold(ops, std::move(patterns), config).failed()) 
+//     signalPassFailure();
+
+// }
+
+
+
+
+  static bool is_unary_not(mlir::cir::UnaryOp op) {
+    return op.getKind() == mlir::cir::UnaryOpKind::Not;
+  } 
+
+  static bool runUnOp(mlir::cir::UnaryOp op, std::set<mlir::Operation*>& erased) {
+    if (!is_unary_not(op) || !op.getResult().hasOneUse())
+      return false;
+    std::cout << "label1\n";
+    auto val = op.getResult();    
+    auto user = *val.user_begin();
+    std::cout << "label2\n";
+    user->dump();
+    std::cout << "label3\n";
+
+    if (auto next = dyn_cast_or_null<mlir::cir::UnaryOp>(user)) {
+      std::cout << "label4\n";
+  
+      if (!is_unary_not(next))
+        return false;
+      std::cout << "replace\n";
+      next.dump();
+      op.getInput().dump();
+      next.replaceAllUsesWith(op.getInput());
+      std::cout << "erase2\n";
+      // next->erase();
+      // op->erase();
+      erased.emplace(op);
+      erased.emplace(next);
+      return true;
+    }
+}
+
+
+void CIRSimplifyPass::runOnOperation() {
   // Collect operations to apply patterns.
   SmallVector<Operation *, 16> ops;
   getOperation()->walk([&](Operation *op) {
-    if (isa<mlir::cir::CastOp, mlir::cir::UnaryOp>(op)) {
-      std::cout << "push_back\n";
-      op->dump();
+    if (isa<mlir::cir::UnaryOp, mlir::cir::CastOp>(op))
       ops.push_back(op);
-    }
   });
 
-  // Apply patterns.
-  if (applyOpPatternsAndFold(ops, std::move(patterns)).failed()) 
-    signalPassFailure();
+  std::set<mlir::Operation*> erased;
 
+  bool changed = true;
+  while (changed) {
+    changed = false;  
+    std::cout << "iter\n";
+    for (auto op : ops) {
+      if (erased.count(op))
+        continue;
+      
+      if (auto uop = dyn_cast<UnaryOp>(op)) {
+        changed |= runUnOp(uop, erased);
+      }
+      else if (auto cast = dyn_cast<CastOp>(op)) {
+        changed |= runCastOp(cast, erased);
+      }
+    }
+  }
+
+  for (auto it = erased.rbegin(); it != erased.rend(); ++it)
+    (*it)->erase();
 }
+
+
+
+
+
+
 } // namespace
 
 std::unique_ptr<Pass> mlir::createCIRSimplifyPass() {
