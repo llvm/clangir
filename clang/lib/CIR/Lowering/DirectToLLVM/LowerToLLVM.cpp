51a884ec0b0c6af8cc3009a0ea508f42512b07e4
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2024-07-08 12:32:01 -0700

    [CIR][LowerToLLVM] Builtins: Lower cir.clear_cache to LLVM

--- clang/lib/CIR/Lowering/DirectToLLVM/LowerToLLVM.cpp
+++ clang/lib/CIR/Lowering/DirectToLLVM/LowerToLLVM.cpp
@@ -3336,6 +3336,26 @@ using CIRFMaxOpLowering =
 using CIRFMinOpLowering =
     CIRBinaryFPToFPBuiltinOpLowering<mlir::cir::FMinOp, mlir::LLVM::MinNumOp>;
 
+class CIRClearCacheOpLowering
+    : public mlir::OpConversionPattern<mlir::cir::ClearCacheOp> {
+public:
+  using OpConversionPattern<mlir::cir::ClearCacheOp>::OpConversionPattern;
+
+  mlir::LogicalResult
+  matchAndRewrite(mlir::cir::ClearCacheOp op, OpAdaptor adaptor,
+                  mlir::ConversionPatternRewriter &rewriter) const override {
+    auto begin = adaptor.getBegin();
+    auto end = adaptor.getEnd();
+    auto intrinNameAttr =
+        mlir::StringAttr::get(op.getContext(), "llvm.clear_cache");
+    rewriter.replaceOpWithNewOp<mlir::LLVM::CallIntrinsicOp>(
+        op, mlir::Type{}, intrinNameAttr, mlir::ValueRange{begin, end},
+        mlir::LLVM::FastmathFlags::none, llvm::ArrayRef<mlir::ValueRange>{});
+
+    return mlir::success();
+  }
+};
+
 void populateCIRToLLVMConversionPatterns(mlir::RewritePatternSet &patterns,
                                          mlir::TypeConverter &converter) {
   patterns.add<CIRReturnLowering>(patterns.getContext());
@@ -3365,7 +3385,8 @@ void populateCIRToLLVMConversionPatterns(mlir::RewritePatternSet &patterns,
       CIRCeilOpLowering, CIRFloorOpLowering, CIRFAbsOpLowering,
       CIRNearbyintOpLowering, CIRRintOpLowering, CIRRoundOpLowering,
       CIRTruncOpLowering, CIRCopysignOpLowering, CIRFMaxOpLowering,
-      CIRFMinOpLowering>(converter, patterns.getContext());
+      CIRFMinOpLowering, CIRClearCacheOpLowering>(converter,
+                                                  patterns.getContext());
 }
 
 namespace {
