4e4fcc119de6c5ac5863c81d9c4f7caab259a829
David Olsen <dolsen@nvidia.com>
2024-07-15 13:57:29 -0700

    [CIR] Change lowering test to check LLVM IR
    Change the lowering test atomic-runtime.cpp to check the LLVM IR rather
    than the LLVM MLIR dialect.  LLVM IR will be more stable over time.

--- clang/test/CIR/Lowering/atomic-runtime.cpp
+++ clang/test/CIR/Lowering/atomic-runtime.cpp
@@ -1,6 +1,5 @@
-// RUN: %clang_cc1 -std=c++17 -triple x86_64-unknown-linux-gnu -fclangir -emit-cir %s -o %t.cir
-// RUN: cir-opt %t.cir -cir-to-llvm -o %t.mlir
-// RUN: FileCheck --input-file=%t.mlir %s
+// RUN: %clang_cc1 -std=c++17 -triple x86_64-unknown-linux-gnu -fclangir -emit-llvm %s -o %t.ll
+// RUN: FileCheck --input-file=%t.ll %s
 
 // Test __atomic_* built-ins that have a memory order parameter with a runtime
 // value.  This requires generating a switch statement, so the amount of
@@ -12,27 +11,27 @@ int runtime_load(int *ptr, int order) {
   return __atomic_load_n(ptr, order);
 }
 
-// CHECK:  %[[T8:.*]] = llvm.load %[[T1:.*]] {alignment = 8 : i64} : !llvm.ptr -> !llvm.ptr
-// CHECK:  %[[T9:.*]] = llvm.load %[[T3:.*]] {alignment = 4 : i64} : !llvm.ptr -> i32
-// CHECK:  llvm.switch %[[T9]] : i32, ^[[BB1:.*]] [
-// CHECK:    1: ^[[BB2:.*]],
-// CHECK:    2: ^[[BB2]],
-// CHECK:    5: ^[[BB3:.*]]
-// CHECK:  ]
-// CHECK: [[BB1]]:
-// CHECK:  %[[T10:.*]] = llvm.load %[[T8]] atomic monotonic {alignment = 4 : i64} : !llvm.ptr -> i32
-// CHECK:  llvm.store %[[T10]], %[[T7:.*]] {alignment = 4 : i64} : i32, !llvm.ptr
-// CHECK:  llvm.br ^[[BB4:.*]]
-// CHECK: [[BB2]]:
-// CHECK:  %[[T11:.*]] = llvm.load %[[T8]] atomic acquire {alignment = 4 : i64} : !llvm.ptr -> i32
-// CHECK:  llvm.store %[[T11]], %[[T7]] {alignment = 4 : i64} : i32, !llvm.ptr
-// CHECK:  llvm.br ^[[BB4]]
-// CHECK: [[BB3]]:
-// CHECK:  %[[T12:.*]] = llvm.load %[[T8]] atomic seq_cst {alignment = 4 : i64} : !llvm.ptr -> i32
-// CHECK:  llvm.store %[[T12]], %[[T7]] {alignment = 4 : i64} : i32, !llvm.ptr
-// CHECK:  llvm.br ^[[BB4]]
-// CHECK: [[BB4]]:
-// CHECK:  %[[T13:.*]] = llvm.load %[[T7]] {alignment = 4 : i64} : !llvm.ptr -> i32
-// CHECK:  llvm.store %[[T13]], %[[T5:.*]] {alignment = 4 : i64} : i32, !llvm.ptr
-// CHECK:  %[[T14:.*]] = llvm.load %[[T5]] {alignment = 4 : i64} : !llvm.ptr -> i32
-// CHECK:  llvm.return %[[T14]] : i32
+// CHECK:   %[[T7:[0-9]+]] = load ptr, ptr %[[T3:[0-9]+]], align 8
+// CHECK:   %[[T8:[0-9]+]] = load i32, ptr %[[T4:[0-9]+]], align 4
+// CHECK:   switch i32 %[[T8]], label %[[L9:[0-9]+]] [
+// CHECK:     i32 1, label %[[L11:[0-9]+]]
+// CHECK:     i32 2, label %[[L11]]
+// CHECK:     i32 5, label %[[L13:[0-9]+]]
+// CHECK:   ]
+// CHECK: [[L9]]:
+// CHECK:   %[[T10:[0-9]+]] = load atomic i32, ptr %[[T7]] monotonic, align 4
+// CHECK:   store i32 %[[T10]], ptr %[[T6:[0-9]+]], align 4
+// CHECK:   br label %[[L15:[0-9]+]]
+// CHECK: [[L11]]:
+// CHECK:   %[[T12:[0-9]+]] = load atomic i32, ptr %[[T7]] acquire, align 4
+// CHECK:   store i32 %[[T12]], ptr %[[T6]], align 4
+// CHECK:   br label %[[L15]]
+// CHECK: [[L13]]:
+// CHECK:   %[[T14:[0-9]+]] = load atomic i32, ptr %[[T7]] seq_cst, align 4
+// CHECK:   store i32 %[[T14]], ptr %[[T6]], align 4
+// CHECK:   br label %[[L15]]
+// CHECK: [[L15]]:
+// CHECK:   %[[T16:[0-9]+]] = load i32, ptr %[[T6]], align 4
+// CHECK:   store i32 %[[T16]], ptr %[[T5:[0-9]+]], align 4
+// CHECK:   %[[T17:[0-9]+]] = load i32, ptr %[[T5]], align 4
+// CHECK:   ret i32 %[[T17]]
