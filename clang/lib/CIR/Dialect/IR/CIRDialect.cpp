bc0688ca169950f9e86851516458fa779cf3a41a
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2024-07-26 17:33:21 -0700

    [CIR][FlattenCFG] Exceptions: lower call to try_call and wire up landing pads

--- clang/lib/CIR/Dialect/IR/CIRDialect.cpp
+++ clang/lib/CIR/Dialect/IR/CIRDialect.cpp
@@ -2462,7 +2462,7 @@ void printCallCommon(Operation *op, mlir::Value indirectCallee,
     assert(landingPad && "expected two successors");
     auto tryCall = dyn_cast<mlir::cir::TryCallOp>(op);
     assert(tryCall && "regular calls do not branch");
-    state << tryCall.getCont();
+    state << ' ' << tryCall.getCont();
     if (!tryCall.getContOperands().empty()) {
       state << "(";
       state << tryCall.getContOperands();
@@ -2489,6 +2489,7 @@ void printCallCommon(Operation *op, mlir::Value indirectCallee,
   elidedAttrs.push_back("ast");
   elidedAttrs.push_back("extra_attrs");
   elidedAttrs.push_back("exception");
+  elidedAttrs.push_back("operandSegmentSizes");
 
   state.printOptionalAttrDict(op->getAttrs(), elidedAttrs);
   state << ' ' << ":";
@@ -2577,8 +2578,13 @@ void TryCallOp::print(::mlir::OpAsmPrinter &state) {
 
 mlir::SuccessorOperands TryCallOp::getSuccessorOperands(unsigned index) {
   assert(index < getNumSuccessors() && "invalid successor index");
-  return SuccessorOperands(index == 0 ? getContOperandsMutable()
-                                      : getLandingPadOperandsMutable());
+  if (index == 0)
+    return SuccessorOperands(getContOperandsMutable());
+  if (index == 1)
+    return SuccessorOperands(getLandingPadOperandsMutable());
+
+  // index == 2
+  return SuccessorOperands(getArgOperandsMutable());
 }
 
 //===----------------------------------------------------------------------===//
