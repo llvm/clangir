239026cfac7f7710552a1f21a6c2ae4739c84b90
7mile <i@7li.moe>
2024-08-03 06:17:29 +0800

    [CIR][Dialect] Emit OpenCL kernel argument metadata (#767)
    Similar to #705, this PR implements the remaining
    `genKernelArgMetadata()` logic.
    
    The attribute `cir.cl.kernel_arg_metadata` is also intentionally placed
    in the `cir.func`'s `extra_attrs` rather than `cir.func`'s standard
    `arg_attrs` list. Also, the metadata is stored by `Array` with proper
    verification on it. See the tablegen doc string for details.
    
    This is in order to
    * keep it side-by-side with `cl.kernel_metadata`.
    * still emit metadata when kernel has an *empty* arg list (see the test
    `kernel-arg-meatadata.cl`).
    * avoid horrors of repeating the long name `cir.cl.kernel_arg_metadata`
    for `numArgs` times.
    
    Because clangir doesn't support OpenCL built-in types and the `half`
    floating point type yet, their changes and test cases are not included.
    Corresponding missing feature flag is added.

--- clang/lib/CIR/CodeGen/CIRGenFunction.cpp
+++ clang/lib/CIR/CodeGen/CIRGenFunction.cpp
@@ -1724,8 +1724,7 @@ void CIRGenFunction::buildKernelMetadata(const FunctionDecl *FD,
   if (!FD->hasAttr<DeviceKernelAttr>() && !FD->hasAttr<CUDAGlobalAttr>())
     return;
 
-  // TODO(cir): CGM.genKernelArgMetadata(Fn, FD, this);
-  assert(!MissingFeatures::openCLGenKernelMetadata());
+  CGM.genKernelArgMetadata(Fn, FD, this);
 
   if (!getLangOpts().OpenCL)
     return;
