2c4538e0091fd43d612388dfe1907bde045e84bc
bruteforceboy <ogbonnachibuoyim12@gmail.com>
2024-08-23 11:49:21 +0300

    minor changes

--- clang/test/CIR/CodeGen/ssync-fetch-and-add.c
+++
@@ -1,9 +0,0 @@
-// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -fclangir -emit-cir %s -o - | FileCheck %s 
-
-#include <stdint.h>
-
-void foo(int64_t x) {
-  __sync_fetch_and_add(&x, 1);
-}
-
-// CHECK: %0 = cir.alloca !s64i, !cir.ptr<!s64i>, ["x", init] {alignment = 8 : i64}
--- clang/test/CIR/CodeGen/usync-fetch-and-add.c
+++
@@ -1,9 +0,0 @@
-// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -fclangir -emit-cir %s -o - | FileCheck %s 
-
-#include <stdint.h>
-
-void foo(uint64_t x) {
-  __sync_fetch_and_add(&x, 1);
-}
-
-// CHECK: %0 = cir.alloca !u64i, !cir.ptr<!u64i>, ["x", init] {alignment = 8 : i64}
