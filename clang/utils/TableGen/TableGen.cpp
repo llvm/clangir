e07dca4abe1f2fa6f74185512b66fe53349e7839
Sirui Mu <msrlancern@gmail.com>
2024-08-28 07:29:56 +0800

    [CIR][Lowering] Add the concept of simple lowering and use it for unary fp2fp operations (#806)
    This PR is the continuation and refinement of PR #434 which is
    originally authored by @philnik777 . Does not update it in-place since I
    don't have commit access to Nikolas' repo.
    
    This PR basically just rebases #434 onto the latest `main`. I also
    updated some naming used in the original PR to keep naming styles
    consistent.
    
    Co-authored-by: Nikolas Klauser <nikolasklauser@berlin.de>

--- clang/utils/TableGen/TableGen.cpp
+++ clang/utils/TableGen/TableGen.cpp
@@ -26,6 +26,7 @@ using namespace clang;
 enum ActionType {
   PrintRecords,
   DumpJSON,
+  GenCIRBuiltinsLowering,
   GenClangAttrClasses,
   GenClangAttrParserStringSwitches,
   GenClangAttrSubjectMatchRulesParserStringSwitches,
@@ -120,6 +121,9 @@ cl::opt<ActionType> Action(
                    "Print all records to stdout (default)"),
         clEnumValN(DumpJSON, "dump-json",
                    "Dump all records as machine-readable JSON"),
+        clEnumValN(GenCIRBuiltinsLowering, "gen-cir-builtins-lowering",
+                   "Generate lowering of ClangIR builtins to equivalent LLVM "
+                   "IR builtins"),
         clEnumValN(GenClangAttrClasses, "gen-clang-attr-classes",
                    "Generate clang attribute clases"),
         clEnumValN(GenClangAttrParserStringSwitches,
@@ -324,6 +328,9 @@ bool ClangTableGenMain(raw_ostream &OS, const RecordKeeper &Records) {
     break;
   case DumpJSON:
     EmitJSON(Records, OS);
+    break;
+  case GenCIRBuiltinsLowering:
+    EmitCIRBuiltinsLowering(Records, OS);
     break;
   case GenClangAttrClasses:
     EmitClangAttrClass(Records, OS);
