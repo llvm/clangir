8efd5c364f81adcffd41e2ebce0eba35206204be
bruteforceboy <ogbonnachibuoyim12@gmail.com>
2024-09-13 16:58:21 +0300

    [CIR][CodeGen] Fix packed structures

--- clang/test/CIR/CodeGen/packed-structs.c
+++ clang/test/CIR/CodeGen/packed-structs.c
@@ -21,8 +21,15 @@ typedef struct {
 
 
 // CHECK: !ty_A = !cir.struct<struct "A" packed {!cir.int<s, 32>, !cir.int<s, 8>}>
-// CHECK: !ty_C = !cir.struct<struct "C" packed {!cir.int<s, 32>, !cir.int<s, 8>}>
+// CHECK: !ty_C = !cir.struct<struct "C" packed {!cir.int<s, 32>, !cir.int<s, 8>, !cir.int<u, 8>}>
+// CHECK: !ty_D = !cir.struct<struct "D" packed {!cir.int<s, 8>, !cir.int<u, 8>, !cir.int<s, 32>}
+// CHECK: !ty_F = !cir.struct<struct "F" packed {!cir.int<s, 64>, !cir.int<s, 8>}
+// CHECK: !ty_E = !cir.struct<struct "E" packed {!cir.struct<struct "D" packed {!cir.int<s, 8>, !cir.int<u, 8>, !cir.int<s, 32>}
+// CHECK: !ty_G = !cir.struct<struct "G" {!cir.struct<struct "F" packed {!cir.int<s, 64>, !cir.int<s, 8>}
+// CHECK: !ty_H = !cir.struct<struct "H" {!cir.int<s, 32>, !cir.struct<union "anon.1" {!cir.int<s, 8>, !cir.int<s, 32>}
 // CHECK: !ty_B = !cir.struct<struct "B" packed {!cir.int<s, 32>, !cir.int<s, 8>, !cir.array<!cir.struct<struct "A" packed {!cir.int<s, 32>, !cir.int<s, 8>}> x 6>}>
+// CHECK: !ty_I = !cir.struct<struct "I" packed {!cir.int<s, 8>, !cir.struct<struct "H" {!cir.int<s, 32>, !cir.struct<union "anon.1" {!cir.int<s, 8>, !cir.int<s, 32>}
+// CHECK: !ty_J = !cir.struct<struct "J" packed {!cir.int<s, 8>, !cir.int<s, 8>, !cir.int<s, 8>, !cir.int<s, 8>, !cir.struct<struct "I" packed {!cir.int<s, 8>, !cir.struct<struct "H" {!cir.int<s, 32>, !cir.struct<union "anon.1" {!cir.int<s, 8>, !cir.int<s, 32>}
 
 // CHECK: cir.func {{.*@foo()}}
 // CHECK:  %0 = cir.alloca !ty_A, !cir.ptr<!ty_A>, ["a"] {alignment = 1 : i64}
@@ -34,4 +41,68 @@ void foo() {
     C c;
 }
 
+#pragma pack(2)
 
+typedef struct {
+    char b;
+    int c;
+} D;
+
+typedef struct {
+    D e;
+    int f;
+} E;
+
+// CHECK: cir.func {{.*@f1()}}
+// CHECK:  %0 = cir.alloca !ty_E, !cir.ptr<!ty_E>, ["a"] {alignment = 2 : i64}
+void f1() {
+    E a = {};
+}
+
+#pragma pack(1)
+
+typedef struct {
+    long b;
+    char c;
+} F;
+
+typedef struct {
+    F e;
+    char f;
+} G;
+
+// CHECK: cir.func {{.*@f2()}}
+// CHECK:  %0 = cir.alloca !ty_G, !cir.ptr<!ty_G>, ["a"] {alignment = 1 : i64}
+void f2() {
+    G a = {};
+}
+
+#pragma pack(1)
+
+typedef struct {
+    int d0;
+    union {
+        char null;
+        int val;
+    } value;
+} H;
+
+typedef struct {
+    char t;
+    H d;
+} I;
+
+typedef struct {
+    char a0;
+    char a1;
+    char a2;
+    char a3;
+    I c;
+    int a;
+} J;
+
+// CHECK: cir.func {{.*@f3()}}
+// CHECK:  %0 = cir.alloca !ty_J, !cir.ptr<!ty_J>, ["a"] {alignment = 1 : i64}
+void f3() {
+    J a = {0};
+}
