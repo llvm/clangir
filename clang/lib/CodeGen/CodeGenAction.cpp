63944ae5f6eb275f1e04e258ce31bae4c6e340e2
Chuanqi Xu <yedeng.yd@linux.alibaba.com>
2024-09-14 11:49:24 +0800

    update

--- clang/lib/CodeGen/CodeGenAction.cpp
+++ clang/lib/CodeGen/CodeGenAction.cpp
@@ -21,7 +21,6 @@
 #include "clang/Basic/LangStandard.h"
 #include "clang/Basic/SourceManager.h"
 #include "clang/Basic/TargetInfo.h"
-#include "clang/Config/config.h"
 #include "clang/CodeGen/BackendUtil.h"
 #include "clang/CodeGen/ModuleBuilder.h"
 #include "clang/Driver/DriverDiagnostic.h"
@@ -1035,20 +1034,14 @@ CodeGenAction::CreateASTConsumer(CompilerInstance &CI, StringRef InFile) {
     CI.getPreprocessor().addPPCallbacks(std::move(Callbacks));
   }
 
-  std::vector<std::unique_ptr<ASTConsumer>> AdditionalConsumers;
-  AdditionalConsumers.reserve(2);
-
   if (CI.getFrontendOpts().GenReducedBMI &&
       !CI.getFrontendOpts().ModuleOutputPath.empty()) {
-
-    AdditionalConsumers.push_back(std::make_unique<ReducedBMIGenerator>(
+    std::vector<std::unique_ptr<ASTConsumer>> Consumers(2);
+    Consumers[0] = std::make_unique<ReducedBMIGenerator>(
         CI.getPreprocessor(), CI.getModuleCache(),
-        CI.getFrontendOpts().ModuleOutputPath));
-  }
-
-  if (!AdditionalConsumers.empty()) {
-    AdditionalConsumers.push_back(std::move(Result));
-    return std::make_unique<MultiplexConsumer>(std::move(AdditionalConsumers));
+        CI.getFrontendOpts().ModuleOutputPath);
+    Consumers[1] = std::move(Result);
+    return std::make_unique<MultiplexConsumer>(std::move(Consumers));
   }
 
   return std::move(Result);
