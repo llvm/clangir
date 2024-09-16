7aa21cb5cbea051546b7e55375640eab565e03a7
gitoleg <forown@yandex.ru>
2024-09-16 21:42:56 +0300

    [CIR][CodeGen][BugFix] Fixes structures name collisions (#844)
    CIR Codegen fails to generate functions with local types with the same
    names. For instance, the next code :
    ```
    void foo(int a, float b) {
      struct A { int x; };
      struct A loc = {a};
      {
        struct A { float y; };
        struct A loc = {b};
      }
    }
    ```
    fails with on the next assertion: `Unable to find record layout
    information for type`. The problem is that we don't create record layout
    for the structures with equal names and
    `CIRGenTypes::convertRecordDeclType` returns the wrong type for the
    second struct type in the example above. This PR fixes this problem.
    
    In the original codegen the call to `Ty->setName(name)` resolves name
    collisions and assign a proper name for the type. In our case looks like
    we need to use the same approach as we did for the anonymous structures,
    i.e. to track the used names in the builder.
    
    Also, I fixed the struct type creation. Previously, the type was created
    several times - first in the `CIRGenTypes::convertRecordDeclType` and
    then in the `CIRGenTypes::computeRecordLayout`. This is why the indexes
    used by the anonymous structures naming had relatively big values and
    this is where the most changes on the tests come from.

--- clang/test/CIR/CodeGen/lambda.cpp
+++ clang/test/CIR/CodeGen/lambda.cpp
@@ -6,13 +6,13 @@ void fn() {
   a();
 }
 
-//      CHECK: !ty_anon2E2 = !cir.struct<class "anon.2" {!u8i}>
+//      CHECK: !ty_anon2E0 = !cir.struct<class "anon.0" {!u8i}>
 //  CHECK-DAG: module
 
 //      CHECK: cir.func lambda internal private @_ZZ2fnvENK3$_0clEv{{.*}}) extra
 
 //      CHECK:   cir.func @_Z2fnv()
-// CHECK-NEXT:     %0 = cir.alloca !ty_anon2E2, !cir.ptr<!ty_anon2E2>, ["a"]
+// CHECK-NEXT:     %0 = cir.alloca !ty_anon2E0, !cir.ptr<!ty_anon2E0>, ["a"]
 //      CHECK:   cir.call @_ZZ2fnvENK3$_0clEv
 
 void l0() {
@@ -23,15 +23,15 @@ void l0() {
 
 // CHECK: cir.func lambda internal private @_ZZ2l0vENK3$_0clEv({{.*}}) extra
 
-// CHECK: %0 = cir.alloca !cir.ptr<!ty_anon2E4>, !cir.ptr<!cir.ptr<!ty_anon2E4>>, ["this", init] {alignment = 8 : i64}
-// CHECK: cir.store %arg0, %0 : !cir.ptr<!ty_anon2E4>, !cir.ptr<!cir.ptr<!ty_anon2E4>>
-// CHECK: %1 = cir.load %0 : !cir.ptr<!cir.ptr<!ty_anon2E4>>, !cir.ptr<!ty_anon2E4>
-// CHECK: %2 = cir.get_member %1[0] {name = "i"} : !cir.ptr<!ty_anon2E4> -> !cir.ptr<!cir.ptr<!s32i>>
+// CHECK: %0 = cir.alloca !cir.ptr<!ty_anon2E2>, !cir.ptr<!cir.ptr<!ty_anon2E2>>, ["this", init] {alignment = 8 : i64}
+// CHECK: cir.store %arg0, %0 : !cir.ptr<!ty_anon2E2>, !cir.ptr<!cir.ptr<!ty_anon2E2>>
+// CHECK: %1 = cir.load %0 : !cir.ptr<!cir.ptr<!ty_anon2E2>>, !cir.ptr<!ty_anon2E2>
+// CHECK: %2 = cir.get_member %1[0] {name = "i"} : !cir.ptr<!ty_anon2E2> -> !cir.ptr<!cir.ptr<!s32i>>
 // CHECK: %3 = cir.load %2 : !cir.ptr<!cir.ptr<!s32i>>, !cir.ptr<!s32i>
 // CHECK: %4 = cir.load %3 : !cir.ptr<!s32i>, !s32i
 // CHECK: %5 = cir.const #cir.int<1> : !s32i
 // CHECK: %6 = cir.binop(add, %4, %5) nsw : !s32i
-// CHECK: %7 = cir.get_member %1[0] {name = "i"} : !cir.ptr<!ty_anon2E4> -> !cir.ptr<!cir.ptr<!s32i>>
+// CHECK: %7 = cir.get_member %1[0] {name = "i"} : !cir.ptr<!ty_anon2E2> -> !cir.ptr<!cir.ptr<!s32i>>
 // CHECK: %8 = cir.load %7 : !cir.ptr<!cir.ptr<!s32i>>, !cir.ptr<!s32i>
 // CHECK: cir.store %6, %8 : !s32i, !cir.ptr<!s32i>
 
@@ -45,15 +45,15 @@ auto g() {
   };
 }
 
-// CHECK: cir.func @_Z1gv() -> !ty_anon2E6
-// CHECK: %0 = cir.alloca !ty_anon2E6, !cir.ptr<!ty_anon2E6>, ["__retval"] {alignment = 8 : i64}
+// CHECK: cir.func @_Z1gv() -> !ty_anon2E3
+// CHECK: %0 = cir.alloca !ty_anon2E3, !cir.ptr<!ty_anon2E3>, ["__retval"] {alignment = 8 : i64}
 // CHECK: %1 = cir.alloca !s32i, !cir.ptr<!s32i>, ["i", init] {alignment = 4 : i64}
 // CHECK: %2 = cir.const #cir.int<12> : !s32i
 // CHECK: cir.store %2, %1 : !s32i, !cir.ptr<!s32i>
-// CHECK: %3 = cir.get_member %0[0] {name = "i"} : !cir.ptr<!ty_anon2E6> -> !cir.ptr<!cir.ptr<!s32i>>
+// CHECK: %3 = cir.get_member %0[0] {name = "i"} : !cir.ptr<!ty_anon2E3> -> !cir.ptr<!cir.ptr<!s32i>>
 // CHECK: cir.store %1, %3 : !cir.ptr<!s32i>, !cir.ptr<!cir.ptr<!s32i>>
-// CHECK: %4 = cir.load %0 : !cir.ptr<!ty_anon2E6>, !ty_anon2E6
-// CHECK: cir.return %4 : !ty_anon2E6
+// CHECK: %4 = cir.load %0 : !cir.ptr<!ty_anon2E3>, !ty_anon2E3
+// CHECK: cir.return %4 : !ty_anon2E3
 
 auto g2() {
   int i = 12;
@@ -65,15 +65,15 @@ auto g2() {
 }
 
 // Should be same as above because of NRVO
-// CHECK: cir.func @_Z2g2v() -> !ty_anon2E8
-// CHECK-NEXT: %0 = cir.alloca !ty_anon2E8, !cir.ptr<!ty_anon2E8>, ["__retval", init] {alignment = 8 : i64}
+// CHECK: cir.func @_Z2g2v() -> !ty_anon2E4
+// CHECK-NEXT: %0 = cir.alloca !ty_anon2E4, !cir.ptr<!ty_anon2E4>, ["__retval", init] {alignment = 8 : i64}
 // CHECK-NEXT: %1 = cir.alloca !s32i, !cir.ptr<!s32i>, ["i", init] {alignment = 4 : i64}
 // CHECK-NEXT: %2 = cir.const #cir.int<12> : !s32i
 // CHECK-NEXT: cir.store %2, %1 : !s32i, !cir.ptr<!s32i>
-// CHECK-NEXT: %3 = cir.get_member %0[0] {name = "i"} : !cir.ptr<!ty_anon2E8> -> !cir.ptr<!cir.ptr<!s32i>>
+// CHECK-NEXT: %3 = cir.get_member %0[0] {name = "i"} : !cir.ptr<!ty_anon2E4> -> !cir.ptr<!cir.ptr<!s32i>>
 // CHECK-NEXT: cir.store %1, %3 : !cir.ptr<!s32i>, !cir.ptr<!cir.ptr<!s32i>>
-// CHECK-NEXT: %4 = cir.load %0 : !cir.ptr<!ty_anon2E8>, !ty_anon2E8
-// CHECK-NEXT: cir.return %4 : !ty_anon2E8
+// CHECK-NEXT: %4 = cir.load %0 : !cir.ptr<!ty_anon2E4>, !ty_anon2E4
+// CHECK-NEXT: cir.return %4 : !ty_anon2E4
 
 int f() {
   return g2()();
@@ -82,10 +82,10 @@ int f() {
 //      CHECK: cir.func @_Z1fv() -> !s32i
 // CHECK-NEXT:   %0 = cir.alloca !s32i, !cir.ptr<!s32i>, ["__retval"] {alignment = 4 : i64}
 // CHECK-NEXT:   cir.scope {
-// CHECK-NEXT:     %2 = cir.alloca !ty_anon2E8, !cir.ptr<!ty_anon2E8>, ["ref.tmp0"] {alignment = 8 : i64}
-// CHECK-NEXT:     %3 = cir.call @_Z2g2v() : () -> !ty_anon2E8
-// CHECK-NEXT:     cir.store %3, %2 : !ty_anon2E8, !cir.ptr<!ty_anon2E8>
-// CHECK-NEXT:     %4 = cir.call @_ZZ2g2vENK3$_0clEv(%2) : (!cir.ptr<!ty_anon2E8>) -> !s32i
+// CHECK-NEXT:     %2 = cir.alloca !ty_anon2E4, !cir.ptr<!ty_anon2E4>, ["ref.tmp0"] {alignment = 8 : i64}
+// CHECK-NEXT:     %3 = cir.call @_Z2g2v() : () -> !ty_anon2E4
+// CHECK-NEXT:     cir.store %3, %2 : !ty_anon2E4, !cir.ptr<!ty_anon2E4>
+// CHECK-NEXT:     %4 = cir.call @_ZZ2g2vENK3$_0clEv(%2) : (!cir.ptr<!ty_anon2E4>) -> !s32i
 // CHECK-NEXT:     cir.store %4, %0 : !s32i, !cir.ptr<!s32i>
 // CHECK-NEXT:   }
 // CHECK-NEXT:   %1 = cir.load %0 : !cir.ptr<!s32i>, !s32i
@@ -114,8 +114,8 @@ int g3() {
 
 // 1. Use `operator int (*)(int const&)()` to retrieve the fnptr to `__invoke()`.
 // CHECK:     %3 = cir.scope {
-// CHECK:       %7 = cir.alloca !ty_anon2E11, !cir.ptr<!ty_anon2E11>, ["ref.tmp0"] {alignment = 1 : i64}
-// CHECK:       %8 = cir.call @_ZZ2g3vENK3$_0cvPFiRKiEEv(%7) : (!cir.ptr<!ty_anon2E11>) -> !cir.ptr<!cir.func<!s32i (!cir.ptr<!s32i>)>>
+// CHECK:       %7 = cir.alloca !ty_anon2E5, !cir.ptr<!ty_anon2E5>, ["ref.tmp0"] {alignment = 1 : i64}
+// CHECK:       %8 = cir.call @_ZZ2g3vENK3$_0cvPFiRKiEEv(%7) : (!cir.ptr<!ty_anon2E5>) -> !cir.ptr<!cir.func<!s32i (!cir.ptr<!s32i>)>>
 // CHECK:       %9 = cir.unary(plus, %8) : !cir.ptr<!cir.func<!s32i (!cir.ptr<!s32i>)>>, !cir.ptr<!cir.func<!s32i (!cir.ptr<!s32i>)>>
 // CHECK:       cir.yield %9 : !cir.ptr<!cir.func<!s32i (!cir.ptr<!s32i>)>>
 // CHECK:     }
