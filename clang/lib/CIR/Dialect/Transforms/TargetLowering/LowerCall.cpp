289ef0ffc6d4c54224e290726de5abc7edcd0dc2
Vinicius Couto Espindola <vini.couto.e@gmail.com>
2024-09-17 20:49:48 -0300

    Fix left over assert_or_abort statament

--- clang/lib/CIR/Dialect/Transforms/TargetLowering/LowerCall.cpp
+++ clang/lib/CIR/Dialect/Transforms/TargetLowering/LowerCall.cpp
@@ -147,7 +147,7 @@ void LowerModule::constructAttributeList(StringRef Name,
     [[fallthrough]];
   case ABIArgInfo::Direct:
     if (RetAI.getInReg())
-      assert_or_abort(!::cir::MissingFeatures::ABIInRegAttribute(), "NYI");
+      cir_assert_or_abort(!::cir::MissingFeatures::ABIInRegAttribute(), "NYI");
     assert(!::cir::MissingFeatures::noFPClass());
     break;
   case ABIArgInfo::Ignore:
@@ -253,7 +253,7 @@ const LowerFunctionInfo &LowerTypes::arrangeFunctionDeclaration(FuncOp fnOp) {
   // When declaring a function without a prototype, always use a
   // non-variadic type.
   if (fnOp.getNoProto()) {
-    assert_or_abort(!::cir::MissingFeatures::ABINoProtoFunctions(), "NYI");
+    cir_assert_or_abort(!::cir::MissingFeatures::ABINoProtoFunctions(), "NYI");
   }
 
   return arrangeFreeFunctionType(FTy);
