e23ae9456904baa62fdd486fd72f735803efc7ca
ghehg <he.guojin@gmail.com>
2024-09-27 09:02:04 -0700

    generate code for builtin call with FPContractMode=1 MathErrno=1

---
+++ clang/test/CIR/CodeGen/builtin-abort.c
@@ -0,0 +1,13 @@
+// RUN: %clang_cc1 -triple aarch64-none-linux-android21 -fclangir -emit-cir %s -o %t.cir
+// RUN: FileCheck --check-prefix=CIR --input-file=%t.cir %s
+// RUN: %clang_cc1 -triple aarch64-none-linux-android21 -fclangir -emit-llvm %s -o %t.ll
+// RUN: FileCheck --input-file=%t.ll %s -check-prefix=LLVM
+
+void abort();
+void test() { abort(); }
+
+// CIR-LABEL: test
+// CIR:  cir.call @abort() : () -> ()
+
+// LLVM-LABEL: test
+// LLVM:  call void @abort()
