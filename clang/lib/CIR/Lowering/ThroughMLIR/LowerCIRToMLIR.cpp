53f16c8106ddd9a239a8d889473b220bc2fd4567
Chuanqi Xu <yedeng.yd@linux.alibaba.com>
2024-10-01 05:50:21 +0800

    [CIR][CIRGen] Add time trace to several CIRGen pieces (#898)
    Then we can observe the time consumed in different part of CIR. This
    patch is not complete. But I think it is fine given we can always add
    them easily.

--- clang/lib/CIR/Lowering/ThroughMLIR/LowerCIRToMLIR.cpp
+++ clang/lib/CIR/Lowering/ThroughMLIR/LowerCIRToMLIR.cpp
@@ -57,6 +57,7 @@
 #include "llvm/ADT/Sequence.h"
 #include "llvm/ADT/SmallVector.h"
 #include "llvm/ADT/TypeSwitch.h"
+#include "llvm/Support/TimeProfiler.h"
 
 using namespace cir;
 using namespace llvm;
@@ -1506,6 +1507,7 @@ std::unique_ptr<llvm::Module>
 lowerFromCIRToMLIRToLLVMIR(mlir::ModuleOp theModule,
                            std::unique_ptr<mlir::MLIRContext> mlirCtx,
                            llvm::LLVMContext &llvmCtx) {
+  llvm::TimeTraceScope scope("Lower from CIR to MLIR To LLVM");
   mlir::PassManager pm(mlirCtx.get());
 
   pm.addPass(createConvertCIRToMLIRPass());
@@ -1560,6 +1562,8 @@ std::unique_ptr<mlir::Pass> createConvertCIRToMLIRPass() {
 
 mlir::ModuleOp lowerFromCIRToMLIR(mlir::ModuleOp theModule,
                                   mlir::MLIRContext *mlirCtx) {
+  llvm::TimeTraceScope scope("Lower CIR To MLIR");
+
   mlir::PassManager pm(mlirCtx);
   pm.addPass(createConvertCIRToMLIRPass());
 
