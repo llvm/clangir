b59a2e722052723370ad7f5081b4c64fb1a0084f
Chuanqi Xu <yedeng.yd@linux.alibaba.com>
2024-10-01 05:50:21 +0800

    [CIR][CIRGen] Add time trace to several CIRGen pieces (#898)
    Then we can observe the time consumed in different part of CIR. This
    patch is not complete. But I think it is fine given we can always add
    them easily.

--- clang/lib/CIR/Lowering/DirectToLLVM/LowerToLLVM.cpp
+++ clang/lib/CIR/Lowering/DirectToLLVM/LowerToLLVM.cpp
@@ -64,6 +64,7 @@
 #include "llvm/IR/DerivedTypes.h"
 #include "llvm/Support/Casting.h"
 #include "llvm/Support/ErrorHandling.h"
+#include "llvm/Support/TimeProfiler.h"
 #include <cstdint>
 #include <deque>
 #include <optional>
@@ -4325,6 +4326,8 @@ void ConvertCIRToLLVMPass::buildGlobalAnnotationsVar() {
 }
 
 void ConvertCIRToLLVMPass::runOnOperation() {
+  llvm::TimeTraceScope scope("Convert CIR to LLVM Pass");
+
   auto module = getOperation();
   mlir::DataLayout dataLayout(module);
   mlir::LLVMTypeConverter converter(&getContext());
@@ -4407,6 +4410,8 @@ extern void registerCIRDialectTranslation(mlir::MLIRContext &context);
 std::unique_ptr<llvm::Module>
 lowerDirectlyFromCIRToLLVMIR(mlir::ModuleOp theModule, LLVMContext &llvmCtx,
                              bool disableVerifier) {
+  llvm::TimeTraceScope scope("lower from CIR to LLVM directly");
+
   mlir::MLIRContext *mlirCtx = theModule.getContext();
   mlir::PassManager pm(mlirCtx);
   populateCIRToLLVMPasses(pm);
@@ -4437,6 +4442,8 @@ lowerDirectlyFromCIRToLLVMIR(mlir::ModuleOp theModule, LLVMContext &llvmCtx,
   mlir::registerLLVMDialectTranslation(*mlirCtx);
   mlir::registerOpenMPDialectTranslation(*mlirCtx);
   registerCIRDialectTranslation(*mlirCtx);
+
+  llvm::TimeTraceScope __scope("translateModuleToLLVMIR");
 
   auto ModuleName = theModule.getName();
   auto llvmModule = mlir::translateModuleToLLVMIR(
