0a632ef90769115764a65b1e1e7be1a1b78ded32
ghehg <166402688+ghehg@users.noreply.github.com>
2024-10-04 19:51:44 -0400

    [CIR][CIRGen][Builtin][Neon] Lower BI__builtin_neon_vmovn_v (#909)
    as title.
    The current implementation of this PR is use cir::CastOP integral
    casting to implement vector type truncation. Thus, LLVM lowering code
    has been change to accommodate it.
    In addition.
    Added code into
    [CIRGenBuiltinAArch64.cpp](https://github.com/llvm/clangir/pull/909/files#diff-6f7700013aa60ed524eb6ddcbab90c4dd288c384f9434547b038357868334932)
    to make it more similar to OG.
    ```
     mlir::Type ty = vTy;
      if (!ty)
    ```
    Added test case into neon.c as the file already contains similar vector
    move test cases such as vmovl
    
    ---------
    
    Co-authored-by: Guojin He <guojinhe@meta.com>

--- clang/lib/CIR/Lowering/DirectToLLVM/LowerToLLVM.cpp
+++ clang/lib/CIR/Lowering/DirectToLLVM/LowerToLLVM.cpp
@@ -581,10 +581,9 @@ public:
 };
 
 static mlir::Value getLLVMIntCast(mlir::ConversionPatternRewriter &rewriter,
-                                  mlir::Value llvmSrc,
-                                  mlir::IntegerType llvmDstIntTy,
-                                  bool isUnsigned, uint64_t cirDstIntWidth) {
-  auto cirSrcWidth = llvm::cast<mlir::IntegerType>(llvmSrc.getType()).getWidth();
+                                  mlir::Value llvmSrc, mlir::Type llvmDstIntTy,
+                                  bool isUnsigned, uint64_t cirSrcWidth,
+                                  uint64_t cirDstIntWidth) {
   if (cirSrcWidth == cirDstIntWidth)
     return llvmSrc;
 
@@ -642,7 +641,7 @@ public:
       auto llvmDstType = mlir::IntegerType::get(ctx, *layoutWidth);
       index = getLLVMIntCast(rewriter, index, llvmDstType,
                              ptrStrideOp.getStride().getType().isUnsigned(),
-                             *layoutWidth);
+                             width, *layoutWidth);
 
       // Rewrite the sub in front of extensions/trunc
       if (rewriteSub) {
@@ -758,11 +757,10 @@ public:
           mlir::cast<mlir::cir::IntType>(elementTypeIfVector(srcType));
       mlir::cir::IntType dstIntType =
           mlir::cast<mlir::cir::IntType>(elementTypeIfVector(dstType));
-      rewriter.replaceOp(castOp,
-                         getLLVMIntCast(rewriter, llvmSrcVal,
-                                        llvm::cast<mlir::IntegerType>(llvmDstType),
-                                        srcIntType.isUnsigned(),
-                                        dstIntType.getWidth()));
+      rewriter.replaceOp(
+          castOp, getLLVMIntCast(rewriter, llvmSrcVal, llvmDstType,
+                                 srcIntType.isUnsigned(), srcIntType.getWidth(),
+                                 dstIntType.getWidth()));
       break;
     }
     case mlir::cir::CastKind::floating: {
@@ -2526,8 +2524,8 @@ public:
 
     // Ensure shift amount is the same type as the value. Some undefined
     // behavior might occur in the casts below as per [C99 6.5.7.3].
-    amt = getLLVMIntCast(rewriter, amt, llvm::cast<mlir::IntegerType>(llvmTy),
-                         !cirAmtTy.isSigned(), cirValTy.getWidth());
+    amt = getLLVMIntCast(rewriter, amt, llvmTy, !cirAmtTy.isSigned(),
+                         cirAmtTy.getWidth(), cirValTy.getWidth());
 
     // Lower to the proper LLVM shift operation.
     if (op.getIsShiftleft())
@@ -2661,9 +2659,9 @@ static mlir::Value createLLVMBitOp(mlir::Location loc,
                                    operand.getType(), operand);
   }
 
-  return getLLVMIntCast(rewriter, op->getResult(0),
-                        llvm::cast<mlir::IntegerType>(resultTy),
-                        /*isUnsigned=*/true, resultIntTy.getWidth());
+  return getLLVMIntCast(rewriter, op->getResult(0), resultTy,
+                        /*isUnsigned=*/true, operandIntTy.getWidth(),
+                        resultIntTy.getWidth());
 }
 
 class CIRBitClrsbOpLowering
