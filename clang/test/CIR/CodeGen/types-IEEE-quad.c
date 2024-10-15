e096d264be703f61214ecefa9019d9690d43a2f7
M V V S Manoj Kumar <mvvsmanojkumar@gmail.com>
2024-10-15 11:21:08 +0530

    Modified test case to check for CIR and LLVM IR
    Modified test case to change prefix to check cir
    to CIR and to change prefect to check llvm ir to
    LLVM.

--- clang/test/CIR/CodeGen/types-IEEE-quad.c
+++ clang/test/CIR/CodeGen/types-IEEE-quad.c
@@ -1,37 +1,32 @@
 // RUN: %clang_cc1 -triple aarch64-none-linux-android21 -emit-cir %s -o %t.cir
-// RUN: FileCheck --input-file=%t.cir --check-prefix=CHECK %s
-// RUN: cir-opt %t.cir -cir-to-llvm -o %t.mlir
-// RUN: FileCheck --input-file=%t.mlir --check-prefix=CHECK-MLIR %s
+// RUN: FileCheck --input-file=%t.cir --check-prefix=CIR %s
+// RUN: %clang_cc1 -triple aarch64-none-linux-android21 -emit-llvm %s -o %t.ll
+// RUN: FileCheck --check-prefix=LLVM --input-file=%t.ll %s
 
 long double i = 0;
 long double t2(long double i2) {
     return i2 + i ; 
 }
 
-// CHECK: cir.global external @i = #cir.fp<0.000000e+00> : !cir.long_double<!cir.f128> {alignment = 16 : i64} loc({{.*}})
-// CHECK-LABEL:   cir.func @t2(%arg0: !cir.long_double<!cir.f128> loc({{.*}})) -> !cir.long_double<!cir.f128>
-// CHECK-NEXT:    %[[#I2:]] = cir.alloca !cir.long_double<!cir.f128>, !cir.ptr<!cir.long_double<!cir.f128>>, ["i2", init] {alignment = 16 : i64}
-// CHECK-NEXT:    %[[#RETVAL:]] = cir.alloca !cir.long_double<!cir.f128>, !cir.ptr<!cir.long_double<!cir.f128>>, ["__retval"] {alignment = 16 : i64}
-// CHECK-NEXT:    cir.store %arg0, %[[#I2]] : !cir.long_double<!cir.f128>, !cir.ptr<!cir.long_double<!cir.f128>>
-// CHECK-NEXT:    %[[#I2_LOAD:]] = cir.load %[[#I2]] : !cir.ptr<!cir.long_double<!cir.f128>>, !cir.long_double<!cir.f128>
-// CHECK-NEXT:    %[[#I:]] = cir.get_global @i : !cir.ptr<!cir.long_double<!cir.f128>>
-// CHECK-NEXT:    %[[#I_LOAD:]] = cir.load %[[#I]] : !cir.ptr<!cir.long_double<!cir.f128>>, !cir.long_double<!cir.f128>
-// CHECK-NEXT:    %[[#ADD:]] = cir.binop(add, %[[#I2_LOAD]], %[[#I_LOAD]]) : !cir.long_double<!cir.f128>
-// CHECK-NEXT:    cir.store %[[#ADD]], %[[#RETVAL]] : !cir.long_double<!cir.f128>, !cir.ptr<!cir.long_double<!cir.f128>>
-// CHECK-NEXT:    %[[#RETVAL_LOAD:]] = cir.load %[[#RETVAL]] : !cir.ptr<!cir.long_double<!cir.f128>>, !cir.long_double<!cir.f128>
-// CHECK-NEXT:    cir.return %[[#RETVAL_LOAD]] : !cir.long_double<!cir.f128>
+// CIR: cir.global external @i = #cir.fp<0.000000e+00> : !cir.long_double<!cir.f128> {alignment = 16 : i64} loc({{.*}})
+// CIR-LABEL:   cir.func @t2(%arg0: !cir.long_double<!cir.f128> loc({{.*}})) -> !cir.long_double<!cir.f128>
+// CIR-NEXT:    %[[#I2:]] = cir.alloca !cir.long_double<!cir.f128>, !cir.ptr<!cir.long_double<!cir.f128>>, ["i2", init] {alignment = 16 : i64}
+// CIR-NEXT:    %[[#RETVAL:]] = cir.alloca !cir.long_double<!cir.f128>, !cir.ptr<!cir.long_double<!cir.f128>>, ["__retval"] {alignment = 16 : i64}
+// CIR-NEXT:    cir.store %arg0, %[[#I2]] : !cir.long_double<!cir.f128>, !cir.ptr<!cir.long_double<!cir.f128>>
+// CIR-NEXT:    %[[#I2_LOAD:]] = cir.load %[[#I2]] : !cir.ptr<!cir.long_double<!cir.f128>>, !cir.long_double<!cir.f128>
+// CIR-NEXT:    %[[#I:]] = cir.get_global @i : !cir.ptr<!cir.long_double<!cir.f128>>
+// CIR-NEXT:    %[[#I_LOAD:]] = cir.load %[[#I]] : !cir.ptr<!cir.long_double<!cir.f128>>, !cir.long_double<!cir.f128>
+// CIR-NEXT:    %[[#ADD:]] = cir.binop(add, %[[#I2_LOAD]], %[[#I_LOAD]]) : !cir.long_double<!cir.f128>
+// CIR-NEXT:    cir.store %[[#ADD]], %[[#RETVAL]] : !cir.long_double<!cir.f128>, !cir.ptr<!cir.long_double<!cir.f128>>
+// CIR-NEXT:    %[[#RETVAL_LOAD:]] = cir.load %[[#RETVAL]] : !cir.ptr<!cir.long_double<!cir.f128>>, !cir.long_double<!cir.f128>
+// CIR-NEXT:    cir.return %[[#RETVAL_LOAD]] : !cir.long_double<!cir.f128>
 
-// CHECK-MLIR:          llvm.mlir.global external @i(0.000000e+00 : f128) {addr_space = 0 : i32, alignment = 16 : i64} : f128
-// CHECK-MLIR-LABEL:    llvm.func @t2(%arg0: f128) -> f128 attributes {cir.extra_attrs = #fn_attr, global_visibility = #cir<visibility default>}
-// CHECK-MLIR-NEXT:     %[[#CONST1:]] = llvm.mlir.constant(1 : index) : i64
-// CHECK-MLIR-NEXT:     %[[#OP1:]] = llvm.alloca %[[#CONST1]] x f128 {alignment = 16 : i64} : (i64) -> !llvm.ptr
-// CHECK-MLIR-NEXT:     %[[#CONST2:]] = llvm.mlir.constant(1 : index) : i64
-// CHECK-MLIR-NEXT:     %[[#RETVAL:]] = llvm.alloca %[[#CONST2]] x f128 {alignment = 16 : i64} : (i64) -> !llvm.ptr
-// CHECK-MLIR-NEXT:     llvm.store %arg0, %[[#OP1]] {alignment = 16 : i64} : f128, !llvm.ptr
-// CHECK-MLIR-NEXT:     %[[#OP1_LOAD:]] = llvm.load %[[#OP1]] {alignment = 16 : i64} : !llvm.ptr -> f128
-// CHECK-MLIR-NEXT:     %[[#OP2_ADDR:]] = llvm.mlir.addressof @i : !llvm.ptr
-// CHECK-MLIR-NEXT:     %[[#OP2_LOAD:]] = llvm.load %[[#OP2_ADDR]] {alignment = 16 : i64} : !llvm.ptr -> f128
-// CHECK-MLIR-NEXT:     %[[#OP_RES:]] = llvm.fadd %[[#OP1_LOAD]], %[[#OP2_LOAD]]  : f128
-// CHECK-MLIR-NEXT:     llvm.store %[[#OP_RES:]], %[[#RETVAL]] {alignment = 16 : i64} : f128, !llvm.ptr
-// CHECK-MLIR-NEXT:     %[[#RETVAL_LOAD:]] = llvm.load %[[#RETVAL]] {alignment = 16 : i64} : !llvm.ptr -> f128
-// CHECK-MLIR-NEXT:     llvm.return %[[#RETVAL_LOAD]] : f128
+//LLVM:         @i = global fp128 0xL00000000000000000000000000000000, align 16
+//LLVM-LABEL:   define dso_local fp128 @t2(fp128 noundef %i2)
+//LLVM-NEXT :   entry:
+//LLVM-NEXT :   %[[#I2_ADDR:]]= alloca fp128, align 16
+//LLVM-NEXT :   store fp128 %i2, ptr %[[#I2_ADDR]], align 16
+//LLVM-NEXT :   %[[#I2_LOAD:]] = load fp128, ptr %[[#I2_ADDR]], align 16
+//LLVM-NEXT :   %[[#I_LOAD:]] = load fp128, ptr @i, align 16
+//LLVM-NEXT :   %[[#RETVAL:]] = fadd fp128 %[[#I2_LOAD]], %[[#I_LOAD]]
+//LLVM-NEXT :   ret fp128 %[[#RETVAL]]
