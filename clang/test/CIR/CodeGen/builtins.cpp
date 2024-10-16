983ba317403cda5a13159d76c2ec64b7cf365330
ghehg <he.guojin@gmail.com>
2024-10-16 13:32:40 -0700

    no warning test

--- clang/test/CIR/CodeGen/builtins.cpp
+++ clang/test/CIR/CodeGen/builtins.cpp
@@ -8,52 +8,51 @@
 // This test file is a collection of test cases for all target-independent
 // builtins that are related to memory operations.
 
-int *test_addressof(int s) {
+int s;
+
+int *test_addressof() {
   return __builtin_addressof(s);
   
   // CIR-LABEL: test_addressof
-  // CIR: cir.store %arg0, [[ADDR:%.*]] : !s32i, !cir.ptr<!s32i>
+  // CIR: [[ADDR:%.*]] = cir.get_global @s : !cir.ptr<!s32i>
   // CIR: cir.store [[ADDR]], [[SAVE:%.*]] : !cir.ptr<!s32i>, !cir.ptr<!cir.ptr<!s32i>>
   // CIR: [[RES:%.*]] = cir.load [[SAVE]] : !cir.ptr<!cir.ptr<!s32i>>, !cir.ptr<!s32i>
   // CIR: cir.return [[RES]] : !cir.ptr<!s32i>
 
-  // LLVM: {{.*}}test_addressof{{.*}}(i32{{.*}}[[S:%.*]])
-  // LLVM: store i32 [[S]], ptr [[ADDR:%.*]], align 4
-  // LLVM: store ptr [[ADDR]], ptr [[SAVE:%.*]], align 8
-  // LLVM: [[RES:%.*]] = load ptr, ptr [[SAVE]], align 8
+  // LLVM-LABEL: test_addressof
+  // LLVM: store ptr @s, ptr [[ADDR:%.*]], align 8
+  // LLVM: [[RES:%.*]] = load ptr, ptr [[ADDR]], align 8
   // LLVM: ret ptr [[RES]]
 }
 
 namespace std { template<typename T> T *addressof(T &); }
-int *test_std_addressof(int s) {
+int *test_std_addressof() {
   return std::addressof(s);
   
   // CIR-LABEL: test_std_addressof
-  // CIR: cir.store %arg0, [[ADDR:%.*]] : !s32i, !cir.ptr<!s32i>
+  // CIR: [[ADDR:%.*]] = cir.get_global @s : !cir.ptr<!s32i>
   // CIR: cir.store [[ADDR]], [[SAVE:%.*]] : !cir.ptr<!s32i>, !cir.ptr<!cir.ptr<!s32i>>
   // CIR: [[RES:%.*]] = cir.load [[SAVE]] : !cir.ptr<!cir.ptr<!s32i>>, !cir.ptr<!s32i>
   // CIR: cir.return [[RES]] : !cir.ptr<!s32i>
 
-  // LLVM: {{.*}}test_std_addressof{{.*}}(i32{{.*}}[[S:%.*]])
-  // LLVM: store i32 [[S]], ptr [[ADDR:%.*]], align 4
-  // LLVM: store ptr [[ADDR]], ptr [[SAVE:%.*]], align 8
-  // LLVM: [[RES:%.*]] = load ptr, ptr [[SAVE]], align 8
+  // LLVM-LABEL: test_std_addressof
+  // LLVM: store ptr @s, ptr [[ADDR:%.*]], align 8
+  // LLVM: [[RES:%.*]] = load ptr, ptr [[ADDR]], align 8
   // LLVM: ret ptr [[RES]]
 }
 
 namespace std { template<typename T> T *__addressof(T &); }
-int *test_std_addressof2(int s) {
+int *test_std_addressof2() {
   return std::__addressof(s);
   
   // CIR-LABEL: test_std_addressof2
-  // CIR: cir.store %arg0, [[ADDR:%.*]] : !s32i, !cir.ptr<!s32i>
+  // CIR: [[ADDR:%.*]] = cir.get_global @s : !cir.ptr<!s32i>
   // CIR: cir.store [[ADDR]], [[SAVE:%.*]] : !cir.ptr<!s32i>, !cir.ptr<!cir.ptr<!s32i>>
   // CIR: [[RES:%.*]] = cir.load [[SAVE]] : !cir.ptr<!cir.ptr<!s32i>>, !cir.ptr<!s32i>
   // CIR: cir.return [[RES]] : !cir.ptr<!s32i>
 
-  // LLVM: {{.*}}test_std_addressof2{{.*}}(i32{{.*}}[[S:%.*]])
-  // LLVM: store i32 [[S]], ptr [[ADDR:%.*]], align 4
-  // LLVM: store ptr [[ADDR]], ptr [[SAVE:%.*]], align 8
-  // LLVM: [[RES:%.*]] = load ptr, ptr [[SAVE]], align 8
+  /// LLVM-LABEL: test_std_addressof2
+  // LLVM: store ptr @s, ptr [[ADDR:%.*]], align 8
+  // LLVM: [[RES:%.*]] = load ptr, ptr [[ADDR]], align 8
   // LLVM: ret ptr [[RES]]
 }
