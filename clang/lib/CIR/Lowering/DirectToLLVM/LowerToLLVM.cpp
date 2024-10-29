1e0645d3b2139fee8e00ed41d7a04e885c7f109e
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2024-10-29 16:30:20 -0700

    [CIR][Lowering] Transform cir.store of const arrays into cir.copy
    Add lowering prepare logic to lower stores to cir.copy.
    
    This bring LLVM lowering closer to OG and turns out the rest of the compiler
    understands memcpys better and generate better assembly code for at least arm64
    and x86_64.
    
    Note that current lowering to memcpy is only using i32 intrinsic version,
    this PR does not touch that code and that will be addressed in another PR.

--- clang/lib/CIR/Lowering/DirectToLLVM/LowerToLLVM.cpp
+++ clang/lib/CIR/Lowering/DirectToLLVM/LowerToLLVM.cpp
@@ -1481,6 +1481,7 @@ public:
     auto ordering = getLLVMMemOrder(memorder);
     auto alignOpt = op.getAlignment();
     unsigned alignment = 0;
+
     if (!alignOpt) {
       const auto llvmTy =
           getTypeConverter()->convertType(op.getValue().getType());
