d28538916962301f9cf5bac3f9f89e3d43f6a426
gitoleg <forown@yandex.ru>
2024-10-29 16:58:56 +0300

    fixed tests

--- clang/test/CIR/CallConvLowering/x86_64/varargs.c
+++ clang/test/CIR/CallConvLowering/x86_64/varargs.c
@@ -3,15 +3,15 @@
 int printf(const char *str, ...);
 
 // CHECK: cir.func {{.*@bar}}
-// CHECK:   %0 = cir.alloca !s32i, !cir.ptr<!s32i>, ["a", init]
-// CHECK:   %1 = cir.alloca !s32i, !cir.ptr<!s32i>, ["b", init]
-// CHECK:   cir.store %arg0, %0 : !s32i, !cir.ptr<!s32i>
-// CHECK:   cir.store %arg1, %1 : !s32i, !cir.ptr<!s32i>
-// CHECK:   %3 = cir.get_global @".str" : !cir.ptr<!cir.array<!s8i x 7>>
-// CHECK:   %4 = cir.cast(array_to_ptrdecay, %3 : !cir.ptr<!cir.array<!s8i x 7>>), !cir.ptr<!s8i>
-// CHECK:   %5 = cir.load %0 : !cir.ptr<!s32i>, !s32i
-// CHECK:   %6 = cir.load %1 : !cir.ptr<!s32i>, !s32i
-// CHECK:   %7 = cir.call @printf(%4, %5, %6) : (!cir.ptr<!s8i>, !s32i, !s32i) -> !s32i
+// CHECK:   %[[#V1:]] = cir.alloca !s32i, !cir.ptr<!s32i>, ["a", init]
+// CHECK:   %[[#V2:]] = cir.alloca !s32i, !cir.ptr<!s32i>, ["b", init]
+// CHECK:   cir.store %arg0, %[[#V0]] : !s32i, !cir.ptr<!s32i>
+// CHECK:   cir.store %arg1, %[[#V1]] : !s32i, !cir.ptr<!s32i>
+// CHECK:   %[[#V2:]] = cir.get_global @".str" : !cir.ptr<!cir.array<!s8i x 7>>
+// CHECK:   %[[#V3:]] = cir.cast(array_to_ptrdecay, %[[#V2]] : !cir.ptr<!cir.array<!s8i x 7>>), !cir.ptr<!s8i>
+// CHECK:   %[[#V4:]] = cir.load %[[#V1]] : !cir.ptr<!s32i>, !s32i
+// CHECK:   %[[#V5:]] = cir.load %[[#V2]] : !cir.ptr<!s32i>, !s32i
+// CHECK:   %[[#V6:]] = cir.call @printf(%[[#V3]], %[[#V4]], %[[#V5]]) : (!cir.ptr<!s8i>, !s32i, !s32i) -> !s32i
 void bar(int a, int b) {
   printf("%d %d\n", a, b);
 }
