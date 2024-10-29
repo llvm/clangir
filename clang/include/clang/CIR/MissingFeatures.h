bd3fce5e236630bfc426540e86b758cb3435633a
Nathan Lanza <nathanlanza@gmail.com>
2024-10-29 19:27:27 -0400

    fix
    Created using spr 1.3.5

--- clang/include/clang/CIR/MissingFeatures.h
+++ clang/include/clang/CIR/MissingFeatures.h
@@ -400,6 +400,8 @@ struct MissingFeatures {
 
   // This Itanium bit is currently being skipped in cir.
   static bool itaniumRecordLayoutBuilderFinishLayout() { return false; }
+
+  static bool mustProgress() { return false; }
 };
 
 } // namespace cir
