e2fadb6bcc2c71e6bd806a58d94982be5af2336d
Bruno Cardoso Lopes <bcardosolopes@users.noreply.github.com>
2024-11-05 10:49:43 -0800

    Update clang/lib/CIR/Dialect/IR/CIRTypes.cpp
    Co-authored-by: Sirui Mu <msrlancern@gmail.com>

--- clang/lib/CIR/Dialect/IR/CIRTypes.cpp
+++ clang/lib/CIR/Dialect/IR/CIRTypes.cpp
@@ -749,7 +749,7 @@ const llvm::fltSemantics &FP80Type::getFloatSemantics() const {
 llvm::TypeSize
 FP80Type::getTypeSizeInBits(const mlir::DataLayout &dataLayout,
                             mlir::DataLayoutEntryListRef params) const {
-  return llvm::TypeSize::getFixed(80);
+  return llvm::TypeSize::getFixed(128);
 }
 
 uint64_t FP80Type::getABIAlignment(const mlir::DataLayout &dataLayout,
