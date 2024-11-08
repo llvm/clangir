9d758ef3e64b6c04653335772aff6c50a29a989a
Shoaib Meenai <smeenai@fb.com>
2024-11-08 10:53:07 -0800

    Reformat

--- clang/lib/CIR/CodeGen/CIRGenBuiltin.cpp
+++ clang/lib/CIR/CodeGen/CIRGenBuiltin.cpp
@@ -1507,8 +1507,8 @@ RValue CIRGenFunction::buildBuiltinExpr(const GlobalDecl GD, unsigned BuiltinID,
     mlir::Attribute levelAttr = ConstantEmitter(*this).emitAbstract(
         E->getArg(0), E->getArg(0)->getType());
     int64_t level = mlir::cast<cir::IntAttr>(levelAttr).getSInt();
-    return RValue::get(builder.create<cir::ReturnAddrOp>(
-        loc, builder.getUInt32(level, loc)));
+    return RValue::get(
+        builder.create<cir::ReturnAddrOp>(loc, builder.getUInt32(level, loc)));
   }
   case Builtin::BI_ReturnAddress:
     llvm_unreachable("BI_ReturnAddress NYI");
