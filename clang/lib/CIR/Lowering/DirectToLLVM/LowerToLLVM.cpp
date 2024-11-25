d5820558609b22c9517fcc0af61a5ce7407f3d1a
Guojin <he.guojin@gmail.com>
2024-11-25 15:19:41 -0500

    [CIR][CIRGen][Builtin] Support __builtin_frame_address (#1137)

--- clang/lib/CIR/Lowering/DirectToLLVM/LowerToLLVM.cpp
+++ clang/lib/CIR/Lowering/DirectToLLVM/LowerToLLVM.cpp
@@ -3662,6 +3662,15 @@ mlir::LogicalResult CIRToLLVMReturnAddrOpLowering::matchAndRewrite(
   return mlir::success();
 }
 
+mlir::LogicalResult CIRToLLVMFrameAddrOpLowering::matchAndRewrite(
+    cir::FrameAddrOp op, OpAdaptor adaptor,
+    mlir::ConversionPatternRewriter &rewriter) const {
+  auto llvmPtrTy = mlir::LLVM::LLVMPointerType::get(rewriter.getContext());
+  replaceOpWithCallLLVMIntrinsicOp(rewriter, op, "llvm.frameaddress", llvmPtrTy,
+                                   adaptor.getOperands());
+  return mlir::success();
+}
+
 mlir::LogicalResult CIRToLLVMClearCacheOpLowering::matchAndRewrite(
     cir::ClearCacheOp op, OpAdaptor adaptor,
     mlir::ConversionPatternRewriter &rewriter) const {
@@ -3938,6 +3947,7 @@ void populateCIRToLLVMConversionPatterns(
       CIRToLLVMEhInflightOpLowering,
       CIRToLLVMEhTypeIdOpLowering,
       CIRToLLVMExpectOpLowering,
+      CIRToLLVMFrameAddrOpLowering,
       CIRToLLVMFreeExceptionOpLowering,
       CIRToLLVMFuncOpLowering,
       CIRToLLVMGetBitfieldOpLowering,
