0743920a1ce642324818c468a2aa872cfdf666f4
Guojin He <he.guojin@gmail.com>
2024-12-03 09:29:02 -0500

    [CIR][Dialect] Add BinOpKind_Max

--- clang/lib/CIR/Lowering/DirectToLLVM/LowerToLLVM.cpp
+++ clang/lib/CIR/Lowering/DirectToLLVM/LowerToLLVM.cpp
@@ -2547,6 +2547,15 @@ mlir::LogicalResult CIRToLLVMBinOpLowering::matchAndRewrite(
   case cir::BinOpKind::Xor:
     rewriter.replaceOpWithNewOp<mlir::LLVM::XOrOp>(op, lhs, rhs);
     break;
+  case cir::BinOpKind::Max:
+    if (mlir::isa<mlir::IntegerType>(llvmEltTy)) {
+      auto isUnsigned = isIntTypeUnsigned(type);
+      if (isUnsigned)
+        rewriter.replaceOpWithNewOp<mlir::LLVM::UMaxOp>(op, llvmTy, lhs, rhs);
+      else
+        rewriter.replaceOpWithNewOp<mlir::LLVM::SMaxOp>(op, llvmTy, lhs, rhs);
+    }
+    break;
   }
 
   return mlir::LogicalResult::success();
