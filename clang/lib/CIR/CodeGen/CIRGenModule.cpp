cde8db033355ff8156e81deb2233c166bf7f0ac2
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2024-12-20 14:54:46 -0300

    Revert "[CIR][CIRGen][TBAA] Add support for scalar types (#1220)"
    Fix https://github.com/llvm/clangir/issues/1246
    
    This reverts commit c94c04e5a104283e984c83b3b3f996de59f86673.

--- clang/lib/CIR/CodeGen/CIRGenModule.cpp
+++ clang/lib/CIR/CodeGen/CIRGenModule.cpp
@@ -4000,7 +4000,7 @@ cir::TBAAAttr CIRGenModule::getTBAABaseTypeInfo(QualType QTy) {
   return tbaa->getBaseTypeInfo(QTy);
 }
 
-cir::TBAAAttr CIRGenModule::getTBAAAccessTagInfo(TBAAAccessInfo tbaaInfo) {
+mlir::ArrayAttr CIRGenModule::getTBAAAccessTagInfo(TBAAAccessInfo tbaaInfo) {
   if (!tbaa) {
     return nullptr;
   }
