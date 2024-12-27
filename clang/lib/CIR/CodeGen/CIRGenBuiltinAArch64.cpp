4660d2c7dfd8ddc85c9a7f0fa49cf11dfcec47af
AmrDeveloper <amr96@programmer.net>
2024-12-27 16:30:30 +0100

    [CIR][CIRGen][Builtin][Neon] Lower vshrd_n_s64

--- clang/lib/CIR/CodeGen/CIRGenBuiltinAArch64.cpp
+++ clang/lib/CIR/CodeGen/CIRGenBuiltinAArch64.cpp
@@ -3810,7 +3810,11 @@ CIRGenFunction::emitAArch64BuiltinExpr(unsigned BuiltinID, const CallExpr *E,
     llvm_unreachable("NEON::BI__builtin_neon_vshld_n_u64 NYI");
   }
   case NEON::BI__builtin_neon_vshrd_n_s64: {
-    llvm_unreachable("NEON::BI__builtin_neon_vshrd_n_s64 NYI");
+    std::optional<llvm::APSInt> amt =
+        E->getArg(1)->getIntegerConstantExpr(getContext());
+    assert(amt && "Expected argument to be a constant");
+    uint64_t bits = std::min(static_cast<uint64_t>(63), amt->getZExtValue());
+    return builder.createShiftRight(Ops[0], bits);
   }
   case NEON::BI__builtin_neon_vshrd_n_u64: {
     llvm_unreachable("NEON::BI__builtin_neon_vshrd_n_u64 NYI");
