c8fc11f263d4231af5ad18448b4c196021b5d104
PikachuHy <pikachuhy@linux.alibaba.com>
2025-02-11 22:50:25 +0800

    [CIR][CIRGen][TBAA] Add support for scalar types (#1329)
    This patch introduces support for TBAA with scalar types. By encoding
    the type name in the CIR, we address the limitation of distinguishing
    between different C++ types that map to the same CIR type. For example,
    long and long long types.
    see https://github.com/llvm/clangir/issues/1241

--- clang/lib/CIR/CodeGen/CIRGenModule.cpp
+++ clang/lib/CIR/CodeGen/CIRGenModule.cpp
@@ -4101,7 +4101,7 @@ cir::TBAAAttr CIRGenModule::getTBAABaseTypeInfo(QualType QTy) {
   return tbaa->getBaseTypeInfo(QTy);
 }
 
-mlir::ArrayAttr CIRGenModule::getTBAAAccessTagInfo(TBAAAccessInfo tbaaInfo) {
+cir::TBAAAttr CIRGenModule::getTBAAAccessTagInfo(TBAAAccessInfo tbaaInfo) {
   if (!tbaa) {
     return nullptr;
   }
