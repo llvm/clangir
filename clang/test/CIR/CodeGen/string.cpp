2379cae9da51632c4e2664911abf6fdce86bdd34
bruteforceboy <ogbonnachibuoyim12@gmail.com>
2025-02-25 16:04:18 +0300

    clang -> clang++

--- clang/test/CIR/CodeGen/string.cpp
+++ clang/test/CIR/CodeGen/string.cpp
@@ -1,4 +1,4 @@
-// RUN: %clang -std=c++20 -target x86_64-unknown-linux-gnu -fclangir -emit-cir %s -o %t.cir
+// RUN: %clang++ -std=c++20 -target x86_64-unknown-linux-gnu -fclangir -emit-cir %s -o %t.cir
 // RUN: FileCheck --input-file=%t.cir %s
 
 #include <iostream>
