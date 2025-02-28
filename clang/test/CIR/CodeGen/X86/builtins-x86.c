19f23dcdd23d9a409ba8230e2c55b7981d70568b
shrikardongre <shrikardongre@gmail.com>
2025-02-28 23:40:22 +0530

    [CIR][CIRGen][builtin] handle _mm_prefetch

--- clang/test/CIR/CodeGen/X86/builtins-x86.c
+++ clang/test/CIR/CodeGen/X86/builtins-x86.c
@@ -3,7 +3,7 @@
 // RUN: FileCheck --check-prefix=CIR --input-file=%t.cir %s
 // RUN: %clang_cc1 -triple x86_64-unknown-linux -Wno-implicit-function-declaration -fclangir -emit-llvm -o %t.ll %s
 // RUN: FileCheck --check-prefix=LLVM --input-file=%t.ll %s
-# include <emmintrin.h> 
+
 void test_mm_clflush(const void* tmp_vCp) {
   // CIR-LABEL: test_mm_clflush
   // LLVM-LABEL: test_mm_clflush
@@ -13,9 +13,9 @@ void test_mm_clflush(const void* tmp_vCp) {
 }
 
 void test_mm_prefetch(char const* p) {
- // CIR-LABEL: test_mm_prefetch
-  // LLVM-LABEL: test_mm_prefetch
-  _mm_prefetch(p, 0);
-  // CIR: {{%.}} = cir.llvm.intrinsic "llvm.prefetch.p0" {{%.}}, {{%.}} : (!cir.ptr<!char>, !cir.i32) -> !void
-  // LLVM: call void @llvm.prefetch.p0(ptr {{.}}, i32 0, i32 0, i32 1)
-}
+  // CIR-LABEL: test_mm_prefetch
+   // LLVM-LABEL: test_mm_prefetch
+   _mm_prefetch(p, 0);
+   // CIR: {{%.}} = cir.llvm.intrinsic "llvm.prefetch.p0" {{%.}}, {{%.}} : (!cir.ptr<!char>, !cir.i32) -> !void
+   // LLVM: call void @llvm.prefetch.p0(ptr {{.}}, i32 0, i32 0, i32 1)
+ }
