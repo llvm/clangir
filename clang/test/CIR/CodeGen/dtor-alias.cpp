0aceb4a28572bc2d80ef9a81e8d5faf4d7cb3d2d
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2025-03-05 16:13:20 -0800

    [CIR] Remove -clangir-disable-passes from test
    Close https://github.com/llvm/clangir/issues/1234
    Update test after fix in  https://github.com/llvm/clangir/pull/1422

--- clang/test/CIR/CodeGen/dtor-alias.cpp
+++ clang/test/CIR/CodeGen/dtor-alias.cpp
@@ -1,8 +1,6 @@
-// FIXME: Remove of -clangir-disable-passes may trigger a memory safe bug in CIR internally during
-// lowering
 // RUN: %clang_cc1 -std=c++17 -triple x86_64-unknown-linux-gnu \
 // RUN:   -mconstructor-aliases -fclangir -emit-cir %s -o %t.cir \
-// RUN:   -clangir-disable-passes -o %t.cir
+// RUN:   -o %t.cir
 // RUN: FileCheck %s --input-file=%t.cir
 
 namespace {
