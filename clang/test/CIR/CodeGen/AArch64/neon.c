f26fff3f331f75907b66c5c0cb7ac4ee3cbdc239
Amr Hesham <amr96@programmer.net>
2025-03-11 03:49:45 +0100

    [CIR][CIRGen][Builtin][Neon] Lower neon vcages_f32 (#1449)
    Lower neon vcages_f32

--- clang/test/CIR/CodeGen/AArch64/neon.c
+++ clang/test/CIR/CodeGen/AArch64/neon.c
@@ -15107,12 +15107,16 @@ int16_t test_vqmovns_s32(int32_t a) {
 //   return (uint64_t)vcltzd_f64(a);
 // }
 
-// NYI-LABEL: @test_vcages_f32(
-// NYI:   [[VCAGES_F32_I:%.*]] = call i32 @llvm.aarch64.neon.facge.i32.f32(float %a, float %b)
-// NYI:   ret i32 [[VCAGES_F32_I]]
-// uint32_t test_vcages_f32(float32_t a, float32_t b) {
-//   return (uint32_t)vcages_f32(a, b);
-// }
+uint32_t test_vcages_f32(float32_t a, float32_t b) {
+  return (uint32_t)vcages_f32(a, b);
+
+  // CIR-LABEL: vcages_f32
+  // CIR: [[TMP0:%.*]] = cir.llvm.intrinsic "aarch64.neon.facge" {{.*}}, {{.*}} : (!cir.float, !cir.float) -> !u32i
+
+  // LLVM-LABEL: @test_vcages_f32(
+  // LLVM:   [[VCAGED_F32_I:%.*]] = call i32 @llvm.aarch64.neon.facge.i32.f32(float %0, float %1)
+  // LLVM:   ret i32 [[VCAGED_F32_I]]
+}
 
 uint64_t test_vcaged_f64(float64_t a, float64_t b) {
   return (uint64_t)vcaged_f64(a, b);
