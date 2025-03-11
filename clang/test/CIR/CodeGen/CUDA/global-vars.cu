b076ef38d490e0d6ea7ce6f8c5025c5a3ea7cd03
Mate Marjanović <sharp.edged2@gmail.com>
2025-03-11 10:44:47 +0000

    Decorate global CUDA variables with appropriate CIR attribute.

--- clang/test/CIR/CodeGen/CUDA/global-vars.cu
+++ clang/test/CIR/CodeGen/CUDA/global-vars.cu
@@ -10,10 +10,17 @@
 // RUN:            %s -o %t.cir
 // RUN: FileCheck --check-prefix=LLVM-DEVICE --input-file=%t.cir %s
 
+// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -fclangir \
+// RUN:            -x cuda -emit-cir -target-sdk-version=12.3 \
+// RUN:            %s -o %t.cir
+// RUN: FileCheck --check-prefix=CIR-HOST --input-file=%t.cir %s
+
 __device__ int a;
 // CIR-DEVICE: cir.global external addrspace(offload_global) @a = #cir.int<0>
 // LLVM-DEVICE: @a = addrspace(1) externally_initialized global i32 0, align 4
+// CIR-HOST: {{.*}}cir.global external @a = #cir.undef : !s32i {alignment = 4 : i64, cu.shadow_name = #cir.cu.shadow_name<a>}{{.*}}
 
 __shared__ int shared;
 // CIR-DEVICE: cir.global external addrspace(offload_local) @shared = #cir.undef
 // LLVM-DEVICE: @shared = addrspace(3) global i32 undef, align 4
+// CIR-HOST: {{.*}}cir.global external @shared = #cir.undef : !s32i {alignment = 4 : i64, cu.shadow_name = #cir.cu.shadow_name<shared>}{{.*}}
