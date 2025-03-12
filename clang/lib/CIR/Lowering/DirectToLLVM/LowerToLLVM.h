1a07ec63733ef1d5e9ab09c69a8a5879333753e2
Letu Ren <fantasquex@gmail.com>
2025-03-13 02:24:48 +0800

    [CIR][CIRGen][builtin] handle `__lzcnt` (#1382)
    Traditional clang implementation:
    https://github.com/llvm/clangir/blob/a1ab6bf6cd3b83d0982c16f29e8c98958f69c024/clang/lib/CodeGen/CGBuiltin.cpp#L3618-L3632
    
    The problem here is that `__builtin_clz` allows undefined result, while
    `__lzcnt` doesn't. As a result, I have to create a new CIR for
    `__lzcnt`. Since the return type of those two builtin differs, I decided
    to change return type of current `CIR_BitOp` to allow new `CIR_LzcntOp`
    to inherit from it.
    
    I would like to hear your suggestions. C.c. @Lancern

--- clang/lib/CIR/Lowering/DirectToLLVM/LowerToLLVM.h
+++ clang/lib/CIR/Lowering/DirectToLLVM/LowerToLLVM.h
@@ -73,12 +73,6 @@ mlir::LLVM::CallIntrinsicOp replaceOpWithCallLLVMIntrinsicOp(
     const llvm::Twine &intrinsicName, mlir::Type resultTy,
     mlir::ValueRange operands);
 
-mlir::Value createLLVMBitOp(mlir::Location loc,
-                            const llvm::Twine &llvmIntrinBaseName,
-                            mlir::Type resultTy, mlir::Value operand,
-                            std::optional<bool> poisonZeroInputFlag,
-                            mlir::ConversionPatternRewriter &rewriter);
-
 class CIRToLLVMCopyOpLowering : public mlir::OpConversionPattern<cir::CopyOp> {
 public:
   using mlir::OpConversionPattern<cir::CopyOp>::OpConversionPattern;
--- clang/test/CIR/CodeGen/builtin-bits.cpp
+++ clang/test/CIR/CodeGen/builtin-bits.cpp
@@ -5,182 +5,180 @@ int test_builtin_clrsb(int x) {
   return __builtin_clrsb(x);
 }
 
-// CIR: cir.func @_Z18test_builtin_clrsbi
-// CIR:   %{{.+}} = cir.bit.clrsb(%{{.+}} : !s32i) : !s32i
-// CIR: }
+// CIR-LABEL: _Z18test_builtin_clrsbi
+// CIR: [[TMP:%.+]] = cir.bit.clrsb(%{{.+}} : !s32i) : !s32i
 
 int test_builtin_clrsbl(long x) {
   return __builtin_clrsbl(x);
 }
 
-// CIR: cir.func @_Z19test_builtin_clrsbll
-// CIR:   %{{.+}} = cir.bit.clrsb(%{{.+}} : !s64i) : !s32i
-// CIR: }
+// CIR-LABEL: _Z19test_builtin_clrsbll
+// CIR: [[TMP:%.+]] = cir.bit.clrsb(%{{.+}} : !s64i) : !s64i
+// CIR: {{%.+}} = cir.cast(integral, [[TMP]] : !s64i), !s32i
 
 int test_builtin_clrsbll(long long x) {
   return __builtin_clrsbll(x);
 }
 
-// CIR: cir.func @_Z20test_builtin_clrsbllx
-// CIR:   %{{.+}} = cir.bit.clrsb(%{{.+}} : !s64i) : !s32i
-// CIR: }
+// CIR-LABEL: _Z20test_builtin_clrsbllx
+// CIR: [[TMP:%.+]] = cir.bit.clrsb(%{{.+}} : !s64i) : !s64i
+// CIR: {{%.+}} = cir.cast(integral, [[TMP]] : !s64i), !s32i
 
 int test_builtin_ctzs(unsigned short x) {
   return __builtin_ctzs(x);
 }
 
-// CIR: cir.func @_Z17test_builtin_ctzst
-// CIR:   %{{.+}} = cir.bit.ctz(%{{.+}} : !u16i) : !s32i
-// CHEKC: }
+// CIR-LABEL: _Z17test_builtin_ctzst
+// CIR: [[TMP:%.+]] = cir.bit.ctz(%{{.+}} : !u16i) zero_poison : !u16i
+// CIR: {{%.+}} = cir.cast(integral, [[TMP]] : !u16i), !s32i
 
 int test_builtin_ctz(unsigned x) {
   return __builtin_ctz(x);
 }
 
-// CIR: cir.func @_Z16test_builtin_ctzj
-// CIR:   %{{.+}} = cir.bit.ctz(%{{.+}} : !u32i) : !s32i
-// CIR: }
+// CIR-LABEL: _Z16test_builtin_ctzj
+// CIR: [[TMP:%.+]] = cir.bit.ctz(%{{.+}} : !u32i) zero_poison : !u32i
+// CIR: {{%.+}} = cir.cast(integral, [[TMP]] : !u32i), !s32i
 
 int test_builtin_ctzl(unsigned long x) {
   return __builtin_ctzl(x);
 }
 
-// CIR: cir.func @_Z17test_builtin_ctzlm
-// CIR:   %{{.+}} = cir.bit.ctz(%{{.+}} : !u64i) : !s32i
-// CIR: }
+// CIR-LABEL: _Z17test_builtin_ctzlm
+// CIR: [[TMP:%.+]] = cir.bit.ctz(%{{.+}} : !u64i) zero_poison : !u64i
+// CIR: {{%.+}} = cir.cast(integral, [[TMP]] : !u64i), !s32i
 
 int test_builtin_ctzll(unsigned long long x) {
   return __builtin_ctzll(x);
 }
 
-// CIR: cir.func @_Z18test_builtin_ctzlly
-// CIR:   %{{.+}} = cir.bit.ctz(%{{.+}} : !u64i) : !s32i
-// CIR: }
+// CIR-LABEL: _Z18test_builtin_ctzlly
+// CIR: [[TMP:%.+]] = cir.bit.ctz(%{{.+}} : !u64i) zero_poison : !u64i
+// CIR: {{%.+}} = cir.cast(integral, [[TMP]] : !u64i), !s32i
 
 int test_builtin_ctzg(unsigned x) {
   return __builtin_ctzg(x);
 }
 
-// CIR: cir.func @_Z17test_builtin_ctzgj
-// CIR:   %{{.+}} = cir.bit.ctz(%{{.+}} : !u32i) : !s32i
-// CIR: }
+// CIR-LABEL: _Z17test_builtin_ctzgj
+// CIR: [[TMP:%.+]] = cir.bit.ctz(%{{.+}} : !u32i) zero_poison : !u32i
+// CIR: {{%.+}} = cir.cast(integral, [[TMP]] : !u32i), !s32i
 
 int test_builtin_clzs(unsigned short x) {
   return __builtin_clzs(x);
 }
 
-// CIR: cir.func @_Z17test_builtin_clzst
-// CIR:   %{{.+}} = cir.bit.clz(%{{.+}} : !u16i) : !s32i
-// CIR: }
+// CIR-LABEL: _Z17test_builtin_clzst
+// CIR: [[TMP:%.+]] = cir.bit.clz(%{{.+}} : !u16i) zero_poison : !u16i
+// CIR: {{%.+}} = cir.cast(integral, [[TMP]] : !u16i), !s32i
 
 int test_builtin_clz(unsigned x) {
   return __builtin_clz(x);
 }
 
-// CIR: cir.func @_Z16test_builtin_clzj
-// CIR:   %{{.+}} = cir.bit.clz(%{{.+}} : !u32i) : !s32i
-// CIR: }
+// CIR-LABEL: _Z16test_builtin_clzj
+// CIR: [[TMP:%.+]] = cir.bit.clz(%{{.+}} : !u32i) zero_poison : !u32i
+// CIR: {{%.+}} = cir.cast(integral, [[TMP]] : !u32i), !s32i
 
 int test_builtin_clzl(unsigned long x) {
   return __builtin_clzl(x);
 }
 
-// CIR: cir.func @_Z17test_builtin_clzlm
-// CIR:   %{{.+}} = cir.bit.clz(%{{.+}} : !u64i) : !s32i
-// CIR: }
+// CIR-LABEL: _Z17test_builtin_clzlm
+// CIR: [[TMP:%.+]] = cir.bit.clz(%{{.+}} : !u64i) zero_poison : !u64i
+// CIR: {{%.+}} = cir.cast(integral, [[TMP]] : !u64i), !s32i
 
 int test_builtin_clzll(unsigned long long x) {
   return __builtin_clzll(x);
 }
 
-// CIR: cir.func @_Z18test_builtin_clzlly
-// CIR:   %{{.+}} = cir.bit.clz(%{{.+}} : !u64i) : !s32i
-// CIR: }
+// CIR-LABEL: _Z18test_builtin_clzlly
+// CIR: [[TMP:%.+]] = cir.bit.clz(%{{.+}} : !u64i) zero_poison : !u64i
+// CIR: {{%.+}} = cir.cast(integral, [[TMP]] : !u64i), !s32i
 
 int test_builtin_clzg(unsigned x) {
   return __builtin_clzg(x);
 }
 
-// CIR: cir.func @_Z17test_builtin_clzgj
-// CIR:   %{{.+}} = cir.bit.clz(%{{.+}} : !u32i) : !s32i
-// CIR: }
+// CIR-LABEL: _Z17test_builtin_clzgj
+// CIR: [[TMP:%.+]] = cir.bit.clz(%{{.+}} : !u32i) zero_poison : !u32i
+// CIR: {{%.+}} = cir.cast(integral, [[TMP]] : !u32i), !s32i
 
 int test_builtin_ffs(int x) {
   return __builtin_ffs(x);
 }
 
-// CIR: cir.func @_Z16test_builtin_ffsi
-// CIR:   %{{.+}} = cir.bit.ffs(%{{.+}} : !s32i) : !s32i
-// CIR: }
+// CIR-LABEL: _Z16test_builtin_ffsi
+// CIR: [[TMP:%.+]] = cir.bit.ffs(%{{.+}} : !s32i) : !s32i
 
 int test_builtin_ffsl(long x) {
   return __builtin_ffsl(x);
 }
 
-// CIR: cir.func @_Z17test_builtin_ffsll
-// CIR:   %{{.+}} = cir.bit.ffs(%{{.+}} : !s64i) : !s32i
-// CIR: }
+// CIR-LABEL: _Z17test_builtin_ffsll
+// CIR: [[TMP:%.+]] = cir.bit.ffs(%{{.+}} : !s64i) : !s64i
+// CIR: {{%.+}} = cir.cast(integral, [[TMP]] : !s64i), !s32i
 
 int test_builtin_ffsll(long long x) {
   return __builtin_ffsll(x);
 }
 
-// CIR: cir.func @_Z18test_builtin_ffsllx
-// CIR:   %{{.+}} = cir.bit.ffs(%{{.+}} : !s64i) : !s32i
-// CIR: }
+// CIR-LABEL: _Z18test_builtin_ffsllx
+// CIR: [[TMP:%.+]] = cir.bit.ffs(%{{.+}} : !s64i) : !s64i
+// CIR: {{%.+}} = cir.cast(integral, [[TMP]] : !s64i), !s32i
 
 int test_builtin_parity(unsigned x) {
   return __builtin_parity(x);
 }
 
-// CIR: cir.func @_Z19test_builtin_parityj
-// CIR:   %{{.+}} = cir.bit.parity(%{{.+}} : !u32i) : !s32i
-// CIR: }
+// CIR-LABEL: _Z19test_builtin_parityj
+// CIR: [[TMP:%.+]] = cir.bit.parity(%{{.+}} : !u32i) : !u32i
+// CIR: {{%.+}} = cir.cast(integral, [[TMP]] : !u32i), !s32i
 
 int test_builtin_parityl(unsigned long x) {
   return __builtin_parityl(x);
 }
 
-// CIR: cir.func @_Z20test_builtin_paritylm
-// CIR:   %{{.+}} = cir.bit.parity(%{{.+}} : !u64i) : !s32i
-// CIR: }
+// CIR-LABEL: _Z20test_builtin_paritylm
+// CIR: [[TMP:%.+]] = cir.bit.parity(%{{.+}} : !u64i) : !u64i
+// CIR: {{%.+}} = cir.cast(integral, [[TMP]] : !u64i), !s32i
 
 int test_builtin_parityll(unsigned long long x) {
   return __builtin_parityll(x);
 }
 
-// CIR: cir.func @_Z21test_builtin_paritylly
-// CIR:   %{{.+}} = cir.bit.parity(%{{.+}} : !u64i) : !s32i
-// CIR: }
+// CIR-LABEL: _Z21test_builtin_paritylly
+// CIR: [[TMP:%.+]] = cir.bit.parity(%{{.+}} : !u64i) : !u64i
+// CIR: {{%.+}} = cir.cast(integral, [[TMP]] : !u64i), !s32i
 
 int test_builtin_popcount(unsigned x) {
   return __builtin_popcount(x);
 }
 
-// CIR: cir.func @_Z21test_builtin_popcountj
-// CIR:   %{{.+}} = cir.bit.popcount(%{{.+}} : !u32i) : !s32i
-// CIR: }
+// CIR-LABEL: _Z21test_builtin_popcountj
+// CIR: [[TMP:%.+]] = cir.bit.popcount(%{{.+}} : !u32i) : !u32i
+// CIR: {{%.+}} = cir.cast(integral, [[TMP]] : !u32i), !s32i
 
 int test_builtin_popcountl(unsigned long x) {
   return __builtin_popcountl(x);
 }
 
-// CIR: cir.func @_Z22test_builtin_popcountlm
-// CIR:   %{{.+}} = cir.bit.popcount(%{{.+}} : !u64i) : !s32i
-// CIR: }
+// CIR-LABEL: _Z22test_builtin_popcountlm
+// CIR: [[TMP:%.+]] = cir.bit.popcount(%{{.+}} : !u64i) : !u64i
+// CIR: {{%.+}} = cir.cast(integral, [[TMP]] : !u64i), !s32i
 
 int test_builtin_popcountll(unsigned long long x) {
   return __builtin_popcountll(x);
 }
 
-// CIR: cir.func @_Z23test_builtin_popcountlly
-// CIR:   %{{.+}} = cir.bit.popcount(%{{.+}} : !u64i) : !s32i
-// CIR: }
+// CIR-LABEL: _Z23test_builtin_popcountlly
+// CIR: [[TMP:%.+]] = cir.bit.popcount(%{{.+}} : !u64i) : !u64i
+// CIR: {{%.+}} = cir.cast(integral, [[TMP]] : !u64i), !s32i
 
 int test_builtin_popcountg(unsigned x) {
   return __builtin_popcountg(x);
 }
 
-// CIR: cir.func @_Z22test_builtin_popcountgj
-// CIR:   %{{.+}} = cir.bit.popcount(%{{.+}} : !u32i) : !s32i
-// CIR: }
+// CIR-LABEL: _Z22test_builtin_popcountgj
+// CIR: [[TMP:%.+]] = cir.bit.popcount(%{{.+}} : !u32i) : !u32i
+// CIR: {{%.+}} = cir.cast(integral, [[TMP]] : !u32i), !s32i
