6c7a1ef9d4991ec1b077cebbbb73f9075a71d8c6
Mate Marjanović <sharp.edged2@gmail.com>
2025-03-12 02:04:50 +0000

    Formatting fixes

--- clang/lib/CIR/CodeGen/CIRGenCUDARuntime.cpp
+++ clang/lib/CIR/CodeGen/CIRGenCUDARuntime.cpp
@@ -42,7 +42,8 @@ static std::unique_ptr<MangleContext> initDeviceMC(CIRGenModule &cgm) {
 
 CIRGenCUDARuntime::~CIRGenCUDARuntime() {}
 
-CIRGenCUDARuntime::CIRGenCUDARuntime(CIRGenModule &cgm) : cgm(cgm), deviceMC(initDeviceMC(cgm)) {
+CIRGenCUDARuntime::CIRGenCUDARuntime(CIRGenModule &cgm) 
+    : cgm(cgm), deviceMC(initDeviceMC(cgm)) {
   if (cgm.getLangOpts().OffloadViaLLVM)
     llvm_unreachable("NYI");
   else if (cgm.getLangOpts().HIP)
