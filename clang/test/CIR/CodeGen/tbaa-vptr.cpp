9ca8b0bc408c831f1942c2fa68bd1a9637412828
PikachuHy <pikachuhy@linux.alibaba.com>
2025-03-14 02:32:30 +0800

    [CIR][CIRGen][TBAA] Add support for vtable pointer (#1463)

--- clang/test/CIR/CodeGen/tbaa-vptr.cpp
+++ clang/test/CIR/CodeGen/tbaa-vptr.cpp
@@ -1,7 +1,15 @@
 // RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -fclangir -emit-cir %s -o %t.cir -O1
 // RUN: FileCheck --check-prefix=CIR --input-file=%t.cir %s
+// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -fclangir -emit-llvm %s -o %t.ll -O1
+// RUN: FileCheck --check-prefix=LLVM --input-file=%t.ll %s
+// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -fclangir -emit-llvm %s -o %t.ll -O1 -relaxed-aliasing
+// RUN: FileCheck --check-prefix=NO-TBAA --input-file=%t.ll %s
+// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -fclangir -emit-llvm %s -o %t.ll -O0
+// RUN: FileCheck --check-prefix=NO-TBAA --input-file=%t.ll %s
 
-// CIR-NOT: #tbaa
+// NO-TBAA-NOT: !tbaa
+
+// CIR: #tbaa[[VPTR:.*]] = #cir.tbaa_vptr<type = !cir.ptr<!cir.ptr<!cir.func<() -> !u32i>>>>
 
 struct Member {
   ~Member();
@@ -16,3 +24,11 @@ struct B : A {
   virtual ~B();
 };
 B::~B() { }
+
+// CIR-LABEL: _ZN1BD2Ev
+// CIR: cir.store %{{.*}}, %{{.*}} : !cir.ptr<!cir.ptr<!cir.func<() -> !u32i>>>, !cir.ptr<!cir.ptr<!cir.ptr<!cir.func<() -> !u32i>>>> tbaa(#tbaa[[VPTR]])
+
+// LLVM-LABEL: _ZN1BD2Ev
+// LLVM: store ptr getelementptr inbounds nuw (i8, ptr @_ZTV1B, i64 16), ptr %{{.*}}, align 8, !tbaa ![[TBAA_VPTR:.*]]
+// LLVM: ![[TBAA_VPTR]] = !{![[TBAA_VPTR_PARENT:.*]], ![[TBAA_VPTR_PARENT]], i64 0}
+// LLVM: ![[TBAA_VPTR_PARENT]] = !{!"vtable pointer", !
