28f989602e82c1c7b1278047434de4885ffb1654
Letu Ren <fantasquex@gmail.com>
2025-03-14 02:34:58 +0800

    [CIR][CIRGen][builtin][X86] handle _mm_lfence (#1474)
    I realized I committed a new file with CRLF before. Really sorry about
    that >_<
    
    Related: https://github.com/llvm/clangir/issues/1404

--- clang/test/CIR/CodeGen/X86/builtins-x86.c
+++ clang/test/CIR/CodeGen/X86/builtins-x86.c
@@ -1,8 +1,10 @@
-// Global variables of intergal types
 // RUN: %clang_cc1 -triple x86_64-unknown-linux -Wno-implicit-function-declaration -fclangir -emit-cir -o %t.cir %s
 // RUN: FileCheck --check-prefix=CIR --input-file=%t.cir %s
 // RUN: %clang_cc1 -triple x86_64-unknown-linux -Wno-implicit-function-declaration -fclangir -emit-llvm -o %t.ll %s
 // RUN: FileCheck --check-prefix=LLVM --input-file=%t.ll %s
+
+// This test mimics clang/test/CodeGen/builtins-x86.c, which eventually
+// CIR shall be able to support fully.
 
 void test_mm_clflush(const void* tmp_vCp) {
   // CIR-LABEL: test_mm_clflush
@@ -10,4 +12,12 @@ void test_mm_clflush(const void* tmp_vCp) {
   _mm_clflush(tmp_vCp);
   // CIR: {{%.*}} = cir.llvm.intrinsic "x86.sse2.clflush" {{%.*}} : (!cir.ptr<!void>) -> !void
   // LLVM: call void @llvm.x86.sse2.clflush(ptr {{%.*}})
+}
+
+void test_mm_lfence() {
+  // CIR-LABEL: test_mm_lfence
+  // LLVM-LABEL: test_mm_lfence
+  _mm_lfence();
+  // CIR: {{%.*}} = cir.llvm.intrinsic "x86.sse2.lfence" : () -> !void
+  // LLVM: call void @llvm.x86.sse2.lfence()
 }
