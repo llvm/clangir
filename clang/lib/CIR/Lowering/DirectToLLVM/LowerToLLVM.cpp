llvm/llvm-project

[CIR][LLVMLowering] Add LLVM lowering for __bf16 (#717)
This PR adds LLVM lowering support for the `__bf16` type. To support its
LLVM lowering, we just need to add a new type conversion rule to the
LLVM lowering pass. The majority of this PR are the new LLVM IR checks
in the tests.

clang/lib/CIR/Lowering/DirectToLLVM/LowerToLLVM.cpp
@@ -3421,6 +3421,9 @@ void prepareTypeConverter(mlir::LLVMTypeConverter &converter,
   converter.addConversion([&](mlir::cir::FP16Type type) -> mlir::Type {
     return mlir::FloatType::getF16(type.getContext());
   });
+  converter.addConversion([&](mlir::cir::BF16Type type) -> mlir::Type {
+    return mlir::FloatType::getBF16(type.getContext());
+  });
   converter.addConversion([&](mlir::cir::FuncType type) -> mlir::Type {
     auto result = converter.convertType(type.getReturnType());
     llvm::SmallVector<mlir::Type> arguments;

llvm/clangir

clang/lib/CIR/Lowering/DirectToLLVM/LowerToLLVM.cpp
@@ -3422,6 +3422,9 @@ void prepareTypeConverter(mlir::LLVMTypeConverter &converter,
   converter.addConversion([&](mlir::cir::FP16Type type) -> mlir::Type {
     return mlir::Float16Type::get(type.getContext());
   });
+  converter.addConversion([&](mlir::cir::BF16Type type) -> mlir::Type {
+    return mlir::Float16Type::get(type.getContext());
+  });
   converter.addConversion([&](mlir::cir::FuncType type) -> mlir::Type {
     auto result = converter.convertType(type.getReturnType());
     llvm::SmallVector<mlir::Type> arguments;