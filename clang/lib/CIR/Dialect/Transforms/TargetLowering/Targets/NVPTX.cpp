b3d05f8cf79260680416a368935bc70bc9d1a871
Yue Huang <30948580+AdUhTkJm@users.noreply.github.com>
2025-03-17 17:36:10 +0000

    [CIR][CUDA] CallConvLowering for basic types in NVPTX (#1468)
    There are some subtleties here.
    
    This is the code in OG:
    ```cpp
    // note: this is different from default ABI
    if (!RetTy->isScalarType())
      return ABIArgInfo::getDirect();
    ```
    which says we should return structs directly. It's correct, has have the
    same behaviour as `nvcc`, and it obeys the PTX ABI as well.
    The comment dates back to 2013 (see [this
    commit](https://github.com/llvm/llvm-project/commit/f9329ff650fa252606c65977405189dc46e5d66f)
    -- it didn't provide any explanation either), so I believe it's
    outdated. I didn't include this comment in the PR.

--- clang/lib/CIR/Dialect/Transforms/TargetLowering/Targets/NVPTX.cpp
+++ clang/lib/CIR/Dialect/Transforms/TargetLowering/Targets/NVPTX.cpp
@@ -12,7 +12,9 @@
 #include "TargetInfo.h"
 #include "TargetLoweringInfo.h"
 #include "clang/CIR/ABIArgInfo.h"
+#include "clang/CIR/Dialect/IR/CIRTypes.h"
 #include "clang/CIR/MissingFeatures.h"
+#include "llvm/Support/Casting.h"
 #include "llvm/Support/ErrorHandling.h"
 
 using ABIArgInfo = cir::ABIArgInfo;
@@ -31,9 +33,10 @@ public:
   NVPTXABIInfo(LowerTypes &lt) : ABIInfo(lt) {}
 
 private:
-  void computeInfo(LowerFunctionInfo &fi) const override {
-    llvm_unreachable("NYI");
-  }
+  ABIArgInfo classifyReturnType(mlir::Type ty) const;
+  ABIArgInfo classifyArgumentType(mlir::Type ty) const;
+
+  void computeInfo(LowerFunctionInfo &fi) const override;
 };
 
 class NVPTXTargetLoweringInfo : public TargetLoweringInfo {
@@ -62,6 +65,48 @@ public:
 };
 
 } // namespace
+
+ABIArgInfo NVPTXABIInfo::classifyReturnType(mlir::Type ty) const {
+  if (llvm::isa<VoidType>(ty))
+    return ABIArgInfo::getIgnore();
+
+  if (getContext().getLangOpts().OpenMP)
+    llvm_unreachable("NYI");
+
+  if (!isScalarType(ty))
+    return ABIArgInfo::getDirect();
+
+  // OG treats enums as their underlying type.
+  // This has already been done for CIR.
+
+  // Integers with size < 32 must be extended to 32 bits.
+  // (See Section 3.3 of PTX ABI.)
+  return (isPromotableIntegerTypeForABI(ty) ? ABIArgInfo::getExtend(ty)
+                                            : ABIArgInfo::getDirect());
+}
+
+ABIArgInfo NVPTXABIInfo::classifyArgumentType(mlir::Type ty) const {
+  if (isAggregateTypeForABI(ty))
+    llvm_unreachable("NYI");
+
+  if (auto intType = llvm::dyn_cast<IntType>(ty)) {
+    if (intType.getWidth() > 128)
+      llvm_unreachable("NYI");
+  }
+
+  return (isPromotableIntegerTypeForABI(ty) ? ABIArgInfo::getExtend(ty)
+                                            : ABIArgInfo::getDirect());
+}
+
+void NVPTXABIInfo::computeInfo(LowerFunctionInfo &fi) const {
+  if (!getCXXABI().classifyReturnType(fi))
+    fi.getReturnInfo() = classifyReturnType(fi.getReturnType());
+
+  for (auto &&[count, argument] : llvm::enumerate(fi.arguments()))
+    argument.info = count < fi.getNumRequiredArgs()
+                        ? classifyArgumentType(argument.type)
+                        : ABIArgInfo::getDirect();
+}
 
 std::unique_ptr<TargetLoweringInfo>
 createNVPTXTargetLoweringInfo(LowerModule &lowerModule) {
