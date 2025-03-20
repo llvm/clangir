29582da772ecb260ad1931545fe367dc21dad790
Letu Ren <fantasquex@gmail.com>
2025-03-21 05:53:49 +0800

    [CIR] Backport clang commit to unxfail some builtin call (#1501)
    Ref:
    https://github.com/llvm/clangir/commit/cd269fee05a0f78fb53b65f701b4e06e9ddab424
    
    Related: https://github.com/llvm/clangir/issues/1497

--- clang/test/CIR/CodeGen/libcall.cpp
+++ clang/test/CIR/CodeGen/libcall.cpp
@@ -1,6 +1,5 @@
 // RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -std=c++20 -fclangir -mconstructor-aliases -emit-cir %s -o %t.cir
 // RUN: FileCheck --input-file=%t.cir %s
-// XFAIL: *
 
 typedef __builtin_va_list va_list;
 
