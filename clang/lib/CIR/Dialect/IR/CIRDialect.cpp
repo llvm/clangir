05a42917d25b766f3b6c3d178a52c7237d1ed6b3
Yue Huang <30948580+AdUhTkJm@users.noreply.github.com>
2025-03-24 18:12:34 +0000

    [CIR] Un-xfail variadic calls (#1517)
    This un-xfails the 6 files in #1497 related to variadic calls.

--- clang/lib/CIR/Dialect/IR/CIRDialect.cpp
+++ clang/lib/CIR/Dialect/IR/CIRDialect.cpp
@@ -2376,7 +2376,7 @@ ParseResult cir::FuncOp::parse(OpAsmParser &parser, OperationState &state) {
   // Parse the function signature.
   bool isVariadic = false;
   if (function_interface_impl::parseFunctionSignatureWithArguments(
-          parser, /*allowVariadic=*/false, arguments, isVariadic, resultTypes,
+          parser, /*allowVariadic=*/true, arguments, isVariadic, resultTypes,
           resultAttrs))
     return failure();
 
