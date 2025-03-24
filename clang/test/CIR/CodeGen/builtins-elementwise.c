6d1a5745c2f6eefaa330c1f69ef8907480c20961
Ayokunle Amodu <ayokunle321@gmail.com>
2025-03-24 13:59:01 -0600

    Update builtins-elementwise.c

--- clang/test/CIR/CodeGen/builtins-elementwise.c
+++ clang/test/CIR/CodeGen/builtins-elementwise.c
@@ -10,31 +10,31 @@ typedef double vdouble4 __attribute__((ext_vector_type(4)));
 
 void test_builtin_elementwise_abs(vint4 vi4, int i, float f, double d, 
                                   vfloat4 vf4, vdouble4  vd4) {
-// CIR-LABEL: test_builtin_elementwise_abs
-// LLVM-LABEL: test_builtin_elementwise_abs
-// CIR: {{%.*}} = cir.fabs {{%.*}} : !cir.float
-// LLVM: {{%.*}} = call float @llvm.fabs.f32(float {{%.*}})
-f = __builtin_elementwise_abs(f);
-
-// CIR: {{%.*}} = cir.fabs {{%.*}} : !cir.double
-// LLVM: {{%.*}} = call double @llvm.fabs.f64(double {{%.*}})
-d = __builtin_elementwise_abs(d);
-
-// CIR: {{%.*}} = cir.abs {{%.*}} : !cir.vector<!s32i x 4>
-// LLVM: {{%.*}} = call <4 x i32> @llvm.abs.v4i32(<4 x i32> {{%.*}}, i1 false)
-vi4 = __builtin_elementwise_abs(vi4);
-
-// CIR: {{%.*}} = cir.abs {{%.*}} : !s32
-// LLVM: {{%.*}} = call i32 @llvm.abs.i32(i32 {{%.*}}, i1 false)
-i = __builtin_elementwise_abs(i);
-
-// CIR: {{%.*}} = cir.fabs {{%.*}} : !cir.vector<!cir.float x 4>
-// LLVM: {{%.*}} = call <4 x float> @llvm.fabs.v4f32(<4 x float> {{%.*}})
-vf4 = __builtin_elementwise_abs(vf4);
-
-// CIR: {{%.*}} = cir.fabs {{%.*}} : !cir.vector<!cir.double x 4>
-// LLVM: {{%.*}} = call <4 x double> @llvm.fabs.v4f64(<4 x double> {{%.*}})
-vd4 = __builtin_elementwise_abs(vd4);
+    // CIR-LABEL: test_builtin_elementwise_abs
+    // LLVM-LABEL: test_builtin_elementwise_abs
+    // CIR: {{%.*}} = cir.fabs {{%.*}} : !cir.float
+    // LLVM: {{%.*}} = call float @llvm.fabs.f32(float {{%.*}})
+    f = __builtin_elementwise_abs(f);
+    
+    // CIR: {{%.*}} = cir.fabs {{%.*}} : !cir.double
+    // LLVM: {{%.*}} = call double @llvm.fabs.f64(double {{%.*}})
+    d = __builtin_elementwise_abs(d);
+    
+    // CIR: {{%.*}} = cir.abs {{%.*}} : !cir.vector<!s32i x 4>
+    // LLVM: {{%.*}} = call <4 x i32> @llvm.abs.v4i32(<4 x i32> {{%.*}}, i1 false)
+    vi4 = __builtin_elementwise_abs(vi4);
+    
+    // CIR: {{%.*}} = cir.abs {{%.*}} : !s32
+    // LLVM: {{%.*}} = call i32 @llvm.abs.i32(i32 {{%.*}}, i1 false)
+    i = __builtin_elementwise_abs(i);
+    
+    // CIR: {{%.*}} = cir.fabs {{%.*}} : !cir.vector<!cir.float x 4>
+    // LLVM: {{%.*}} = call <4 x float> @llvm.fabs.v4f32(<4 x float> {{%.*}})
+    vf4 = __builtin_elementwise_abs(vf4);
+    
+    // CIR: {{%.*}} = cir.fabs {{%.*}} : !cir.vector<!cir.double x 4>
+    // LLVM: {{%.*}} = call <4 x double> @llvm.fabs.v4f64(<4 x double> {{%.*}})
+    vd4 = __builtin_elementwise_abs(vd4);
 }
 
 void test_builtin_elementwise_acos(float f, double d, vfloat4 vf4,
