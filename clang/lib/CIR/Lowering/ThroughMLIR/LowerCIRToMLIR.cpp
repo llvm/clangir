a14c5c941169918f41598aedb365d81dedb33fa3
Ayokunle Amodu <121697771+ayokunle321@users.noreply.github.com>
2025-03-31 09:02:19 -0600

    fix conflict

--- clang/lib/CIR/Lowering/ThroughMLIR/LowerCIRToMLIR.cpp
+++ clang/lib/CIR/Lowering/ThroughMLIR/LowerCIRToMLIR.cpp
@@ -491,6 +491,18 @@ public:
   }
 };
 
+class CIRASinOpLowering : public mlir::OpConversionPattern<cir::ASinOp> {
+public:
+  using mlir::OpConversionPattern<cir::ASinOp>::OpConversionPattern;
+
+  mlir::LogicalResult
+  matchAndRewrite(cir::ASinOp op, OpAdaptor adaptor,
+                  mlir::ConversionPatternRewriter &rewriter) const override {
+    rewriter.replaceOpWithNewOp<mlir::math::AsinOp>(op, adaptor.getSrc());
+    return mlir::LogicalResult::success();
+  }
+};
+
 template <typename CIROp, typename MLIROp>
 class CIRCountZerosBitOpLowering : public mlir::OpConversionPattern<CIROp> {
 public:
@@ -1395,8 +1407,8 @@ void populateCIRToMLIRConversionPatterns(mlir::RewritePatternSet &patterns,
            CIRBitClzOpLowering, CIRBitCtzOpLowering, CIRBitPopcountOpLowering,
            CIRBitClrsbOpLowering, CIRBitFfsOpLowering, CIRBitParityOpLowering,
            CIRIfOpLowering, CIRVectorCreateLowering, CIRVectorInsertLowering,
-           CIRVectorExtractLowering, CIRVectorCmpOpLowering, CIRACosOpLowering>(
-          converter, patterns.getContext());
+           CIRVectorExtractLowering, CIRVectorCmpOpLowering, CIRACosOpLowering,
+           CIRASinOpLowering>(converter, patterns.getContext());
 }
 
 static mlir::TypeConverter prepareTypeConverter() {
