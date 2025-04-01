7c5b0bc933cef894b07790d935241b5c6353bb10
Iris <0.0@owo.li>
2025-04-01 10:25:42 +0800

    [CIR][LowerToLLVM] Support for LinkerOptions lowering (#1532)
    - Part of #70.

--- clang/lib/CIR/Lowering/DirectToLLVM/LowerToLLVM.cpp
+++ clang/lib/CIR/Lowering/DirectToLLVM/LowerToLLVM.cpp
@@ -4330,6 +4330,15 @@ mlir::LogicalResult CIRToLLVMSignBitOpLowering::matchAndRewrite(
   return mlir::success();
 }
 
+mlir::LogicalResult CIRToLLVMLinkerOptionsOpLowering::matchAndRewrite(
+    cir::LinkerOptionsOp op, OpAdaptor adaptor,
+    mlir::ConversionPatternRewriter &rewriter) const {
+  auto newOp = rewriter.create<mlir::LLVM::LinkerOptionsOp>(
+      op.getLoc(), op.getOptionsAttr());
+  rewriter.replaceOp(op, newOp);
+  return mlir::success();
+}
+
 void populateCIRToLLVMConversionPatterns(
     mlir::RewritePatternSet &patterns, mlir::TypeConverter &converter,
     mlir::DataLayout &dataLayout, cir::LowerModule *lowerModule,
@@ -4414,6 +4423,7 @@ void populateCIRToLLVMConversionPatterns(
       CIRToLLVMInsertMemberOpLowering,
       CIRToLLVMIsConstantOpLowering,
       CIRToLLVMIsFPClassOpLowering,
+      CIRToLLVMLinkerOptionsOpLowering,
       CIRToLLVMLLVMIntrinsicCallOpLowering,
       CIRToLLVMMemChrOpLowering,
       CIRToLLVMMemCpyInlineOpLowering,
