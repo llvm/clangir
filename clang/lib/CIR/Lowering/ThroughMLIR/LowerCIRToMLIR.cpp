64f01d1eaf2a8648af872cecb162affef949aaef
Gokul <gokulnath136@gmail.com>
2025-04-05 02:18:50 +0530

    [CIR][ThroughMLIR] Add TanOp Lowering (#1540)
    Adds implementation for TanOp's lowering via ThroughMLIR.

--- clang/lib/CIR/Lowering/ThroughMLIR/LowerCIRToMLIR.cpp
+++ clang/lib/CIR/Lowering/ThroughMLIR/LowerCIRToMLIR.cpp
@@ -311,6 +311,18 @@ public:
   }
 };
 
+class CIRTanOpLowering : public mlir::OpConversionPattern<cir::TanOp> {
+public:
+  using OpConversionPattern<cir::TanOp>::OpConversionPattern;
+
+  mlir::LogicalResult
+  matchAndRewrite(cir::TanOp op, OpAdaptor adaptor,
+                  mlir::ConversionPatternRewriter &rewriter) const override {
+    rewriter.replaceOpWithNewOp<mlir::math::TanOp>(op, adaptor.getSrc());
+    return mlir::LogicalResult::success();
+  }
+};
+
 class CIRSqrtOpLowering : public mlir::OpConversionPattern<cir::SqrtOp> {
 public:
   using mlir::OpConversionPattern<cir::SqrtOp>::OpConversionPattern;
@@ -1418,8 +1430,8 @@ void populateCIRToMLIRConversionPatterns(mlir::RewritePatternSet &patterns,
            CIRBitClrsbOpLowering, CIRBitFfsOpLowering, CIRBitParityOpLowering,
            CIRIfOpLowering, CIRVectorCreateLowering, CIRVectorInsertLowering,
            CIRVectorExtractLowering, CIRVectorCmpOpLowering, CIRACosOpLowering,
-           CIRASinOpLowering, CIRUnreachableOpLowering>(converter,
-                                                        patterns.getContext());
+           CIRASinOpLowering, CIRUnreachableOpLowering, CIRTanOpLowering>(
+          converter, patterns.getContext());
 }
 
 static mlir::TypeConverter prepareTypeConverter() {
