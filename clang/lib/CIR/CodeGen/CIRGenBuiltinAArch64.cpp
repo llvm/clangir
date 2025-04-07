22e96972d334dcd31c1b12675f3decb4dd25bea0
Amr Hesham <amr96@programmer.net>
2025-04-08 00:08:52 +0200

    [CIR][CIRGen][Builtin][Neon] Lower vminv_u32 (#1551)
    Lower vminv_u32

--- clang/lib/CIR/CodeGen/CIRGenBuiltinAArch64.cpp
+++ clang/lib/CIR/CodeGen/CIRGenBuiltinAArch64.cpp
@@ -2810,7 +2810,8 @@ static mlir::Value emitCommonNeonSISDBuiltinExpr(
     return emitNeonCall(builder, {argTy}, ops, "aarch64.neon.sminv", resultTy,
                         loc);
   case NEON::BI__builtin_neon_vminv_u32:
-    llvm_unreachable(" neon_vminv_u32 NYI ");
+    return emitNeonCall(builder, {argTy}, ops, "aarch64.neon.uminv", resultTy,
+                        loc);
   case NEON::BI__builtin_neon_vminvq_f32:
     llvm_unreachable(" neon_vminvq_f32 NYI ");
   case NEON::BI__builtin_neon_vminvq_f64:
