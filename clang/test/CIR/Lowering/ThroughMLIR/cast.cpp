7275515ff7867776f8ee2031e5d296e3f5d278a8
Ronan Keryell <rkeryell@nvidia.com>
2025-04-17 17:33:20 -0700

    [CIR][ThroughMLIR][NFC] Fix test syntax
    The -emit-mlir syntax has changed and now requires the kind of MLIR to be
    specified.

--- clang/test/CIR/Lowering/ThroughMLIR/cast.cpp
+++ clang/test/CIR/Lowering/ThroughMLIR/cast.cpp
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -fno-clangir-direct-lowering -emit-mlir %s -o %t.mlir
+// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -fno-clangir-direct-lowering -emit-mlir=core %s -o %t.mlir
 // RUN: FileCheck --input-file=%t.mlir %s
 
 
