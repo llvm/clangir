88d79317028d3af3fa8491304b791b3f602aa928
Fangyi Zhou <me@fangyi.io>
2025-05-20 17:31:06 +0100

    [CIR] Un-xfail some tests affected by GEP changes (#1621)
    This commit un-xfails some tests that were affected by upstream GEP
    changes. #1497
    
    The changes are likely introduced by inference of `inbounds` and `nuw`
    flags
    https://github.com/llvm/llvm-project/commit/10f315dc9c96ec2413881ab55a285e35d80def88.
    It seems to allow LLVM to enable some constant foldings, whose effects
    include re-calculating the address using i8 and a single offset. (This
    change seems to arise from
    https://discourse.llvm.org/t/rfc-replacing-getelementptr-with-ptradd/68699/39)

--- clang/test/CIR/CodeGen/vtable-emission.cpp
+++ clang/test/CIR/CodeGen/vtable-emission.cpp
@@ -3,7 +3,6 @@
 // RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -fclangir  -emit-llvm -o - %s \
 // RUN: | opt -S -passes=instcombine,mem2reg,simplifycfg -o %t.ll
 // RUN: FileCheck --check-prefix=LLVM --input-file=%t.ll %s
-// XFAIL: *
 
 struct S {
   virtual void key();
@@ -20,11 +19,12 @@ void S::key() {}
 // LLVM: @_ZTV1S = global { [4 x ptr] } { [4 x ptr]
 // LLVM-SAME: [ptr null, ptr @_ZTI1S, ptr @_ZN1S3keyEv, ptr @_ZN1S6nonKeyEv] }, align 8
 
+// Note: GEP emitted by cir might not be the same as LLVM, due to constant folding.
 // CHECK: cir.global external @sobj = #cir.const_record
 // CHECK-SAME: <{#cir.global_view<@_ZTV1S, [0 : i32, 2 : i32]> :
 // CHECK-SAME: !cir.ptr<!rec_anon_struct1>}> : !rec_anon_struct2 {alignment = 8 : i64}
-// LLVM: @sobj = global { ptr } { ptr getelementptr inbounds
-// LLVM-SAME: ({ [4 x ptr] }, ptr @_ZTV1S, i32 0, i32 0, i32 2) }, align 8
+// LLVM: @sobj = global { ptr } { ptr getelementptr inbounds nuw
+// LLVM-SAME: (i8, ptr @_ZTV1S, i64 16) }, align 8
 
 // The reference from the vtable should result in nonKey being emitted.
 // CHECK: cir.func linkonce_odr @_ZN1S6nonKeyEv({{.*}} {
