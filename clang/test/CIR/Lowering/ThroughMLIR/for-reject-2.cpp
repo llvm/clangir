310cd50780c62ab05238d27bd7ef13112d9fe9fb
terapines open source contributor 2 <osc-2@terapines.com>
2025-05-28 00:42:37 +0100

    [CIR][ThroughMLIR] Lower uncanonicalized fors to whiles (#1644)
    The transformation functions are all named `transferToXXXOp`. Are those
    typos?
    
    Co-authored-by: Yue Huang <yue.huang@terapines.com>

--- clang/test/CIR/Lowering/ThroughMLIR/for-reject-2.cpp
+++ clang/test/CIR/Lowering/ThroughMLIR/for-reject-2.cpp
@@ -1,4 +1,5 @@
 // RUN: not %clang_cc1 -triple x86_64-unknown-linux-gnu -fclangir -fno-clangir-direct-lowering -emit-mlir=core %s -o - 2>&1 | FileCheck %s
+// XFAIL: *
 
 void reject() {
   for (int i = 0; i < 100; i++, i++);
