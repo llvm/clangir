d994965574b78120f7096c34c7a2797deea5225f
Andres Salamanca <andrealebarbaritos@gmail.com>
2025-06-08 13:13:22 -0500

    Add triple

--- clang/test/CIR/CodeGen/dumb-record.cpp
+++ clang/test/CIR/CodeGen/dumb-record.cpp
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1  -fclangir -emit-cir -fdump-record-layouts %s -o - | FileCheck %s
+// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -fclangir -emit-cir -fdump-record-layouts %s -o - | FileCheck %s
 
 struct SimpleStruct {
   int a;
