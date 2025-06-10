114e98e12f14582b5e40b474beabe64d565b8d35
Andy Kaylor <akaylor@nvidia.com>
2025-06-10 11:11:38 -0700

    [CIR] Avoid unnecessary type cache clearing with Enum type completion (#1673)
    When we process a completed Enum type, we were checking to see if the
    completed type was in the type cache and clearing the cache if the
    completed and converted underlying type for the enum doesn't pass an
    `isInteger(32)` check. Unfortunately, this checks to see if the type is
    the MLIR builtin 32-bit integer type, whereas it will always be a CIR
    integer type, so the check always fails.
    
    I don't believe there can ever be a case where the forward declared type
    for the enum doesn't match the completed type, so we should never need
    to clear the cache. This change replaces the previous check with an
    assert that compares the actual completed type to the cached type.

--- clang/lib/CIR/CodeGen/CIRGenTypes.cpp
+++ clang/lib/CIR/CodeGen/CIRGenTypes.cpp
@@ -822,6 +822,22 @@ void CIRGenTypes::UpdateCompletedType(const TagDecl *TD) {
       if (!convertType(ED->getIntegerType()).isInteger(32))
         TypeCache.clear();
     }
+    // Only flush the cache if we've actually already converted this type.
+    if (TypeCache.count(astContext.getCanonicalTagType(ED).getTypePtr())) {
+      // Okay, we formed some types based on this.  We speculated that the enum
+      // would be lowered to i32, so we only need to flush the cache if this
+      // didn't happen.
+      if (!convertType(ED->getIntegerType()).isInteger(32))
+        TypeCache.clear();
+    }
+    // Classic codegen clears the type cache if it contains an entry for this
+    // enum type that doesn't use i32 as the underlying type, but I can't find
+    // a test case that meets that condition. C++ doesn't allow forward
+    // declaration of enums, and C doesn't allow an incomplete forward
+    // declaration with a non-default type.
+    assert(
+        !TypeCache.count(astContext.getCanonicalTagType(ED).getTypePtr()) ||
+        (convertType(ED->getIntegerType()) == TypeCache[astContext.getCanonicalTagType(ED).getTypePtr()]));
     // If necessary, provide the full definition of a type only used with a
     // declaration so far.
     assert(!cir::MissingFeatures::generateDebugInfo());
