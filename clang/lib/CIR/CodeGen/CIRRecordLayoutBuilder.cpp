b79b622a68fa4fd8c1211a7e94177827fcb7731e
Andres Salamanca <andrealebarbaritos@gmail.com>
2025-06-17 17:30:27 -0500

    Fine-grained bitfield access NYI

--- clang/lib/CIR/CodeGen/CIRRecordLayoutBuilder.cpp
+++ clang/lib/CIR/CodeGen/CIRRecordLayoutBuilder.cpp
@@ -651,8 +651,7 @@ CIRRecordLowering::accumulateBitFields(RecordDecl::field_iterator Field,
           } else if (cirGenTypes.getModule()
                          .getCodeGenOpts()
                          .FineGrainedBitfieldAccesses) {
-            // Fine-grained access, so no merging of spans.
-            InstallBest = true;
+            llvm_unreachable("NYI");
           } else {
             // Otherwise, we're not installing. Update the bit size
             // of the current span to go all the way to LimitOffset, which is
