7fbe68c9db8a5064ac9ba965adc9f161375abe34
Henrich Lauko <xlauko@mail.muni.cz>
2025-07-02 08:53:52 +0200

    [CIR] Clean up IntAttr (#1725)
    - Add common CIR_ prefix
    - Simplify printing/parsing
    - Make it use IntTypeInterface

--- clang/include/clang/CIR/Dialect/IR/CIRAttrs.h
+++ clang/include/clang/CIR/Dialect/IR/CIRAttrs.h
@@ -24,6 +24,7 @@
 #include "clang/CIR/Dialect/IR/CIROpsEnums.h"
 
 #include "clang/CIR/Interfaces/ASTAttrInterfaces.h"
+#include "clang/CIR/Interfaces/CIRTypeInterfaces.h"
 
 //===----------------------------------------------------------------------===//
 // CIR Dialect Attrs
