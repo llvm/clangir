daac47d299514f2c47a3eb4b120737f90bceb169
Tommy McMichen <tommymcmichen@fb.com>
2025-07-18 15:27:32 -0700

    [CIR][NFC] Update vtable test

--- clang/test/CIR/CodeGen/vtable-available-externally.cpp
+++ clang/test/CIR/CodeGen/vtable-available-externally.cpp
@@ -1,10 +1,12 @@
-// RUN: %clang_cc1 %s -I%S -triple x86_64-unknown-linux-gnu -std=c++98 -O0 -disable-llvm-passes -emit-cir -o %t
+// RUN: %clang_cc1 %s -I%S -I%S/../Inputs -triple x86_64-unknown-linux-gnu -std=c++98 -O0 -disable-llvm-passes -emit-cir -o %t
 // RUN: FileCheck -allow-deprecated-dag-overlap --check-prefix=CHECK %s < %t
-// RUN: %clang_cc1 %s -I%S -triple x86_64-unknown-linux-gnu -std=c++98 -O2 -disable-llvm-passes -emit-cir -o %t.opt
+// RUN: %clang_cc1 %s -I%S -I%S/../Inputs -triple x86_64-unknown-linux-gnu -std=c++98 -O2 -disable-llvm-passes -emit-cir -o %t.opt
 // RUN: FileCheck -allow-deprecated-dag-overlap --check-prefix=CHECK-FORCE-EMIT %s < %t.opt
 
+#include "typeinfo"
+
 // CHECK: cir.global{{.*}} external @_ZTV1A
-// CHECK-FORCE-EMIT: cir.global{{.*}} available_externally @_ZTV1A
+// CHECK-FORCE-EMIT-DAG: cir.global{{.*}} available_externally @_ZTV1A
 struct A {
   A();
   virtual void f();
@@ -16,9 +18,6 @@ A::A() { }
 void f(A* a) {
   a->f();
 };
-
-// CHECK-LABEL: cir.func{{.*}} @_Z1gv
-// CHECK:         cir.call @_Z1fP1A
 void g() {
   A a;
   f(&a);
