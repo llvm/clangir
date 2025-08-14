57dfa7c3e86ba0b8bf4ef230b235f4e782f2069c
AmrDeveloper <amr96@programmer.net>
2025-08-14 20:30:04 +0200

    [CIR] Backport Comma Operator for ComplexType

--- clang/lib/CIR/CodeGen/CIRGenExprComplex.cpp
+++ clang/lib/CIR/CodeGen/CIRGenExprComplex.cpp
@@ -317,7 +317,8 @@ public:
     return emitLoadOfLValue(LV, E->getExprLoc());
   };
   mlir::Value VisitBinComma(const BinaryOperator *E) {
-    llvm_unreachable("NYI");
+    CGF.emitIgnoredExpr(E->getLHS());
+    return Visit(E->getRHS());
   }
 
   mlir::Value
