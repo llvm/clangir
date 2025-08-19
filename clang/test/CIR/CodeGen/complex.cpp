818a12b8b432f4c14183873d52ab2fc48d16e4c5
AmrDeveloper <amr96@programmer.net>
2025-08-19 11:00:41 +0200

    [CIR] Implement support for __imag__ operator on complex rvalue

--- clang/test/CIR/CodeGen/complex.cpp
+++ clang/test/CIR/CodeGen/complex.cpp
@@ -85,3 +85,27 @@ int _Complex complex_real_operator_on_rvalue() {
 // LLVM: store { i32, i32 } zeroinitializer, ptr %[[RET_ADDR]], align 4
 // LLVM: %[[TMP_RET:.*]] = load { i32, i32 }, ptr %[[RET_ADDR]], align 4
 // LLVM: ret { i32, i32 } %[[TMP_RET]]
+
+int _Complex complex_imag_operator_on_rvalue() {
+  int imag = __imag__ complex_imag_operator_on_rvalue();
+  return {};
+}
+
+// CIR: %[[RET_ADDR:.*]] = cir.alloca !cir.complex<!s32i>, !cir.ptr<!cir.complex<!s32i>>, ["__retval"]
+// CIR: %[[IMAG_ADDR:.*]] = cir.alloca !s32i, !cir.ptr<!s32i>, ["imag", init]
+// CIR: %[[CALL:.*]] = cir.call @_Z31complex_imag_operator_on_rvaluev() : () -> !cir.complex<!s32i>
+// CIR: %[[IMAG:.*]] = cir.complex.imag %[[CALL]] : !cir.complex<!s32i> -> !s32i
+// CIR: cir.store{{.*}} %[[IMAG]], %[[IMAG_ADDR]] : !s32i, !cir.ptr<!s32i>
+// CIR: %[[RET_COMPLEX:.*]] = cir.const #cir.complex<#cir.int<0> : !s32i, #cir.int<0> : !s32i> : !cir.complex<!s32i>
+// CIR: cir.store{{.*}} %[[RET_COMPLEX]], %[[RET_ADDR]] : !cir.complex<!s32i>, !cir.ptr<!cir.complex<!s32i>>
+// CIR: %[[TMP_RET:.*]] = cir.load %[[RET_ADDR]] : !cir.ptr<!cir.complex<!s32i>>, !cir.complex<!s32i>
+// CIR: cir.return %[[TMP_RET]] : !cir.complex<!s32i>
+
+// LLVM: %[[RET_ADDR:.*]] = alloca { i32, i32 }, i64 1, align 4
+// LLVM: %[[IMAG_ADDR:.*]] = alloca i32, i64 1, align 4
+// LLVM: %[[CALL:.*]] = call { i32, i32 } @_Z31complex_imag_operator_on_rvaluev()
+// LLVM: %[[IMAG:.*]] = extractvalue { i32, i32 } %[[CALL]], 1
+// LLVM: store i32 %[[IMAG]], ptr %[[IMAG_ADDR]], align 4
+// LLVM: store { i32, i32 } zeroinitializer, ptr %[[RET_ADDR]], align 4
+// LLVM: %[[TMP_RET:.*]] = load { i32, i32 }, ptr %[[RET_ADDR]], align 4
+// LLVM: ret { i32, i32 } %[[TMP_RET]]
