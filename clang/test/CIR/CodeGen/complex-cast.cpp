24a790364ec84d324acc6751f1361059b617e27a
AmrDeveloper <amr96@programmer.net>
2025-08-19 11:58:10 +0200

    [CIR] Implement UserDefinedConversion for ComplexType

--- clang/test/CIR/CodeGen/complex-cast.cpp
+++ clang/test/CIR/CodeGen/complex-cast.cpp
@@ -19,3 +19,27 @@ void complex_lvalue_bitcast() {
 
 // LLVM: %[[A_ADDR:.*]] = alloca %struct.CX, i64 1, align 8
 // LLVM: store { double, double } zeroinitializer, ptr %[[A_ADDR]], align 8
+
+void complex_user_defined_cast() {
+  struct Point {
+    int x;
+    int y;
+    operator int _Complex() const { return {x, y}; }
+  };
+
+  Point p{1, 2};
+  int _Complex c = p;
+}
+
+// CIR-AFTER: %[[P_ADDR:.*]] = cir.alloca !rec_Point, !cir.ptr<!rec_Point>, ["p"]
+// CIR-AFTER: %[[C_ADDR:.*]] = cir.alloca !cir.complex<!s32i>, !cir.ptr<!cir.complex<!s32i>>, ["c", init]
+// CIR-AFTER: %[[P_CONST:.*]] = cir.const #cir.const_record<{#cir.int<1> : !s32i, #cir.int<2> : !s32i}> : !rec_Point
+// CIR-AFTER: cir.store{{.*}} %[[P_CONST]], %[[P_ADDR]] : !rec_Point, !cir.ptr<!rec_Point>
+// CIR-AFTER: %[[POINT_TO_COMPLEX:.*]] = cir.call @_ZZ25complex_user_defined_castvENK5PointcvCiEv(%[[P_ADDR]]) : (!cir.ptr<!rec_Point>) -> !cir.complex<!s32i>
+// CIR-AFTER: cir.store{{.*}} %[[POINT_TO_COMPLEX]], %[[C_ADDR]] : !cir.complex<!s32i>, !cir.ptr<!cir.complex<!s32i>>
+
+// LLVM: %[[P_ADDR:.*]] = alloca %struct.Point, i64 1, align 4
+// LLVM: %[[C_ADDR:.*]] = alloca { i32, i32 }, i64 1, align 4
+// LLVM: store %struct.Point { i32 1, i32 2 }, ptr %[[P_ADDR]], align 4
+// LLVM: %[[POINT_TO_COMPLEX:.*]] = call { i32, i32 } @_ZZ25complex_user_defined_castvENK5PointcvCiEv(ptr %[[P_ADDR]])
+// LLVM: store { i32, i32 } %[[POINT_TO_COMPLEX]], ptr %[[C_ADDR]], align 4
