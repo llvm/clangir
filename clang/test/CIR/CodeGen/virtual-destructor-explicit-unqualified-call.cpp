45b5109adf5594e91ede30daa69960d4d5833e3b
ahmedshakill <shakil.000024@gmail.com>
2025-08-26 03:12:33 +0600

    make test more readable and consistent

--- clang/test/CIR/CodeGen/virtual-destructor-explicit-unqualified-call.cpp
+++ clang/test/CIR/CodeGen/virtual-destructor-explicit-unqualified-call.cpp
@@ -2,26 +2,22 @@
 // RUN: FileCheck --check-prefix=CIR --input-file=%t.cir %s
 
 
-// CIR-LABEL:   cir.func dso_local @_ZN1A1BES_(
-// CIR-SAME:      %[[ARG0:.*]]: !cir.ptr<!rec_A>
-// CIR-SAME:      %[[ARG1:.*]]: !rec_A
-// CIR-NEXT:           %[[VAL_0:.*]] = cir.alloca !cir.ptr<!rec_A>, !cir.ptr<!cir.ptr<!rec_A>>, ["this", init] {alignment = 8 : i64} 
-// CIR-NEXT:           %[[VAL_1:.*]] = cir.alloca !rec_A, !cir.ptr<!rec_A>, ["", init] {alignment = 8 : i64} 
-// CIR-NEXT:           %[[VAL_2:.*]] = cir.alloca !rec_A, !cir.ptr<!rec_A>, ["__retval"] {alignment = 8 : i64} 
-// CIR-NEXT:           cir.store %[[ARG0]], %[[VAL_0]] : !cir.ptr<!rec_A>, !cir.ptr<!cir.ptr<!rec_A>> 
-// CIR-NEXT:           cir.store %[[ARG1]], %[[VAL_1]] : !rec_A, !cir.ptr<!rec_A> 
-// CIR-NEXT:           %[[VAL_3:.*]] = cir.load %[[VAL_0]] : !cir.ptr<!cir.ptr<!rec_A>>, !cir.ptr<!rec_A> 
-// CIR-NEXT:           %[[VAL_4:.*]] = cir.vtable.get_vptr %[[VAL_3]] : !cir.ptr<!rec_A> -> !cir.ptr<!cir.vptr> 
-// CIR-NEXT:           %[[VAL_5:.*]] = cir.load align(8) %[[VAL_4]] : !cir.ptr<!cir.vptr>, !cir.vptr 
-// CIR-NEXT:           %[[VAL_6:.*]] = cir.vtable.get_virtual_fn_addr %[[VAL_5]][0] : !cir.vptr -> !cir.ptr<!cir.ptr<!cir.func<(!cir.ptr<!rec_A>)>>> 
-// CIR-NEXT:           %[[VAL_7:.*]] = cir.load align(8) %[[VAL_6]] : !cir.ptr<!cir.ptr<!cir.func<(!cir.ptr<!rec_A>)>>>, !cir.ptr<!cir.func<(!cir.ptr<!rec_A>)>> 
-// CIR-NEXT:           cir.call %[[VAL_7]](%[[VAL_3]]) : (!cir.ptr<!cir.func<(!cir.ptr<!rec_A>)>>, !cir.ptr<!rec_A>) -> () 
-// CIR-NEXT:           cir.trap 
-// CIR-NEXT:         } 
-
-
 class A {
   virtual ~A();
   A B(A);
 };
-A A::B(A) { this->~A(); }
+A A::B(A) { 
+  // CIR-LABEL:   cir.func dso_local @_ZN1A1BES_(
+  // CIR-SAME:      %[[THIS_ARG:.*]]: !cir.ptr<!rec_A>
+  // CIR-NEXT:           %[[THIS_VAR:.*]] = cir.alloca !cir.ptr<!rec_A>, !cir.ptr<!cir.ptr<!rec_A>>
+  // CIR:                %[[THIS:.*]] = cir.load %[[THIS_VAR]] : !cir.ptr<!cir.ptr<!rec_A>>, !cir.ptr<!rec_A> 
+  // CIR-NEXT:           %[[VPTR_PTR:.*]] = cir.vtable.get_vptr %[[THIS]] : !cir.ptr<!rec_A> -> !cir.ptr<!cir.vptr> 
+  // CIR-NEXT:           %[[VPTR:.*]] = cir.load align(8) %[[VPTR_PTR]] : !cir.ptr<!cir.vptr>, !cir.vptr 
+  // CIR-NEXT:           %[[DTOR_PTR:.*]] = cir.vtable.get_virtual_fn_addr %[[VPTR]][0] : !cir.vptr -> !cir.ptr<!cir.ptr<!cir.func<(!cir.ptr<!rec_A>)>>> 
+  // CIR-NEXT:           %[[DTOR:.*]] = cir.load align(8) %[[DTOR_PTR]] : !cir.ptr<!cir.ptr<!cir.func<(!cir.ptr<!rec_A>)>>>, !cir.ptr<!cir.func<(!cir.ptr<!rec_A>)>> 
+  // CIR-NEXT:           cir.call %[[DTOR]](%[[THIS]]) : (!cir.ptr<!cir.func<(!cir.ptr<!rec_A>)>>, !cir.ptr<!rec_A>) -> () 
+  // CIR-NEXT:           cir.trap 
+  // CIR-NEXT:         } 
+
+  this->~A(); 
+}
