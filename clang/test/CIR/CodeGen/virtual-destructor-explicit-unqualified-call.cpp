6bdbd0b6fe5ad1d2b02390a701c741789f78d711
ahmedshakill <shakil.000024@gmail.com>
2025-08-29 04:12:38 +0600

    add cir-llvm and og-llvm test

--- clang/test/CIR/CodeGen/virtual-destructor-explicit-unqualified-call.cpp
+++ clang/test/CIR/CodeGen/virtual-destructor-explicit-unqualified-call.cpp
@@ -1,6 +1,11 @@
 // RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -std=c++20 -fclangir -emit-cir %s -o %t.cir
 // RUN: FileCheck --check-prefix=CIR --input-file=%t.cir %s
 
+// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -std=c++20 -fclangir -emit-llvm %s -o %t.cir
+// RUN: FileCheck --check-prefix=LLVM --input-file=%t.cir %s
+
+// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -std=c++20 -emit-llvm %s -o %t.cir
+// RUN: FileCheck --check-prefix=OGCG --input-file=%t.cir %s
 
 class A {
   virtual ~A();
@@ -18,6 +23,28 @@ A A::B(A) {
   // CIR-NEXT:           cir.call %[[DTOR]](%[[THIS]]) : (!cir.ptr<!cir.func<(!cir.ptr<!rec_A>)>>, !cir.ptr<!rec_A>) -> () 
   // CIR-NEXT:           cir.trap 
   // CIR-NEXT:         } 
+
+
+  // LLVM-LABEL: define dso_local %class.A @_ZN1A1BES_(
+  // LLVM:         %[[VAL_0:.*]] = alloca ptr, i64 1, align 8
+  // LLVM:         %[[VAL_6:.*]] = load ptr, ptr %[[VAL_0]], align 8
+  // LLVM-NEXT:    %[[VAL_7:.*]] = load ptr, ptr %[[VAL_6]], align 8
+  // LLVM-NEXT:    %[[VAL_8:.*]] = getelementptr inbounds ptr, ptr %[[VAL_7]], i32 0
+  // LLVM-NEXT:    %[[VAL_9:.*]] = load ptr, ptr %[[VAL_8]], align 8
+  // LLVM-NEXT:    call void %[[VAL_9]](ptr %[[VAL_6]])
+  // LLVM-NEXT:    call void @llvm.trap()
+  
+
+  // OGCG-LABEL: define dso_local void @_ZN1A1BES_(
+  // OGCG:         %[[VAL_0:.*]] = alloca ptr, align 8
+  // OGCG-NEXT:    %[[VAL_1:.*]] = alloca ptr, align 8
+  // OGCG:         %[[VAL_6:.*]] = load ptr, ptr %[[VAL_1]], align 8
+  // OGCG-NEXT:    %[[VAL_7:.*]] = load ptr, ptr %[[VAL_6]], align 8
+  // OGCG-NEXT:    %[[VAL_8:.*]] = getelementptr inbounds ptr, ptr %[[VAL_7]], i64 0
+  // OGCG-NEXT:    %[[VAL_9:.*]] = load ptr, ptr %[[VAL_8]], align 8
+  // OGCG-NEXT:    call void %[[VAL_9]](ptr noundef nonnull align 8 dereferenceable(8) %[[VAL_6]]) #2
+  // OGCG-NEXT:    call void @llvm.trap()
+
 
   this->~A(); 
 }
