8ef29a0741efd66f1808e1f5c5165363e59c27ca
Sharp0802 <sharp0802.business@gmail.com>
2025-08-28 15:32:41 +0900

    [CIR] Add tests for annotating constructor and destructor

---
+++ clang/test/CIR/CodeGen/attr-annotate-destructor.cpp
@@ -0,0 +1,10 @@
+// RUN: %clang_cc1 -emit-cir %s -o %t.cir
+// RUN: FileCheck --input-file %t.cir %s -check-prefix=CIR
+
+class Foo {
+public:
+  [[clang::annotate("test")]] ~Foo() {}
+};
+// CHECK: #cir.annotation<name = "test", args = []>
+
+Foo foo;
