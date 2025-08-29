83f2d3d05d7fdf8166cb5f2198aa061959732d0a
ahmedshakill <shakil.000024@gmail.com>
2025-08-30 01:41:21 +0600

    name values properly

--- clang/test/CIR/CodeGen/virtual-destructor-explicit-unqualified-call.cpp
+++ clang/test/CIR/CodeGen/virtual-destructor-explicit-unqualified-call.cpp
@@ -11,40 +11,47 @@ class A {
   virtual ~A();
   A B(A);
 };
-A A::B(A) { 
+A A::B(A) {
   // CIR-LABEL:   cir.func dso_local @_ZN1A1BES_(
   // CIR-SAME:      %[[THIS_ARG:.*]]: !cir.ptr<!rec_A>
   // CIR-NEXT:           %[[THIS_VAR:.*]] = cir.alloca !cir.ptr<!rec_A>, !cir.ptr<!cir.ptr<!rec_A>>
-  // CIR:                %[[THIS:.*]] = cir.load %[[THIS_VAR]] : !cir.ptr<!cir.ptr<!rec_A>>, !cir.ptr<!rec_A> 
-  // CIR-NEXT:           %[[VPTR_PTR:.*]] = cir.vtable.get_vptr %[[THIS]] : !cir.ptr<!rec_A> -> !cir.ptr<!cir.vptr> 
-  // CIR-NEXT:           %[[VPTR:.*]] = cir.load align(8) %[[VPTR_PTR]] : !cir.ptr<!cir.vptr>, !cir.vptr 
-  // CIR-NEXT:           %[[DTOR_PTR:.*]] = cir.vtable.get_virtual_fn_addr %[[VPTR]][0] : !cir.vptr -> !cir.ptr<!cir.ptr<!cir.func<(!cir.ptr<!rec_A>)>>> 
-  // CIR-NEXT:           %[[DTOR:.*]] = cir.load align(8) %[[DTOR_PTR]] : !cir.ptr<!cir.ptr<!cir.func<(!cir.ptr<!rec_A>)>>>, !cir.ptr<!cir.func<(!cir.ptr<!rec_A>)>> 
-  // CIR-NEXT:           cir.call %[[DTOR]](%[[THIS]]) : (!cir.ptr<!cir.func<(!cir.ptr<!rec_A>)>>, !cir.ptr<!rec_A>) -> () 
-  // CIR-NEXT:           cir.trap 
-  // CIR-NEXT:         } 
+  // CIR:                %[[THIS:.*]] = cir.load %[[THIS_VAR]] : !cir.ptr<!cir.ptr<!rec_A>>, !cir.ptr<!rec_A>
+  // CIR-NEXT:           %[[VPTR_PTR:.*]] = cir.vtable.get_vptr %[[THIS]] : !cir.ptr<!rec_A> -> !cir.ptr<!cir.vptr>
+  // CIR-NEXT:           %[[VPTR:.*]] = cir.load align(8) %[[VPTR_PTR]] : !cir.ptr<!cir.vptr>, !cir.vptr
+  // CIR-NEXT:           %[[DTOR_PTR:.*]] = cir.vtable.get_virtual_fn_addr %[[VPTR]][0] : !cir.vptr -> !cir.ptr<!cir.ptr<!cir.func<(!cir.ptr<!rec_A>)>>>
+  // CIR-NEXT:           %[[DTOR:.*]] = cir.load align(8) %[[DTOR_PTR]] : !cir.ptr<!cir.ptr<!cir.func<(!cir.ptr<!rec_A>)>>>, !cir.ptr<!cir.func<(!cir.ptr<!rec_A>)>>
+  // CIR-NEXT:           cir.call %[[DTOR]](%[[THIS]]) : (!cir.ptr<!cir.func<(!cir.ptr<!rec_A>)>>, !cir.ptr<!rec_A>) -> ()
+  // CIR-NEXT:           cir.trap
+  // CIR-NEXT:         }
 
 
-  // LLVM-LABEL: define dso_local %class.A @_ZN1A1BES_(
-  // LLVM:         %[[VAL_0:.*]] = alloca ptr, i64 1, align 8
-  // LLVM:         %[[VAL_6:.*]] = load ptr, ptr %[[VAL_0]], align 8
-  // LLVM-NEXT:    %[[VAL_7:.*]] = load ptr, ptr %[[VAL_6]], align 8
-  // LLVM-NEXT:    %[[VAL_8:.*]] = getelementptr inbounds ptr, ptr %[[VAL_7]], i32 0
-  // LLVM-NEXT:    %[[VAL_9:.*]] = load ptr, ptr %[[VAL_8]], align 8
-  // LLVM-NEXT:    call void %[[VAL_9]](ptr %[[VAL_6]])
-  // LLVM-NEXT:    call void @llvm.trap()
+  // LLVM-LABEL:   define dso_local %class.A @_ZN1A1BES_(
+  // LLVM-SAME:      ptr %[[THIS_ARG:[0-9]+]],
+  // LLVM-NEXT:          %[[THIS_VAR:.*]] = alloca ptr, i64 1, align 8
+  // LLVM:               store ptr %[[THIS_ARG]], ptr %[[THIS_VAR]], align 8
+  // LLVM:               %[[THIS_V2:.*]] = load ptr, ptr %[[THIS_VAR]], align 8
+  // LLVM-NEXT:          %[[VTABLE_PTR:.*]] = load ptr, ptr %[[THIS_V2]], align 8
+  // LLVM-NEXT:          %[[DTOR_PTR:.*]] = getelementptr inbounds ptr, ptr %[[VTABLE_PTR]], i32 0
+  // LLVM-NEXT:          %[[DTOR:.*]] = load ptr, ptr %[[DTOR_PTR]], align 8
+  // LLVM-NEXT:          call void %[[DTOR]](ptr %[[THIS_V2]])
+  // LLVM-NEXT:          call void @llvm.trap()
+  // LLVM-NEXT:          unreachable
+  // LLVM-NEXT:        }
+
   
+  // OGCG-LABEL:   define dso_local void @_ZN1A1BES_(
+  // OGCG-SAME:      ptr {{.*}}%[[THIS_ARG:.*]],
+  // OGCG:               %[[VAL_0:.*]] = alloca ptr, align 8
+  // OGCG-NEXT:          %[[THIS_VAR:.*]] = alloca ptr, align 8
+  // OGCG:               store ptr %[[THIS_ARG]], ptr %[[THIS_VAR]], align 8
+  // OGCG:               %[[THIS_V2:.*]] = load ptr, ptr %[[THIS_VAR]], align 8
+  // OGCG-NEXT:          %[[VTABLE:.*]] = load ptr, ptr %[[THIS_V2]], align 8
+  // OGCG-NEXT:          %[[DTOR_PTR:.*]] = getelementptr inbounds ptr, ptr %[[VTABLE]], i64 0
+  // OGCG-NEXT:          %[[DTOR:.*]] = load ptr, ptr %[[DTOR_PTR]], align 8
+  // OGCG-NEXT:          call void %[[DTOR]](ptr noundef nonnull align 8 dereferenceable(8) %[[THIS_V2]]) #2
+  // OGCG-NEXT:          call void @llvm.trap()
+  // OGCG-NEXT:          unreachable
+  // OGCG-NEXT:        }
 
-  // OGCG-LABEL: define dso_local void @_ZN1A1BES_(
-  // OGCG:         %[[VAL_0:.*]] = alloca ptr, align 8
-  // OGCG-NEXT:    %[[VAL_1:.*]] = alloca ptr, align 8
-  // OGCG:         %[[VAL_6:.*]] = load ptr, ptr %[[VAL_1]], align 8
-  // OGCG-NEXT:    %[[VAL_7:.*]] = load ptr, ptr %[[VAL_6]], align 8
-  // OGCG-NEXT:    %[[VAL_8:.*]] = getelementptr inbounds ptr, ptr %[[VAL_7]], i64 0
-  // OGCG-NEXT:    %[[VAL_9:.*]] = load ptr, ptr %[[VAL_8]], align 8
-  // OGCG-NEXT:    call void %[[VAL_9]](ptr noundef nonnull align 8 dereferenceable(8) %[[VAL_6]]) #2
-  // OGCG-NEXT:    call void @llvm.trap()
-
-
-  this->~A(); 
+  this->~A();
 }
