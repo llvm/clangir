3de2824095209c3f056ac76be09596e4e010a88d
Jasmine Tang <jjasmine@igalia.com>
2025-09-09 16:11:44 -0700

    [CIR] Added support for CK_BaseToDerived in emitCastLValue (#1864)
    Fixes #1808

--- clang/lib/CIR/CodeGen/CIRGenExpr.cpp
+++ clang/lib/CIR/CodeGen/CIRGenExpr.cpp
@@ -2016,7 +2016,25 @@ LValue CIRGenFunction::emitCastLValue(const CastExpr *E) {
   case CK_ToUnion:
     assert(0 && "NYI");
   case CK_BaseToDerived: {
-    assert(0 && "NYI");
+    const auto *derivedClassTy = E->getType()->castAs<RecordType>();
+    auto *derivedClassDecl = cast<CXXRecordDecl>(derivedClassTy->getDecl());
+
+    LValue lv = emitLValue(E->getSubExpr());
+
+    // Perform the base-to-derived conversion
+    Address derived = getAddressOfDerivedClass(
+        lv.getAddress(), derivedClassDecl, E->path_begin(), E->path_end(),
+        /*NullCheckValue=*/false);
+    // C++11 [expr.static.cast]p2: Behavior is undefined if a downcast is
+    // performed and the object is not of the derived type.
+    if (sanitizePerformTypeCheck())
+      llvm_unreachable("TCK_DowncastReference NYI");
+
+    if (SanOpts.has(SanitizerKind::CFIDerivedCast))
+      llvm_unreachable("CFITypeCheckKind NYI");
+
+    return makeAddrLValue(derived, E->getType(), lv.getBaseInfo(),
+                          CGM.getTBAAInfoForSubobject(lv, E->getType()));
   }
   case CK_LValueBitCast: {
     // This must be a reinterpret_cast (or c-style equivalent).
