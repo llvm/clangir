78b2ef5bfc3dca6018770617eedf8837f7d676bb
David Rivera <davidriverg@gmail.com>
2025-09-15 17:37:45 -0400

    [CIR][CIRGen][CUDA] Lower PTX synchronization primitives (#1903)

--- clang/test/CIR/CodeGen/CUDA/builtins-nvptx-ptx60.cu
+++ clang/test/CIR/CodeGen/CUDA/builtins-nvptx-ptx60.cu
@@ -1,41 +1,41 @@
 // RUN: %clang_cc1 -triple nvptx64-nvidia-cuda -fclangir -target-cpu sm_70 \
 // RUN:            -fcuda-is-device -target-feature +ptx60 \
-// RUN:            -emit-cir -o - -x cuda %s \
-// RUN:   | FileCheck -check-prefix=CIR %s
+// RUN:            -emit-cir -o %t.cir -x cuda %s
+// RUN: FileCheck -check-prefix=CIR --input-file=%t.cir %s
 // RUN: %clang_cc1 -triple nvptx64-nvidia-cuda -fclangir -target-cpu sm_80 \
 // RUN:            -fcuda-is-device -target-feature +ptx65 \
-// RUN:            -emit-cir -o - -x cuda %s \
-// RUN:   | FileCheck -check-prefix=CIR %s
+// RUN:            -emit-cir -o %t.cir -x cuda %s
+// RUN: FileCheck -check-prefix=CIR --input-file=%t.cir %s
 // RUN: %clang_cc1 -triple nvptx64-nvidia-cuda -fclangir -target-cpu sm_80 \
 // RUN:            -fcuda-is-device -target-feature +ptx70 \
-// RUN:            -emit-cir -o - -x cuda %s \
-// RUN:   | FileCheck -check-prefix=CIR %s
+// RUN:            -emit-cir -o %t.cir -x cuda %s
+// RUN: FileCheck -check-prefix=CIR --input-file=%t.cir %s
 
 // RUN: %clang_cc1 -triple nvptx64-nvidia-cuda -fclangir -target-cpu sm_70 \
 // RUN:            -fcuda-is-device -target-feature +ptx60 \
-// RUN:            -emit-llvm -o - -x cuda %s \
-// RUN:   | FileCheck -check-prefix=LLVM %s
+// RUN:            -emit-llvm -o %t.ll -x cuda %s
+// RUN: FileCheck -check-prefix=LLVM --input-file=%t.ll %s
 // RUN: %clang_cc1 -triple nvptx64-nvidia-cuda -fclangir -target-cpu sm_80 \
 // RUN:            -fcuda-is-device -target-feature +ptx65 \
-// RUN:            -emit-llvm -o - -x cuda %s \
-// RUN:   | FileCheck -check-prefix=LLVM %s
+// RUN:            -emit-llvm -o %t.ll -x cuda %s
+// RUN: FileCheck -check-prefix=LLVM --input-file=%t.ll %s
 // RUN: %clang_cc1 -triple nvptx64-nvidia-cuda -fclangir -target-cpu sm_80 \
 // RUN:            -fcuda-is-device -target-feature +ptx70 \
-// RUN:            -emit-llvm -o - -x cuda %s \
-// RUN:   | FileCheck -check-prefix=LLVM %s
+// RUN:            -emit-llvm -o %t.ll -x cuda %s
+// RUN: FileCheck -check-prefix=LLVM --input-file=%t.ll %s
 
 // RUN: %clang_cc1 -triple nvptx64-nvidia-cuda -target-cpu sm_70 \
 // RUN:            -fcuda-is-device -target-feature +ptx60 \
-// RUN:            -emit-llvm -o - -x cuda %s \
-// RUN:   | FileCheck -check-prefix=OGCHECK %s
+// RUN:            -emit-llvm -o %t_og.ll -x cuda %s
+// RUN: FileCheck -check-prefix=OGCHECK --input-file=%t_og.ll %s
 // RUN: %clang_cc1 -triple nvptx64-nvidia-cuda -target-cpu sm_80 \
 // RUN:            -fcuda-is-device -target-feature +ptx65 \
-// RUN:            -emit-llvm -o - -x cuda %s \
-// RUN:   | FileCheck -check-prefix=OGCHECK %s
+// RUN:            -emit-llvm -o %t_og.ll -x cuda %s
+// RUN: FileCheck -check-prefix=OGCHECK --input-file=%t_og.ll %s
 // RUN: %clang_cc1 -triple nvptx64-nvidia-cuda -target-cpu sm_80 \
 // RUN:            -fcuda-is-device -target-feature +ptx70 \
-// RUN:            -emit-llvm -o - -x cuda %s \
-// RUN:   | FileCheck -check-prefix=OGCHECK %s
+// RUN:            -emit-llvm -o %t_og.ll -x cuda %s
+// RUN: FileCheck -check-prefix=OGCHECK --input-file=%t_og.ll %s
 
 #define __device__ __attribute__((device))
 #define __global__ __attribute__((global))
@@ -51,5 +51,15 @@ __device__ void nvvm_sync(unsigned mask, int i, float f, int a, int b,
   // LLVM: call void @llvm.nvvm.bar.warp.sync(i32
   // OGCHECK: call void @llvm.nvvm.bar.warp.sync(i32
   __nvvm_bar_warp_sync(mask);
+
+  // CIR: cir.llvm.intrinsic "nvvm.barrier.cta.sync.all" {{.*}} : (!u32i)
+  // LLVM: call void @llvm.nvvm.barrier.cta.sync.all(i32
+  // OGCHECK: call void @llvm.nvvm.barrier.cta.sync.all(i32
+  __nvvm_barrier_sync(mask);
+
+  // CIR: cir.llvm.intrinsic "nvvm.barrier.cta.sync.count" {{.*}} : (!u32i, !u32i)
+  // LLVM: call void @llvm.nvvm.barrier.cta.sync.count(i32
+  // OGCHECK: call void @llvm.nvvm.barrier.cta.sync.count(i32
+  __nvvm_barrier_sync_cnt(mask, i);
 
 }
