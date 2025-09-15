fe5977dc626ade5e743f1e7de0ee7ec66341d570
Amr Hesham <amr96@programmer.net>
2025-09-15 23:36:36 +0200

    [CIR] Backport VisitOpaqueValueExpr support for Complex (#1902)
    Backport fixes in VisitAbstractConditionalOperator to handle
    OpaqueValueExpr from the upstream
    https://github.com/llvm/llvm-project/pull/157331

--- clang/lib/CIR/CodeGen/CIRGenExprComplex.cpp
+++ clang/lib/CIR/CodeGen/CIRGenExprComplex.cpp
@@ -332,21 +332,32 @@ public:
 
   mlir::Value
   VisitAbstractConditionalOperator(const AbstractConditionalOperator *E) {
-    mlir::Value condValue = Visit(E->getCond());
     mlir::Location loc = CGF.getLoc(E->getSourceRange());
+
+    // Bind the common expression if necessary.
+    CIRGenFunction::OpaqueValueMapping binding(CGF, E);
+
+    CIRGenFunction::ConditionalEvaluation eval(CGF);
+
+    Expr *cond = E->getCond()->IgnoreParens();
+    mlir::Value condValue = CGF.evaluateExprAsBool(cond);
 
     return Builder
         .create<cir::TernaryOp>(
             loc, condValue,
             /*thenBuilder=*/
             [&](mlir::OpBuilder &b, mlir::Location loc) {
+              eval.begin(CGF);
               mlir::Value trueValue = Visit(E->getTrueExpr());
               b.create<cir::YieldOp>(loc, trueValue);
+              eval.end(CGF);
             },
             /*elseBuilder=*/
             [&](mlir::OpBuilder &b, mlir::Location loc) {
+              eval.begin(CGF);
               mlir::Value falseValue = Visit(E->getFalseExpr());
               b.create<cir::YieldOp>(loc, falseValue);
+              eval.end(CGF);
             })
         .getResult();
   }
