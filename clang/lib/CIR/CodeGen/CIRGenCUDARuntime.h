f75ba98cd56b1863bb836fd2c6cf415b9523500c
David Rivera <davidriverg@gmail.com>
2025-09-17 13:55:07 -0400

    [CIR][CIRGen][CUDA] Add kernel stub retrieval and update built-in variables tests (#1904)

--- clang/lib/CIR/CodeGen/CIRGenCUDARuntime.h
+++ clang/lib/CIR/CodeGen/CIRGenCUDARuntime.h
@@ -45,6 +45,9 @@ public:
                                         const CUDAKernelCallExpr *expr,
                                         ReturnValueSlot retValue);
   virtual mlir::Operation *getKernelHandle(cir::FuncOp fn, GlobalDecl GD) = 0;
+  /// Get kernel stub by kernel handle.
+  virtual mlir::Operation *getKernelStub(mlir::Operation *handle) = 0;
+
   virtual void internalizeDeviceSideVar(const VarDecl *d,
                                         cir::GlobalLinkageKind &linkage) = 0;
   /// Returns function or variable name on device side even if the current
