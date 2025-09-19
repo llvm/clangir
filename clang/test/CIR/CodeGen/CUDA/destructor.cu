d8f3180574d1583fa11fa413b42e47b3717043d4
David Rivera <davidriverg@gmail.com>
2025-09-19 15:10:33 -0400

    [CIR][CUDA] FIx CUDA host compilation on kernel launch (#1906)
    This PR implements some missing blocks that allow us to effectively
    allow us to launch kernels from the host. All of the tests stated in
    this
    [commit](https://github.com/llvm/clangir/commit/69f2099870067dcacbc6fecb10c77f22e0c5b2d9)
    are now resolved.
    
    I spent half a day figuring the following:
    
    I tried experiementing performing host compilation(`-fcuda-is-device`)
    with target triple: `nvptx64-nvidia-cuda` but was getting a module
    verification error that, to keep it simple looked like: `error:
    'cir.call' op calling convention mismatch: expected ptx_kernel, but
    provided c`.
    
    I thought that was expected given that we're essentially using the
    device to compile on the host, which doesn't make a lot of sense. until
    I tried to replicate the same in OG and didn't really run into any
    problem in that regard. Are the calling conventions enforced in CIR much
    more strict as compared to OG? Or is that simply a bug from OG?

--- clang/test/CIR/CodeGen/CUDA/destructor.cu
+++ clang/test/CIR/CodeGen/CUDA/destructor.cu
@@ -10,6 +10,26 @@
 // RUN:            %s -o %t.cir
 // RUN: FileCheck --check-prefix=CIR-DEVICE --input-file=%t.cir %s
 
+// RUN: %clang_cc1 -triple nvptx64-nvidia-cuda -fclangir \
+// RUN:            -fcuda-is-device -emit-llvm -target-sdk-version=12.3 \
+// RUN:            %s -o %t.ll
+// RUN: FileCheck --check-prefix=LLVM-DEVICE --input-file=%t.ll %s
+
+// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -fclangir \
+// RUN:            -x cuda -emit-llvm -target-sdk-version=12.3 \
+// RUN:            %s -o %t.ll
+// RUN: FileCheck --check-prefix=LLVM-HOST --input-file=%t.ll %s
+
+// RUN: %clang_cc1 -triple nvptx64-nvidia-cuda \
+// RUN:            -fcuda-is-device -emit-llvm -target-sdk-version=12.3 \
+// RUN:            %s -o %t.ll
+// RUN: FileCheck --check-prefix=OGCG-DEVICE --input-file=%t.ll %s
+
+// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu \
+// RUN:            -x cuda -emit-llvm -target-sdk-version=12.3 \
+// RUN:            %s -o %t.ll
+// RUN: FileCheck --check-prefix=OGCG-HOST --input-file=%t.ll %s
+
 // Make sure we do emit device-side kernel even if it's only referenced
 // by the destructor of a variable not present on device.
 template<typename T> __global__ void f(T) {}
@@ -19,11 +39,23 @@ template<typename T> struct A {
 
 // CIR-HOST: module
 // CIR-DEVICE: module
-// CIR-DEVICE-DISABLED: cir.func dso_local @_Z1fIiEvT_
+// CIR-DEVICE: cir.func dso_local @_Z1fIiEvT_
+// LLVM-DEVICE: define dso_local ptx_kernel void @_Z1fIiEvT_
+// OGCG-DEVICE: define ptx_kernel void @_Z1fIiEvT_
 
-// CIR-HOST-DISABLED: cir.func {{.*}} @_ZN1AIiED2Ev{{.*}} {
-// CIR-HOST-DISABLED:   cir.call @__cudaPushCallConfiguration
-// CIR-HOST-DISABLED:   cir.call @_Z16__device_stub__fIiEvT_
-// CIR-HOST-DISABLED: }
+// CIR-HOST: cir.func {{.*}} @_ZN1AIiED2Ev{{.*}} {
+// CIR-HOST:   cir.call @__cudaPushCallConfiguration
+// CIR-HOST:   cir.call @_Z16__device_stub__fIiEvT_
+// CIR-HOST: }
 
-// A<int> a;
+// LLVM-HOST: define linkonce_odr void @_ZN1AIiED2Ev
+// LLVM-HOST: call i32 @__cudaPushCallConfiguration(
+// LLVM-HOST: call void @_Z16__device_stub__fIiEvT_
+
+// OGCG-HOST: define linkonce_odr void @_ZN1AIiED2Ev
+// OGCG-HOST: call i32 @__cudaPushCallConfiguration(
+// OGCG-HOST: call void @_Z16__device_stub__fIiEvT_
+
+
+
+A<int> a;
