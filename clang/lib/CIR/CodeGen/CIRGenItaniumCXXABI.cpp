b898f638d84b8a29616df8af332381827950d0df
Nathan Lanza <nathanlanza@gmail.com>
2025-11-22 17:11:36 -0800

    [CIR] Fix RTTI mangling assertion with qualified types
    Fixes an assertion failure when creating RTTI descriptors for types
    with top-level qualifiers (const, volatile, etc.). The Itanium C++ ABI
    requires that RTTI descriptors be created for unqualified types only,
    but ClangIR was passing qualified types to the mangling function.
    
    The fix strips qualifiers in BuildTypeInfo before mangling, matching
    the behavior of traditional CodeGen. Also strips qualifiers when
    getting RTTI descriptors for dynamic_cast operands, consistent with
    CodeGen's implementation.
    
    This fixes std::regex usage and other cases where qualified types
    are used with typeid or dynamic_cast.
    
    
    ghstack-source-id: e18ff762bcee1e6dc973b16d00163fc55ed7ae08
    Pull-Request: https://github.com/llvm/clangir/pull/2002

--- clang/lib/CIR/CodeGen/CIRGenItaniumCXXABI.cpp
+++ clang/lib/CIR/CodeGen/CIRGenItaniumCXXABI.cpp
@@ -1542,6 +1542,10 @@ mlir::Attribute CIRGenItaniumRTTIBuilder::BuildTypeInfo(mlir::Location loc,
   // We want to operate on the canonical type.
   Ty = Ty.getCanonicalType();
 
+  // RTTI descriptors for qualified types are really just descriptors
+  // for the unqualified type, so strip qualifiers.
+  Ty = Ty.getUnqualifiedType();
+
   // Check if we've already emitted an RTTI descriptor for this type.
   SmallString<256> Name;
   llvm::raw_svector_ostream Out(Name);
@@ -2696,9 +2700,9 @@ static cir::DynamicCastInfoAttr emitDynamicCastInfo(CIRGenFunction &CGF,
                                                     QualType SrcRecordTy,
                                                     QualType DestRecordTy) {
   auto srcRtti = mlir::cast<cir::GlobalViewAttr>(
-      CGF.CGM.getAddrOfRTTIDescriptor(Loc, SrcRecordTy));
+      CGF.CGM.getAddrOfRTTIDescriptor(Loc, SrcRecordTy.getUnqualifiedType()));
   auto destRtti = mlir::cast<cir::GlobalViewAttr>(
-      CGF.CGM.getAddrOfRTTIDescriptor(Loc, DestRecordTy));
+      CGF.CGM.getAddrOfRTTIDescriptor(Loc, DestRecordTy.getUnqualifiedType()));
 
   auto runtimeFuncOp = getItaniumDynamicCastFn(CGF);
   auto badCastFuncOp = getBadCastFn(CGF);
