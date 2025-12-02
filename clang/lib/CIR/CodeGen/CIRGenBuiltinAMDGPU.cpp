3cd36802ee13adf7c69e967966f4c668cb889bd0
skc7 <Krishna.Sankisa@amd.com>
2025-12-02 21:06:02 +0530

    Fix comments

--- clang/lib/CIR/CodeGen/CIRGenBuiltinAMDGPU.cpp
+++ clang/lib/CIR/CodeGen/CIRGenBuiltinAMDGPU.cpp
@@ -1,5 +1,4 @@
-//===---- CIRGenBuiltinAMDGPU.cpp - Emit CIR for AMDGPU builtins
-//----------------===//
+//===---- CIRGenBuiltinAMDGPU.cpp - Emit CIR for AMDGPU builtins ----------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
