8ca1ac1f7fbc9ce19ab0602517b2de7b87c41862
Chibuoyim (Wilson) Ogbonna <ogbonnachibuoyim12@gmail.com>
2025-12-02 23:56:27 +0100

    [CIR][CodeGen] Implement MayDropFunctionReturn for non-trivial destructor (#2038)
    Pretty much the title. Nothing new introduced here, just pulled from the
    [OG](https://github.com/llvm/clangir/blob/4037fb65a7269b0cfd248605aabeca922c6c5442/clang/lib/CodeGen/CGCall.cpp#L1897C1-L1901C4)
    and added a couple of tests.

--- clang/test/CIR/CodeGen/return.cpp
+++ clang/test/CIR/CodeGen/return.cpp
@@ -1,4 +1,6 @@
 // RUN: %clang_cc1 -std=c++17 -triple x86_64-unknown-linux-gnu -fclangir -emit-cir %s -o - | FileCheck %s
+// RUN: %clang_cc1 -std=c++17 -triple x86_64-unknown-linux-gnu -fno-strict-return -fclangir -emit-llvm %s -o - | FileCheck %s --check-prefix=CHECK-NOSTRICT
+// RUN: %clang_cc1 -std=c++17 -triple x86_64-unknown-linux-gnu -fno-strict-return -emit-llvm %s -o - | FileCheck %s --check-prefix=OGCG-CHECK-NOSTRICT
 
 int &ret0(int &x) {
   return x;
@@ -31,3 +33,34 @@ int unreachable_after_return() {
 // CHECK-NEXT:   cir.store{{.*}} %3, %0 : !s32i, !cir.ptr<!s32i>
 // CHECK-NEXT:   cir.br ^bb1
 // CHECK-NEXT: }
+
+struct NonTrivialDefaultConstructor {
+  int x;
+
+  NonTrivialDefaultConstructor() { }
+};
+
+// CHECK-NOSTRICT-LABEL: @_Z28nonTrivialDefaultConstructorv
+// OGCG-CHECK-NOSTRICT-LABEL: @_Z28nonTrivialDefaultConstructorv
+NonTrivialDefaultConstructor nonTrivialDefaultConstructor() {
+  // CHECK-NOSTRICT-NOT: call void @llvm.trap
+  // CHECK-NOSTRICT-NOT: unreachable
+  // OGCG-CHECK-NOSTRICT-NOT: call void @llvm.trap
+  // OGCG-CHECK-NOSTRICT-NOT: unreachable
+}
+
+// Functions that return records with non-trivial destructors should always use
+// the -fstrict-return optimization.
+
+struct NonTrivialDestructor {
+  ~NonTrivialDestructor();
+};
+
+// CHECK-NOSTRICT-LABEL: @_Z20nonTrivialDestructorv
+// OGCG-CHECK-NOSTRICT-LABEL: @_Z20nonTrivialDestructorv
+NonTrivialDestructor nonTrivialDestructor() {
+  // CHECK-NOSTRICT: call void @llvm.trap
+  // CHECK-NOSTRICT-NEXT: unreachable
+  // OGCG-CHECK-NOSTRICT: call void @llvm.trap
+  // OGCG-CHECK-NOSTRICT-NEXT: unreachable
+}
