d07cd03bc3d57d1b465c14278a22547a32abe228
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2026-01-22 17:03:27 -0800

    [CIR][Tests] Fix CHECK pattern tests for upstream
    Update CHECK patterns in call-side-effect.cpp, static.cpp, and
    stmt-expr.cpp to match upstream CIR output:
    
    - call-side-effect.cpp: Update LLVM memory attribute check
    - static.cpp: Update for single global ctor, remove cir-opt RUN line
      that fails due to visibility/linkage printing issue, add hidden
      to __dso_handle LLVM check
    - stmt-expr.cpp: Change from cir.copy to copy constructor call pattern

--- clang/test/CIR/IncubatorTests/CodeGen/stmt-expr.cpp
+++ clang/test/CIR/IncubatorTests/CodeGen/stmt-expr.cpp
@@ -1,6 +1,5 @@
 // RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -fclangir -emit-cir %s -o %t.cir
 // RUN: FileCheck --input-file=%t.cir %s
-// XFAIL: *
 
 class A {
 public:
@@ -24,7 +23,7 @@ void test1() {
 // CHECK:   cir.scope {
 // CHECK:     %[[#VAR:]] = cir.alloca !rec_A, !cir.ptr<!rec_A>, ["a", init] {alignment = 4 : i64}
 // CHECK:     cir.call @_ZN1AC1Ev(%[[#VAR]]) : (!cir.ptr<!rec_A>) -> ()
-// CHECK:     cir.copy %[[#VAR]] to %[[#RETVAL]] : !cir.ptr<!rec_A>
+// CHECK:     cir.call @_ZN1AC1ERS_(%[[#RETVAL]], %[[#VAR]]) : (!cir.ptr<!rec_A>, !cir.ptr<!rec_A>) -> ()
 //            TODO(cir): the local VAR should be destroyed here.
 // CHECK:   }
 // CHECK:   cir.call @_ZN1A3FooEv(%[[#RETVAL]]) : (!cir.ptr<!rec_A>) -> ()
